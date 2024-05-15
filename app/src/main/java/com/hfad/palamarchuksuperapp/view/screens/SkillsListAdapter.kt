a1510401c0f04c5c6242af8594e0890af4e8c86f
palamarchukhome <palamarchuk.office@gmail.com>
2024-05-15 17:31:59 +0300

    Adjusting size/code to add expandable text for my description.

--- app/src/main/java/com/hfad/palamarchuksuperapp/view/screens/SkillsListAdapter.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/view/screens/SkillsListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.constraintlayout.widget.Constraints.LayoutParams
 import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.AsyncListDiffer
@@ -17,19 +18,23 @@ import com.hfad.palamarchuksuperapp.databinding.ListItemSkillsBinding
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class SkillsListAdapter (
+class SkillsListAdapter(
     private val viewModel: SkillsViewModel,
-    private val fragmentManager: FragmentManager) :
+    private val fragmentManager: FragmentManager
+) :
     ListAdapter<Skill, SkillsListAdapter.SkillHolder>(SkillDiffItemCallback()) {
 
     private val asyncListDiffer = AsyncListDiffer(this, SkillDiffItemCallback())
 
-    fun setDate (skillList: List<Skill>) {
+    val startedHeight: Int = 0
+
+    fun setDate(skillList: List<Skill>) {
         asyncListDiffer.submitList(skillList)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SkillHolder {
-        val binding = ListItemSkillsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ListItemSkillsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return SkillHolder(binding, viewModel, fragmentManager)
     }
 
@@ -49,20 +54,12 @@ class SkillsListAdapter (
 
         fun bind(skill: Skill) {
 
-            if(binding.skillDescription.text.length<skill.description.length) {
-                binding.expandDetails.isVisible = true
-                binding.cardSkill.setOnClickListener {
-                    Log.d("Was clicked", "expandDetails")
-                    val layoutParams = binding.skillCard.layoutParams
-                    layoutParams.height = 200
-                    binding.skillCard.layoutParams = layoutParams
-                    binding.skillDescription.maxLines = Int.MAX_VALUE
-                    binding.expandDetails.isVisible = false
-                }
+            binding.cardSkill.setOnClickListener {
+                binding.materialCheckBox.isChecked = !binding.materialCheckBox.isChecked
             }
 
             binding.skillTitle.text = skill.name.uppercase(Locale.getDefault())
-            binding.skillDescription.text = skill.description
+            //    binding.skillDescription.text = skill.description
             binding.skillDate.text =
                 SimpleDateFormat("dd MMMM: HH:mm", Locale.US).format(skill.date)
             binding.moreButton.setOnClickListener {
@@ -72,8 +69,9 @@ class SkillsListAdapter (
                     when (item.itemId) {
 
                         R.id.menu_option_edit -> {
-                                val bottomSheetFragment = BottomSheetFragment(skill)
-                                bottomSheetFragment.show(parentFragmentManager, "BSDialogFragment")
+                            val bottomSheetFragment =
+                                BottomSheetFragment(skill, viewModel = myViewModel)
+                            bottomSheetFragment.show(parentFragmentManager, "BSDialogFragment")
 
                             Toast.makeText(
                                 this.binding.root.context,
@@ -81,6 +79,7 @@ class SkillsListAdapter (
                                 Toast.LENGTH_SHORT
                             ).show()
                         }
+
                         R.id.menu_option_delete -> {
                             myViewModel.deleteSkill(myViewModel.date.value.indexOf(skill))
                             Toast.makeText(
@@ -103,11 +102,57 @@ class SkillsListAdapter (
                 }
                 popupMenu.show()
             }
+
+            binding.skillDescription.text = skill.description
+            Log.d("Maximum 2", "${binding.skillDescription.lineCount}")
+
+
+            if (binding.skillDescription.text.length < skill.description.length) {
+                binding.expandDetails.isEnabled = true
+                binding.expandDetails.isVisible = true
+                binding.expandDetails.setOnClickListener {
+                    expandOrHide(skill)
+                }
+            }
+        }
+
+        val startedHeight = binding.skillCard.layoutParams.height
+
+        fun expandOrHide(skill: Skill) {
+
+            if (!skill.isExpanded) {
+                val layoutParamsDescription = binding.skillDescription.layoutParams
+                layoutParamsDescription.height = LayoutParams.MATCH_PARENT
+                binding.skillDescription.layoutParams = layoutParamsDescription
+                binding.skillDescription.maxLines = Int.MAX_VALUE
+
+                val layoutParamsCard = binding.skillCard.layoutParams
+                layoutParamsCard.height = LayoutParams.WRAP_CONTENT
+                binding.skillCard.layoutParams = layoutParamsCard
+
+                binding.expandDetails.text = "Hide >>"
+                skill.isExpanded = true
+
+            } else {
+
+                val layoutParamsDescription = binding.skillDescription.layoutParams
+                layoutParamsDescription.height = 0
+                binding.skillDescription.maxLines = 3
+                binding.skillDescription.layoutParams = layoutParamsDescription
+
+                val layoutParamsCard = binding.skillCard.layoutParams
+                layoutParamsCard.height = startedHeight
+                binding.skillCard.layoutParams = layoutParamsCard
+
+                binding.expandDetails.text = "Details >>"
+                skill.isExpanded = false
+            }
         }
     }
 
     class SkillDiffItemCallback : DiffUtil.ItemCallback<Skill>() {
         override fun areItemsTheSame(oldItem: Skill, newItem: Skill): Boolean = oldItem == newItem
-        override fun areContentsTheSame(oldItem: Skill, newItem: Skill): Boolean = oldItem.id == newItem.id
+        override fun areContentsTheSame(oldItem: Skill, newItem: Skill): Boolean =
+            oldItem.id == newItem.id
     }
 }
