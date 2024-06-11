b846302bb39da0398814e88eb3a571d95e78f1f3
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-11 22:43:06 +0300

    Move fetching data from view model to repository. View Model asked repository for skills.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/SkillsRepositoryImpl.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/SkillsRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.hfad.palamarchuksuperapp.data
 
 import com.hfad.palamarchuksuperapp.domain.models.Skill
 import com.hfad.palamarchuksuperapp.domain.repository.SkillRepository
+import java.util.UUID
 import javax.inject.Inject
 
 class SkillsRepositoryImpl @Inject constructor(
@@ -9,7 +10,98 @@ class SkillsRepositoryImpl @Inject constructor(
 ) : SkillRepository {
 
     override fun getSkillsFromDB(): List<Skill> {
-        return emptyList()
+        val one =
+            Skill(
+                name = "some skills",
+                description = "One, Two, Three",
+                id = UUID.randomUUID()
+            )
+
+        val two = Skill(name = "XML", description = "One, Two, Three", id = UUID.randomUUID())
+        val three = Skill(
+            name = "Recycler View",
+            description = "Paging, DiffUtil, Adapter",
+            id = UUID.randomUUID()
+        )
+
+        val four =
+            Skill(
+                name = "Retrofit",
+                description = "One, Two, Three",
+                id = UUID.randomUUID()
+            )
+
+        val five =
+            Skill(
+                name = "Async",
+                description = "One, Two, Three",
+                id = UUID.randomUUID()
+            )
+
+        val six = Skill(
+            name = "Dependency injections",
+            description = "Multi-module architecture, Component Dependencies, Multibindings, " +
+                    "Components, Scope, Injections" +
+                    " Libraries: Dagger 2, Hilt, ---Kodein, ---Koin",
+            id = UUID.randomUUID()
+        )
+
+        val seven =
+            Skill(
+                name = "RxJava",
+                description = "One, Two, Three",
+                id = UUID.randomUUID()
+            )
+
+        val eight = Skill(
+            name = "To learn",
+            description = "Firebase!!!, GITFLOW, GIT, SOLID, MVVM, MVP, " +
+                    "RESTful API!!!, JSON!, PUSH NOTIFICATIONS!!!, ",
+            id = UUID.randomUUID()
+        )
+
+        val nine = Skill(
+            name = "Recycler View",
+            description = "One, Two, Three",
+            id = UUID.randomUUID()
+        )
+
+        val ten = Skill(name = "Project Pattern", description = "MVVM", id = UUID.randomUUID())
+        val eleven = Skill(
+            name = "Work with Image",
+            description = "FileProvider,Coil",
+            id = UUID.randomUUID()
+        )
+
+        val twelve = Skill(
+            name = "Work with Image",
+            description = "FileProvider,Coil",
+            id = UUID.randomUUID()
+        )
+
+
+        val thirteen = Skill(
+            name = "Work with Image",
+            description = "FileProvider,Coil",
+            id = UUID.randomUUID()
+        )
+
+        val _skillsList = listOf(
+            one,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine,
+            ten,
+            eleven,
+            twelve,
+            thirteen
+        )
+        return _skillsList
     }
 
     override fun deleteSkill(position: Int) {
