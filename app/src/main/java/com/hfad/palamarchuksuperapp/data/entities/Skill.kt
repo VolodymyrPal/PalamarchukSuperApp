b28fef3c63f38016dee17fb4866a6076cfc08687
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-15 23:42:20 +0300

    Adding entity data class Skill for Room database. Package change.

---
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/Skill.kt
@@ -0,0 +1,16 @@
+package com.hfad.palamarchuksuperapp.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+import java.util.UUID
+
+@Entity(tableName = "mySkillsDB")
+data class Skill(
+    @PrimaryKey(autoGenerate = true) val numbering: Int = 0,
+    @ColumnInfo (name = "uniqID") val id: UUID? = null,
+    @ColumnInfo (name = "skillName") val name: String = "",
+    @ColumnInfo (name = "skillDescription") val description: String = "",
+    @ColumnInfo (name = "skillDateStudied") val date: Date = Date(),
+)
