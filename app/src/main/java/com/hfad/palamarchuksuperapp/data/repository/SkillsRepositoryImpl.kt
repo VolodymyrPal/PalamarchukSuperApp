f30f421a9cbecc3195449fae21906d9bed426d80
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-17 23:06:25 +0300

    Creating DummyRepository to test app.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/SkillsRepositoryImpl.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/SkillsRepositoryImpl.kt
@@ -34,3 +34,30 @@ class SkillsRepositoryImpl @Inject constructor(
 
     override suspend fun addSkill(skill: Skill) = skillsDao.addSkill(skill)
 }
+
+class SkillsRepositoryImplDummy : SkillRepository {
+    override fun getSkillsFromDB(): List<Skill> {
+        val one = Skill(name = "some skills", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val two = Skill(name = "XML", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val three = Skill(name = "Recycler View", description = "Paging, DiffUtil, Adapter", uuid = UUID.randomUUID())
+        val four = Skill(name = "Retrofit", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val five = Skill(name = "Async", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val six = Skill(name = "Dependency injections", description = "Multi-module architecture, Component Dependencies, Multibindings, " + "Components, Scope, Injections" + " Libraries: Dagger 2, Hilt, ---Kodein, ---Koin", uuid = UUID.randomUUID())
+        val seven = Skill(name = "RxJava", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val eight = Skill(name = "To learn", description = "Firebase!!!, GITFLOW, GIT, SOLID, MVVM, MVP, " + "RESTful API!!!, JSON!, PUSH NOTIFICATIONS!!!, ",uuid = UUID.randomUUID())
+        val nine = Skill(name = "Recycler View", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val ten = Skill(name = "Project Pattern", description = "MVVM", uuid = UUID.randomUUID())
+        val eleven = Skill(name = "Compose", description = "Composition tracing", uuid = UUID.randomUUID())
+        val twelve = Skill(name = "Work with Image", description = "FileProvider,Coil", uuid = UUID.randomUUID())
+        val thirteen = Skill(name = "Work with Image", description = "FileProvider,Coil", uuid = UUID.randomUUID())
+        val _skillsList = listOf(one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen)
+        return _skillsList
+    }
+
+    override suspend fun deleteSkill(skill: Skill) {
+    }
+
+    override suspend fun addSkill(skill: Skill) {
+
+    }
+}
