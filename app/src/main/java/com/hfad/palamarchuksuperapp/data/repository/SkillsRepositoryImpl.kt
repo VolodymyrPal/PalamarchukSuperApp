6529e9854411b22a63312ee937354c9030d6823f
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-17 23:06:03 +0300

    Establishing skillsDao to Repository.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/SkillsRepositoryImpl.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/SkillsRepositoryImpl.kt
@@ -7,109 +7,30 @@ import java.util.UUID
 import javax.inject.Inject
 
 class SkillsRepositoryImpl @Inject constructor(
-    //private val skillsDataBase: SkillDataBase
+    private val skillsDao: SkillsDao
 ) : SkillRepository {
 
     override fun getSkillsFromDB(): List<Skill> {
-        val one =
-            Skill(
-                name = "some skills",
-                description = "One, Two, Three",
-                id = UUID.randomUUID()
-            )
-
-        val two = Skill(name = "XML", description = "One, Two, Three", id = UUID.randomUUID())
-        val three = Skill(
-            name = "Recycler View",
-            description = "Paging, DiffUtil, Adapter",
-            id = UUID.randomUUID()
-        )
-
-        val four =
-            Skill(
-                name = "Retrofit",
-                description = "One, Two, Three",
-                id = UUID.randomUUID()
-            )
-
-        val five =
-            Skill(
-                name = "Async",
-                description = "One, Two, Three",
-                id = UUID.randomUUID()
-            )
-
-        val six = Skill(
-            name = "Dependency injections",
-            description = "Multi-module architecture, Component Dependencies, Multibindings, " +
-                    "Components, Scope, Injections" +
-                    " Libraries: Dagger 2, Hilt, ---Kodein, ---Koin",
-            id = UUID.randomUUID()
-        )
-
-        val seven =
-            Skill(
-                name = "RxJava",
-                description = "One, Two, Three",
-                id = UUID.randomUUID()
-            )
-
-        val eight = Skill(
-            name = "To learn",
-            description = "Firebase!!!, GITFLOW, GIT, SOLID, MVVM, MVP, " +
-                    "RESTful API!!!, JSON!, PUSH NOTIFICATIONS!!!, ",
-            id = UUID.randomUUID()
-        )
-
-        val nine = Skill(
-            name = "Recycler View",
-            description = "One, Two, Three",
-            id = UUID.randomUUID()
-        )
-
-        val ten = Skill(name = "Project Pattern", description = "MVVM", id = UUID.randomUUID())
-        val eleven = Skill(
-            name = "Compose",
-            description = "Composition tracing",
-            id = UUID.randomUUID()
-        )
-
-        val twelve = Skill(
-            name = "Work with Image",
-            description = "FileProvider,Coil",
-            id = UUID.randomUUID()
-        )
-
-
-        val thirteen = Skill(
-            name = "Work with Image",
-            description = "FileProvider,Coil",
-            id = UUID.randomUUID()
-        )
-
-        val _skillsList = listOf(
-            one,
-            two,
-            three,
-            four,
-            five,
-            six,
-            seven,
-            eight,
-            nine,
-            ten,
-            eleven,
-            twelve,
-            thirteen
-        )
+        val one = Skill(name = "some skills", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val two = Skill(name = "XML", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val three = Skill(name = "Recycler View", description = "Paging, DiffUtil, Adapter", uuid = UUID.randomUUID())
+        val four = Skill(name = "Retrofit", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val five = Skill(name = "Async", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val six = Skill(name = "Dependency injections", description = "Multi-module architecture, Component Dependencies, Multibindings, " + "Components, Scope, Injections" + " Libraries: Dagger 2, Hilt, ---Kodein, ---Koin", uuid = UUID.randomUUID())
+        val seven = Skill(name = "RxJava", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val eight = Skill(name = "To learn", description = "Firebase!!!, GITFLOW, GIT, SOLID, MVVM, MVP, " + "RESTful API!!!, JSON!, PUSH NOTIFICATIONS!!!, ",uuid = UUID.randomUUID())
+        val nine = Skill(name = "Recycler View", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val ten = Skill(name = "Project Pattern", description = "MVVM", uuid = UUID.randomUUID())
+        val eleven = Skill(name = "Compose", description = "Composition tracing", uuid = UUID.randomUUID())
+        val twelve = Skill(name = "Work with Image", description = "FileProvider,Coil", uuid = UUID.randomUUID())
+        val thirteen = Skill(name = "Work with Image", description = "FileProvider,Coil", uuid = UUID.randomUUID())
+        val _skillsList = listOf(one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen)
         return _skillsList
     }
 
-    override fun deleteSkill(position: Int) {
-
+    override suspend fun deleteSkill(skill: Skill) {
+        skillsDao.deleteSkill(skill = skill)
     }
 
-    override fun addSkill(skill: Skill) {
-
-    }
+    override suspend fun addSkill(skill: Skill) = skillsDao.addSkill(skill)
 }
