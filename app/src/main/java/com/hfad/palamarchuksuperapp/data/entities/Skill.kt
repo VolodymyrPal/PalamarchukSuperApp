61b83e705a54d723b0a2b1a9cc7aea57b8e6c62f
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-17 23:01:25 +0300

    Adding converters Class for Skill entity to convert date to long and back.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/Skill.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/Skill.kt
@@ -1,12 +1,17 @@
 package com.hfad.palamarchuksuperapp.data.entities
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import java.util.Date
 import java.util.UUID
 
 @Entity(tableName = "mySkillsDB")
+@TypeConverters (Converters::class)
 data class Skill(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo (name = "uuid") val uuid: UUID? = null,
@@ -14,3 +19,16 @@ data class Skill(
     @ColumnInfo (name = "description") val description: String = "",
     @ColumnInfo (name = "studiedDate") val date: Date = Date(),
 )
+
+class Converters {
+    @RequiresApi(Build.VERSION_CODES.O)
+    @TypeConverter
+    fun fromDateTime(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToDateTime(date: Date?): Long? {
+        return date?.time
+    }
+}
