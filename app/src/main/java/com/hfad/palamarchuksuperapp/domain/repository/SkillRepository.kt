a0d7b4982aed3b521bdd35a07e224e1e830619d4
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-21 16:53:45 +0300

    Removing suspend config from Repository.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/SkillRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/SkillRepository.kt
@@ -4,7 +4,7 @@ import com.hfad.palamarchuksuperapp.data.entities.Skill
 import kotlinx.coroutines.flow.Flow
 
 interface SkillRepository {
-    suspend fun getSkillsFromDB(): List<Skill>
+    fun getSkillsFromDB(): Flow<List<Skill>>
     suspend fun deleteSkill(skill: Skill)
     suspend fun addSkill(skill: Skill)
 }
