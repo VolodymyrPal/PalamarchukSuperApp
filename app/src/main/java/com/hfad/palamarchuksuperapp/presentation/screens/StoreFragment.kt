4e6b2425bb0f1b4906bcf224e1100b4f4e0f0629
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-23 00:13:31 +0300

    Adding new screen (store sample) for Api handling.

---
+++ app/src/main/java/com/hfad/palamarchuksuperapp/presentation/screens/StoreFragment.kt
@@ -0,0 +1,33 @@
+package com.hfad.palamarchuksuperapp.presentation.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hfad.palamarchuksuperapp.databinding.FragmentStoreBinding
+
+class StoreFragment : Fragment() {
+    private var _binding: FragmentStoreBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "_binding = null"
+        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentStoreBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
