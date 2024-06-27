f2c241ee8227e31917f2eb4c3d1ee91b39987aa6
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-27 21:31:47 +0300

    Updating StoreViewModel with using new GenericViewModel.

--- app/src/main/java/com/hfad/palamarchuksuperapp/presentation/viewModels/StoreViewModel.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/presentation/viewModels/StoreViewModel.kt
@@ -8,130 +8,61 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class StoreViewModel @Inject constructor(private val repository: StoreRepository,
-) : ViewModel(), StoreContract {
+class StoreViewModel @Inject constructor(
+    val repository: StoreRepository,
+) : GenericViewModel<List<ProductDomainRW>, StoreViewModel.Event, StoreViewModel.Effect>() {
 
-    private val mutableState = MutableStateFlow(StoreContract.State.Empty)
-    override val state: StateFlow<StoreContract.State<StoreContract.State.Empty>> =
-        mutableState.asStateFlow()
+    sealed class Event : BaseEvent() {
+        object FetchSkills : Event()
+        object OnRefresh : Event()
+        data class ShowToast(val message: String) : Event()
+        data class AddProduct(val product: ProductDomainRW, val quantity: Int = 1) : Event()
+    }
 
-    private val effectFlow = MutableSharedFlow<StoreContract.Effect>()
-    override val effect: SharedFlow<StoreContract.Effect> = effectFlow.asSharedFlow()
+    sealed class Effect: BaseEffect() {
+        object OnBackPressed : Effect()
+        data class ShowToast(val message: String) : Effect()
+        object Vibration : Effect()
+    }
 
-    override fun event(event: StoreContract.Event) {
+    override fun event(event: Event) {
         when (event) {
-            is StoreContract.Event.OnGetNewsList -> {
+            is Event.FetchSkills -> {
+                emitState(emitProcessing = true) { current ->
+                    if (current is State.Error) {
+                        return@emitState current
+                    }
+                    try {
+                        val skills = repository.fetchProducts().first().map { ProductToProductDomainRW.map(it) }
+                        return@emitState if (skills.isNotEmpty()) {
+                            State.Success(data = skills)
+                        } else State.Empty
+                    } catch (e: Exception) {
+                        State.Error(e)
+                    }
+                }
+            }
+
+
+            is Event.OnRefresh -> {
 
             }
-            is StoreContract.Event.OnSetShowFavoriteList -> {
+
+            is Event.ShowToast -> {
 
             }
-            is StoreContract.Event.OnRefresh -> {
+            is Event.AddProduct -> {
+                viewModelScope.launch {
 
-            }
-            is StoreContract.Event.OnBackPressed -> {
-
-            }
-            is StoreContract.Event.ShowToast -> {
-
+                }
             }
         }
     }
 
     fun fetchProducts() {
         viewModelScope.launch {
-            val a = repository.fetchProducts().first().map { ProductToProductDomainRW.map(it) }
+
         }
     }
 }
 
-interface UnidirectionalViewModel<STATE, EVENT, EFFECT> {
-    val state: StateFlow<STATE>
-    val effect: SharedFlow<EFFECT>
-    fun event(event: EVENT)
-}
-
-interface StoreContract :
-    UnidirectionalViewModel<StoreContract.State<*>, StoreContract.Event, StoreContract.Effect> {
-
-    sealed interface State<out T> {
-        data object Processing : State<Nothing>
-
-        data class Success<out T>(val data: T, ) : State<T>
-
-        data object Empty : State<Nothing>
-
-        data class Error(val exception: Throwable) : State<Nothing>
-    }
-
-    sealed class Event {
-        data class OnGetNewsList(val showFavoriteList: Boolean) : Event()
-        data class OnSetShowFavoriteList(val showFavoriteList: Boolean) : Event()
-        object OnRefresh : Event()
-        object OnBackPressed : Event()
-        data class ShowToast(val message: String) : Event()
-    }
-
-    sealed class Effect {
-        object OnBackPressed : Effect()
-        data class ShowToast(val message: String) : Effect()
-        object Vibration : Effect()
-    }
-}
-
-abstract class UIStateViewModel<T> : ViewModel(), StoreContract {
-
-    private val _uiState: MutableStateFlow<RepoResult<T>> = MutableStateFlow(RepoResult.Empty)
-
-    val uiState: StateFlow<RepoResult<T>> =
-        _uiState.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = RepoResult.Empty
-        )
-
-
-    private val mutableState = MutableStateFlow(StoreContract.State.Empty)
-    override val state: StateFlow<StoreContract.State.Empty> =
-        mutableState.asStateFlow()
-
-    private val effectFlow = MutableSharedFlow<StoreContract.Effect>()
-    override val effect: SharedFlow<StoreContract.Effect> =
-        effectFlow.asSharedFlow()
-
-    protected fun emitState(
-        emitProcessing: Boolean,
-        block: suspend (current: RepoResult<T>) -> RepoResult<T>,
-    ): Job =
-        viewModelScope.launch {
-            val current = _uiState.value
-            if (emitProcessing) {
-                emitProcessing()
-            }
-            _uiState.update { block(current) }
-        }
-
-    protected fun emitState(value: RepoResult<T>) {
-        _uiState.update { value }
-    }
-
-    protected suspend fun emitState(value: T?) {
-        if (value == null) {
-            emitEmpty()
-        } else {
-            _uiState.update { RepoResult.Success(value) }
-        }
-    }
-
-    private fun emitEmpty() {
-        _uiState.update { RepoResult.Empty }
-    }
-
-    private fun emitProcessing() {
-        _uiState.update { RepoResult.Processing }
-    }
-
-    protected fun emitFailure(e: Throwable) {
-        _uiState.update { RepoResult.Error(e) }
-    }
-}
