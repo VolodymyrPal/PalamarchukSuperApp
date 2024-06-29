15107c5cbf02ca1b6cbce48e904b573451c5440b
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-29 23:53:27 +0300

    Getting data in Repository with Skill.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/SkillsRepositoryImpl.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/SkillsRepositoryImpl.kt
@@ -4,25 +4,21 @@ import com.hfad.palamarchuksuperapp.data.dao.SkillsDao
 import com.hfad.palamarchuksuperapp.data.entities.Skill
 import com.hfad.palamarchuksuperapp.domain.repository.SkillRepository
 import com.hfad.palamarchuksuperapp.presentation.common.SkillDomainRW
-import com.hfad.palamarchuksuperapp.presentation.common.toDomainRW
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.map
 import java.util.UUID
 import javax.inject.Inject
 
 class SkillsRepositoryImpl @Inject constructor(
-    private val skillsDao: SkillsDao
+    private val skillsDao: SkillsDao,
 ) : SkillRepository {
 
-    override fun getSkillsFromDB(): Flow<List<SkillDomainRW>> {
-        return skillsDao.getAllSkillsFromDB().map {
-            skillsList -> skillsList.map { skill->
-                skill.toDomainRW() }
-        }
+    override fun getSkillsFromDB(): Flow<List<Skill>> {
+        return skillsDao.getAllSkillsFromDB()
     }
 
-    override suspend fun deleteSkill(skill: SkillDomainRW) = skillsDao.deleteSkill(skill = skill.skill)
+    override suspend fun deleteSkill(skill: SkillDomainRW) =
+        skillsDao.deleteSkill(skill = skill.skill)
 
 
     override suspend fun addSkill(skill: SkillDomainRW) = skillsDao.addSkill(skill.skill)
@@ -31,22 +27,63 @@ class SkillsRepositoryImpl @Inject constructor(
 }
 
 class SkillsRepositoryImplForPreview : SkillRepository {
-    override fun getSkillsFromDB(): Flow<List<SkillDomainRW>> {
-        val one = Skill(name = "some skills", description = "One, Two, Three", uuid = UUID.randomUUID())
+    override fun getSkillsFromDB(): Flow<List<Skill>> {
+        val one =
+            Skill(name = "some skills", description = "One, Two, Three", uuid = UUID.randomUUID())
         val two = Skill(name = "XML", description = "One, Two, Three", uuid = UUID.randomUUID())
-        val three = Skill(name = "Recycler View", description = "Paging, DiffUtil, Adapter", uuid = UUID.randomUUID())
-        val four = Skill(name = "Retrofit", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val three = Skill(
+            name = "Recycler View",
+            description = "Paging, DiffUtil, Adapter",
+            uuid = UUID.randomUUID()
+        )
+        val four =
+            Skill(name = "Retrofit", description = "One, Two, Three", uuid = UUID.randomUUID())
         val five = Skill(name = "Async", description = "One, Two, Three", uuid = UUID.randomUUID())
-        val six = Skill(name = "Dependency injections", description = "Multi-module architecture, Component Dependencies, Multibindings, " + "Components, Scope, Injections" + " Libraries: Dagger 2, Hilt, ---Kodein, ---Koin", uuid = UUID.randomUUID())
-        val seven = Skill(name = "RxJava", description = "One, Two, Three", uuid = UUID.randomUUID())
-        val eight = Skill(name = "To learn", description = "Firebase!!!, GITFLOW, GIT, SOLID, MVVM, MVP, " + "RESTful API!!!, JSON!, PUSH NOTIFICATIONS!!!, ",uuid = UUID.randomUUID())
-        val nine = Skill(name = "Recycler View", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val six = Skill(
+            name = "Dependency injections",
+            description = "Multi-module architecture, Component Dependencies, Multibindings, " + "Components, Scope, Injections" + " Libraries: Dagger 2, Hilt, ---Kodein, ---Koin",
+            uuid = UUID.randomUUID()
+        )
+        val seven =
+            Skill(name = "RxJava", description = "One, Two, Three", uuid = UUID.randomUUID())
+        val eight = Skill(
+            name = "To learn",
+            description = "Firebase!!!, GITFLOW, GIT, SOLID, MVVM, MVP, " + "RESTful API!!!, JSON!, PUSH NOTIFICATIONS!!!, ",
+            uuid = UUID.randomUUID()
+        )
+        val nine =
+            Skill(name = "Recycler View", description = "One, Two, Three", uuid = UUID.randomUUID())
         val ten = Skill(name = "Project Pattern", description = "MVVM", uuid = UUID.randomUUID())
-        val eleven = Skill(name = "Compose", description = "Composition tracing", uuid = UUID.randomUUID())
-        val twelve = Skill(name = "Work with Image", description = "FileProvider,Coil", uuid = UUID.randomUUID())
-        val thirteen = Skill(name = "Work with Image", description = "FileProvider,Coil", uuid = UUID.randomUUID())
-        val _skillsList = MutableStateFlow(listOf( one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen))
-        return _skillsList.map { it.map { it.toDomainRW() } }
+        val eleven =
+            Skill(name = "Compose", description = "Composition tracing", uuid = UUID.randomUUID())
+        val twelve = Skill(
+            name = "Work with Image",
+            description = "FileProvider,Coil",
+            uuid = UUID.randomUUID()
+        )
+        val thirteen = Skill(
+            name = "Work with Image",
+            description = "FileProvider,Coil",
+            uuid = UUID.randomUUID()
+        )
+        val _skillsList = MutableStateFlow(
+            listOf(
+                one,
+                two,
+                three,
+                four,
+                five,
+                six,
+                seven,
+                eight,
+                nine,
+                ten,
+                eleven,
+                twelve,
+                thirteen
+            )
+        )
+        return _skillsList
     }
 
     override suspend fun deleteSkill(skill: SkillDomainRW) {
