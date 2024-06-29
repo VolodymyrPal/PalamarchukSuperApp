c25e88896456643fec53eae56e3c0effc9c4b3ca
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-29 23:56:51 +0300

    Updating StoreListAdapter to deal with final data, not flow.

--- app/src/main/java/com/hfad/palamarchuksuperapp/presentation/screens/StoreListAdapter.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/presentation/screens/StoreListAdapter.kt
@@ -5,45 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.hfad.palamarchuksuperapp.R
-import com.hfad.palamarchuksuperapp.data.entities.Product
 import com.hfad.palamarchuksuperapp.databinding.ListItemProductBinding
 import com.hfad.palamarchuksuperapp.presentation.common.ProductDomainRW
-import com.hfad.palamarchuksuperapp.presentation.common.ProductToProductDomainRW
 import com.hfad.palamarchuksuperapp.presentation.viewModels.StoreViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+
 
 class StoreListAdapter(
     private val viewModel: StoreViewModel,
     private val fragmentManager: FragmentManager,
-) : ListAdapter<Product, StoreListAdapter.ProductHolder> (ProductDiffItemCallback()) {
+) : ListAdapter<ProductDomainRW, StoreListAdapter.ProductHolder> (ProductDiffItemCallback()) {
 
-    private var products = emptyList<Product>()
-    private val adapterScope = CoroutineScope(Dispatchers.Main)
+    private val asyncListDiffer = AsyncListDiffer(this, ProductDiffItemCallback())
 
-    fun submitData(productFlow: Flow<List<Product>>) {
-        adapterScope.launch {
-            productFlow.collectLatest { newProducts ->
-                withContext(Dispatchers.Default) {
-                    val diffResult =
-                        DiffUtil.calculateDiff(ProductDiffCallback(products, newProducts))
-                    withContext(Dispatchers.Main) {
-                        products = newProducts
-                        diffResult.dispatchUpdatesTo(this@StoreListAdapter)
-                    }
-                }
-            }
-        }
+    fun setData(productList: List<ProductDomainRW>) {
+        asyncListDiffer.submitList(productList)
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
         val binding =
@@ -52,10 +35,10 @@ class StoreListAdapter(
     }
 
     override fun onBindViewHolder(holder: ProductHolder, position: Int) {
-        holder.bind(ProductToProductDomainRW.map(products[position]))
+        holder.bind(asyncListDiffer.currentList[position])
     }
 
-    override fun getItemCount(): Int = products.size
+    override fun getItemCount(): Int = asyncListDiffer.currentList.size
 
     class ProductHolder(
         private val binding: ListItemProductBinding,
@@ -97,91 +80,16 @@ class StoreListAdapter(
         }
     }
 
-    private class ProductDiffCallback(
-        private val oldList: List<Product>,
-        private val newList: List<Product>,
-    ) : DiffUtil.Callback() {
-        override fun getOldListSize() = oldList.size
-        override fun getNewListSize() = newList.size
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-            oldList[oldItemPosition].id == newList[newItemPosition].id
+    class ProductDiffItemCallback : DiffUtil.ItemCallback<ProductDomainRW>() {
+        override fun areItemsTheSame(
+            oldItem: ProductDomainRW,
+            newItem: ProductDomainRW,
+        ): Boolean = oldItem == newItem
 
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-            oldList[oldItemPosition] == newList[newItemPosition]
+        override fun areContentsTheSame(
+            oldItem: ProductDomainRW,
+            newItem: ProductDomainRW,
+        ): Boolean =
+            oldItem.product.id == newItem.product.id
     }
-}
-
-//
-//private val asyncListDiffer = AsyncListDiffer(this, ProductDiffItemCallback())
-//
-//fun setData(productList: List<ProductDomainRW>) {
-//    asyncListDiffer.submitList(productList)
-//}
-//
-//override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
-//    val binding =
-//        ListItemProductBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-//    return ProductHolder(binding, viewModel, fragmentManager)
-//}
-//
-//override fun getItemCount(): Int = asyncListDiffer.currentList.size
-//
-//override fun onBindViewHolder(holder: ProductHolder, position: Int) {
-//    val product = asyncListDiffer.currentList[position]
-//    holder.bind(product)
-//}
-//
-//class ProductHolder(
-//    private val binding: ListItemProductBinding,
-//    private val viewModel: StoreViewModel,
-//    private val parentFragmentManager: FragmentManager,
-//) : RecyclerView.ViewHolder(binding.root) {
-//
-//    fun bind(productDomainRW: ProductDomainRW) {
-//
-//        binding.apply {
-//            productName.text = "${productDomainRW.product.title}"
-//            productPrice.text = "${productDomainRW.product.price.toString()}$"
-//            productPriceDiscounted.text = "${(productDomainRW.product.price * 0.5)}$"
-//            quantity.text = productDomainRW.quantity.toString()
-//            productImage.load(productDomainRW.product.images.urls.getOrNull(0)) {
-//                placeholder(R.drawable.lion_jpg_21)
-//                this.error(R.drawable.lion_jpg_21)
-//            }
-//
-//            quantityPlusCard.visibility = View.VISIBLE
-//            quantityMinusCard.visibility = View.VISIBLE
-//            quantity.visibility = View.VISIBLE
-//
-//            quantityPlus.setOnClickListener {
-//                viewModel.event(event = StoreViewModel.Event.AddProduct(product = productDomainRW))
-//                quantity.text = productDomainRW.quantity.toString()
-//                quantityPlusCard.visibility = View.VISIBLE
-//                quantityMinusCard.visibility = View.VISIBLE
-//                quantity.visibility = View.VISIBLE
-//            }
-//            quantityMinus.setOnClickListener {
-//                quantityPlusCard.visibility = View.VISIBLE
-//                quantityMinusCard.visibility = View.VISIBLE
-//                quantity.text = ""
-//                quantity.visibility = View.VISIBLE
-//            }
-//            productPrice.paintFlags =
-//                productPriceDiscounted.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-//        }
-//    }
-//}
-
-
-class ProductDiffItemCallback : DiffUtil.ItemCallback<Product>() {
-    override fun areItemsTheSame(
-        oldItem: Product,
-        newItem: Product,
-    ): Boolean = oldItem == newItem
-
-    override fun areContentsTheSame(
-        oldItem: Product,
-        newItem: Product,
-    ): Boolean =
-        oldItem.id == newItem.id
 }
