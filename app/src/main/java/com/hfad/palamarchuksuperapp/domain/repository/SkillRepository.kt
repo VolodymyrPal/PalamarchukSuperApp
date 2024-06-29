677f62427dab8f720c27026afda7e8d24b7f5caa
palamarchukhome <palamarchuk.office@gmail.com>
2024-06-29 23:49:18 +0300

    Back from List to Flow in Repository Skills.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/SkillRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/SkillRepository.kt
@@ -1,10 +1,11 @@
 package com.hfad.palamarchuksuperapp.domain.repository
 
+import com.hfad.palamarchuksuperapp.data.entities.Skill
 import com.hfad.palamarchuksuperapp.presentation.common.SkillDomainRW
 import kotlinx.coroutines.flow.Flow
 
 interface SkillRepository {
-    fun getSkillsFromDB(): Flow<List<SkillDomainRW>>
+    fun getSkillsFromDB(): Flow<List<Skill>>
     suspend fun deleteSkill(skill: SkillDomainRW)
     suspend fun addSkill(skill: SkillDomainRW)
     suspend fun updateSkill(skill: SkillDomainRW)
