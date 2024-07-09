34cfab44a1c0f5e8807279be30a3070964578c14
palamarchukhome <palamarchuk.office@gmail.com>
2024-07-09 21:37:09 +0300

    Testing how API works in MainActivity.

--- app/src/main/java/com/hfad/palamarchuksuperapp/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/presentation/screens/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.hfad.palamarchuksuperapp.presentation.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import com.hfad.palamarchuksuperapp.R
 import com.hfad.palamarchuksuperapp.appComponent
+import com.hfad.palamarchuksuperapp.data.services.PlatziApi
 import com.hfad.palamarchuksuperapp.domain.models.AppImages
 import com.hfad.palamarchuksuperapp.databinding.ActivityMainBinding
 import com.hfad.palamarchuksuperapp.domain.models.AppVibrator
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
@@ -16,8 +20,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    @Inject lateinit var mainImage: AppImages.MainImage
-    @Inject lateinit var vibe: AppVibrator
+    @Inject
+    lateinit var mainImage: AppImages.MainImage
+
+    @Inject
+    lateinit var vibe: AppVibrator
+
+    @Inject
+    lateinit var platziApi: PlatziApi
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +37,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         applicationContext.appComponent.inject(this)
+
+        lifecycleScope.launch {
+            val response = platziApi.fetchProducts()
+            Log.d("My API: ", "$response")
+        }
 
         val badge = binding.bottomNavigation.getOrCreateBadge(R.id.bnav_settings)
         badge.isVisible = true
