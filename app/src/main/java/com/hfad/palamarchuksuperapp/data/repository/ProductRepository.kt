9327f7f8697892494166f8d1f807214f89515da4
palamarchukhome <palamarchuk.office@gmail.com>
2024-07-09 21:34:54 +0300

    Creating new ProductRepository with PlatziApi interface.

---
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/ProductRepository.kt
@@ -0,0 +1,25 @@
+package com.hfad.palamarchuksuperapp.data.repository
+
+import com.hfad.palamarchuksuperapp.data.services.PlatziApi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Inject
+
+class ProductRepository @Inject constructor() : PlatziApi {
+    private var platziApi: PlatziApi
+
+    init {
+        val httpClient: OkHttpClient = OkHttpClient.Builder().build()
+        val retrofit: Retrofit =
+            Retrofit.Builder().baseUrl("https://api.escuelajs.co/api/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
+                .build()
+        platziApi = retrofit.create<PlatziApi>()
+    }
+
+    override suspend fun fetchProducts() = platziApi.fetchProducts()
+
+}
