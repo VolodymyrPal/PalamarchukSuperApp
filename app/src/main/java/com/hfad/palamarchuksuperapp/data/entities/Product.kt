7073a17868b8863a84d9fb430adadbe1141877cf
palamarchukhome <palamarchuk.office@gmail.com>
2024-07-23 19:14:31 +0300

    Add Converter for Product class for future implementing in Room.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/Product.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/Product.kt
@@ -1,10 +1,18 @@
 package com.hfad.palamarchuksuperapp.data.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.hfad.palamarchuksuperapp.data.database.DATABASE_STORE_NAME
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+@Entity(tableName = DATABASE_STORE_NAME)
 @JsonClass(generateAdapter = true)
+@TypeConverters(ProductConverters::class)
 data class Product(
+    @PrimaryKey
     @Json(name="id")
     val id: Int = 0,
     @Json(name="title")
@@ -20,6 +28,18 @@ data class Product(
     @Json(name = "rating")
     val rating: ProductRating = ProductRating()
 )
+
+class ProductConverters {
+    @TypeConverter
+    fun fromProductRating(rating: ProductRating): String {
+        return "${rating.rate},${rating.count}"
+    }
+    @TypeConverter
+    fun toProductRating (rating: String): ProductRating {
+        val parts = rating.split(",")
+        return ProductRating(parts[0].toDouble(), parts[1].toInt())
+    }
+}
 
 @JsonClass (generateAdapter = true)
 data class ProductRating (
