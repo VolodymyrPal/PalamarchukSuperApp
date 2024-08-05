bcbe9d82d3995db8cec1f1d3dc77879f424226ae
palamarchukhome <palamarchuk.office@gmail.com>
2024-08-05 17:58:10 +0300

    Move viewModel as parameter for better testing.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/compose/StoreScreen.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/compose/StoreScreen.kt
@@ -127,11 +127,11 @@ import kotlinx.coroutines.launch
 fun StoreScreen(
     modifier: Modifier = Modifier,
     navController: Navigation?,
+    viewModel: StoreViewModel =
+        viewModel(factory = LocalContext.current.appComponent.viewModelFactory()),
 ) {
     val scrollBehavior =
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
-    val viewModel: StoreViewModel =
-        viewModel(factory = LocalContext.current.appComponent.viewModelFactory())
 
 
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
