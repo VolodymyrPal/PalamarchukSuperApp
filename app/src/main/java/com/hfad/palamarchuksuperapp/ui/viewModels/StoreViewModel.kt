7fef4515eb8d3f0f3362bf06a45b8580a36e7c72
palamarchukhome <palamarchuk.office@gmail.com>
2024-08-16 18:04:44 +0300

    Emitting loading param for states, small changes.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/viewModels/StoreViewModel.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/viewModels/StoreViewModel.kt
@@ -3,26 +3,16 @@ package com.hfad.palamarchuksuperapp.ui.viewModels
 import android.util.Log
 import androidx.compose.runtime.Stable
 import androidx.lifecycle.viewModelScope
-import com.hfad.palamarchuksuperapp.data.entities.Product
 import com.hfad.palamarchuksuperapp.data.repository.ProductRepository
 import com.hfad.palamarchuksuperapp.domain.repository.StoreRepository
 import com.hfad.palamarchuksuperapp.ui.common.ProductDomainRW
 import com.hfad.palamarchuksuperapp.ui.common.toProductDomainRW
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -38,52 +28,6 @@ class StoreViewModel @Inject constructor(
 
     init {
         event(Event.FetchSkills)
-    }
-
-    private fun getProductsAsFlow(): Flow<List<Product>> = flow {
-        val products = apiRepository.fetchProducts()
-        emit(products)
-    }.flowOn(Dispatchers.IO)
-
-    private var _products: Flow<Async<List<ProductDomainRW>>> =
-        combine(
-            getProductsAsFlow(), _isLoading
-        ) { products, _ ->
-            products.map { it.toProductDomainRW() }
-        }
-            .map { Async.Success(it) }
-            .catch { Async.Error(it) }
-
-    val myState: StateFlow<MyState> = combine(
-        _isLoading, _products
-    ) { isLoading, products ->
-        when (products) {
-            is Async.Loading -> {
-                MyState(isLoading)
-            }
-
-            is Async.Error -> {
-                MyState(
-                    loading = isLoading,  //TODO for testing (in product must be - false)
-                    message = products.errorMessage.message!!)
-            }
-
-            is Async.Success -> {
-                MyState(
-                    loading = isLoading, //TODO for testing (in product must be - false)
-                    items = products.data,
-                )
-            }
-        }
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(),
-        initialValue = MyState(loading = true)
-    )
-
-    private fun updateBasketList(state: State.Success<List<ProductDomainRW>>) {
-        val updatedBasketList = state.data.filter { it.quantity > 0 }
-        _basketList.update { updatedBasketList }
     }
 
     private val _basketList = MutableStateFlow<List<ProductDomainRW>>(emptyList())
@@ -115,10 +59,7 @@ class StoreViewModel @Inject constructor(
                 }
             }
 
-
             is Event.OnRefresh -> {
-            //    refreshProducts()
-            //refresh()
                 fetchProducts()
             }
 
@@ -128,58 +69,10 @@ class StoreViewModel @Inject constructor(
 
             is Event.AddProduct -> {
                 addProduct(event.product, event.quantity)
-                //refresh()
             }
 
             is Event.SetItemToBasket -> {
-                //setItemToBasket(event.product, event.quantity)
-                //refresh()
-            }
-        }
-    }
-
-    fun refresh() {
-        _isLoading.value = true
-        viewModelScope.launch {
-            try {
-                apiRepository.fetchProducts()
-            } catch (e: Exception) {
-                Log.d("Exception in refresh", "event: ${e.message}")
-            }
-            delay(1500)
-            _isLoading.value = false
-        }
-    }
-
-
-    private fun refreshProducts() {
-        viewModelScope.launch {
-            _isLoading.value = true
-
-            try {
-                val newProducts = apiRepository.fetchProducts()
-
-                _products = combine(
-                    flowOf(newProducts), _isLoading
-                ) { products, _ ->
-                    products.map { it.toProductDomainRW() }
-                }
-                    .map { Async.Success(it) }
-                    .stateIn(
-                        scope = viewModelScope,
-                        started = SharingStarted.WhileSubscribed(),
-                        initialValue = Async.Loading
-                    )
-            } catch (e: Exception) {
-                _products = flowOf(Async.Error(e))
-                    .stateIn(
-                        scope = viewModelScope,
-                        started = SharingStarted.WhileSubscribed(),
-                        initialValue = Async.Error(e)
-                    )
-            } finally {
-                delay(1500)
-                _isLoading.value = false
+                setItemToBasket(event.product, event.quantity)
             }
         }
     }
@@ -224,10 +117,21 @@ class StoreViewModel @Inject constructor(
                 val skills = products.map { it.toProductDomainRW() }
                 delay(2000)
                 if (Random.nextFloat() > 0.5) throw Exception("Something went wrong") //TODO
-                return@emitState if (skills.isNotEmpty()) State.Success(data = skills) else State.Empty
+                return@emitState if (skills.isNotEmpty()) State.Success(data = skills) else State.Empty (loading = false)
             } catch (e: Exception) {
                 State.Error(e)
             }
         }
     }
+
+//    private fun getProductsAsFlow(): Flow<List<Product>> = flow {
+//        val products = apiRepository.fetchProducts()
+//        emit(products)
+//    }.flowOn(Dispatchers.IO)
+//
+//    private fun updateBasketList(state: State.Success<List<ProductDomainRW>>) {
+//        val updatedBasketList = state.data.filter { it.quantity > 0 }
+//        _basketList.update { updatedBasketList }
+//    }
+
 }
