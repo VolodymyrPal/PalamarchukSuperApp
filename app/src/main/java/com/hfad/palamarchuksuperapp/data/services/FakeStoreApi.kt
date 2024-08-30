243974b125ace9e4140ce99eeed0b37697c30abf
palamarchukhome <palamarchuk.office@gmail.com>
2024-08-30 22:06:22 +0300

    Now i'll get ProductDomainRW from API with Interface.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/services/FakeStoreApi.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/services/FakeStoreApi.kt
@@ -1,9 +1,11 @@
 package com.hfad.palamarchuksuperapp.data.services
 
 import com.hfad.palamarchuksuperapp.data.entities.Product
+import com.hfad.palamarchuksuperapp.ui.common.ProductDomainRW
 import retrofit2.http.GET
 
 interface FakeStoreApi {
     @GET ("products")
     suspend fun fetchProducts(): List<Product>
+    suspend fun getProductsDomainRw(): List<ProductDomainRW>
 }
