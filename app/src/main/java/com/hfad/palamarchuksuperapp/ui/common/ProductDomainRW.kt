2e96e8a562b90b36ba3148f1e1aa7496e4d79733
palamarchukhome <palamarchuk.office@gmail.com>
2024-08-30 22:08:51 +0300

    Make my ProductDomainRW as Entity class for Room database.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/common/ProductDomainRW.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/common/ProductDomainRW.kt
@@ -1,21 +1,38 @@
 package com.hfad.palamarchuksuperapp.ui.common
 
-import androidx.compose.runtime.Immutable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.hfad.palamarchuksuperapp.data.database.DATABASE_MAIN_ENTITY_PRODUCT
 import com.hfad.palamarchuksuperapp.data.entities.Product
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
 
-@Immutable
+@Entity(tableName = DATABASE_MAIN_ENTITY_PRODUCT)
+@JsonClass(generateAdapter = true)
+@TypeConverters(ProductDomainRWConverters::class)
 data class ProductDomainRW (
-    val product: Product,
-    val quantity: Int = 0,
-    val liked: Boolean = false,
+    @PrimaryKey @Json(name="id") val id : Int = 0,
+    @Json(name="product") val product: Product,
+    @Json(name = "quantity") val quantity: Int = 0,
+    @Json(name = "liked") val liked: Boolean = false,
 )
 
-object ProductToProductDomainRW : Mapper<Product, ProductDomainRW> {
-    override fun map(from: Product) = ProductDomainRW (
-        product = from,
-    )
+class ProductDomainRWConverters {
+
+    @TypeConverter
+    fun fromProduct(product: Product): String {
+        return Moshi.Builder().build().adapter(Product::class.java).toJson(product)
+    }
+    @TypeConverter
+    fun toProduct (productDomainRW: String): Product {
+        return Moshi.Builder().build().adapter(Product::class.java).fromJson(productDomainRW)!!
+    }
 }
 
 fun Product.toProductDomainRW() = ProductDomainRW(
     product = this,
+    id = this.id
 )
