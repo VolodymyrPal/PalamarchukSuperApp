bccb107dcb4129bba7ff97ebc92702d3f7e299f2
palamarchukhome <palamarchuk.office@gmail.com>
2024-08-30 22:14:54 +0300

    Change StoreRepository interface to implement in my code.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
@@ -1,13 +1,12 @@
 package com.hfad.palamarchuksuperapp.domain.repository
 
 import com.hfad.palamarchuksuperapp.data.entities.Product
+import com.hfad.palamarchuksuperapp.ui.common.ProductDomainRW
 import kotlinx.coroutines.flow.Flow
 
 interface StoreRepository {
-    fun fetchProducts(): Flow<List<Product>>
-    suspend fun deleteProduct(product: Product)
-    suspend fun addProduct(product: Product)
+    val fetchProductsAsFlowFromDB: Flow<List<ProductDomainRW>>
     suspend fun updateProduct(product: Product)
-    fun fetchProductsTest() : Flow<List<Product>>
     suspend fun refreshProducts()
+    suspend fun upsertAll(products: List<ProductDomainRW>)
 }
