37ebd7e32ef334277299b26341024601ad490180
palamarchukhome <palamarchuk.office@gmail.com>
2024-08-31 17:07:30 +0300

    Adding correct funs for StoreRepository

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
@@ -3,10 +3,13 @@ package com.hfad.palamarchuksuperapp.domain.repository
 import com.hfad.palamarchuksuperapp.data.entities.Product
 import com.hfad.palamarchuksuperapp.ui.common.ProductDomainRW
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 
 interface StoreRepository {
     val fetchProductsAsFlowFromDB: Flow<List<ProductDomainRW>>
+    val errorFlow : MutableStateFlow<Exception?>
     suspend fun updateProduct(product: Product)
-    suspend fun refreshProducts()
+    suspend fun hardRefreshProducts()
+    suspend fun softRefreshProducts()
     suspend fun upsertAll(products: List<ProductDomainRW>)
 }
