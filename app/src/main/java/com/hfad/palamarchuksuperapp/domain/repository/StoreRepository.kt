a7d0a37417ad82fc7adc722b4b7f895bdcd6c715
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-01 13:24:11 +0300

    Passing entity of database to update it.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 interface StoreRepository {
     val fetchProductsAsFlowFromDB: Flow<List<ProductDomainRW>>
     val errorFlow : MutableStateFlow<Exception?>
-    suspend fun updateProduct(product: Product)
+    suspend fun updateProduct(product: ProductDomainRW)
     suspend fun hardRefreshProducts()
     suspend fun softRefreshProducts()
     suspend fun upsertAll(products: List<ProductDomainRW>)
