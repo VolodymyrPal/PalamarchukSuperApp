b0c5c50888d0d5656edf17725b38ced4953e0984
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-03 19:32:40 +0300

    New StoreViewModel

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/viewModels/StoreViewModel.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/viewModels/StoreViewModel.kt
@@ -1,20 +1,15 @@
 package com.hfad.palamarchuksuperapp.ui.viewModels
 
-import android.util.Log
 import androidx.compose.runtime.Stable
 import androidx.lifecycle.viewModelScope
 import com.hfad.palamarchuksuperapp.data.repository.FakeStoreApiRepository
 import com.hfad.palamarchuksuperapp.domain.repository.StoreRepository
 import com.hfad.palamarchuksuperapp.ui.common.ProductDomainRW
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -24,44 +19,36 @@ class StoreViewModel @Inject constructor(
     private val apiRepository: FakeStoreApiRepository,
 ) : GenericViewModel<List<ProductDomainRW>, StoreViewModel.Event, StoreViewModel.Effect>() {
 
-    override suspend fun getData(): suspend () -> List<ProductDomainRW> {
-        return apiRepository::getProductsDomainRw
-    }
+    override val _dataFlow = repository.fetchProductsAsFlowFromDB
 
-    override val dataFlow =
-        repository.fetchProductsAsFlowFromDB.catch { Log.d("TAG", "getDataFlow: $it") }
+    override val uiState: StateFlow<State<List<ProductDomainRW>>>
+        get() = combine(_dataFlow, _errorFlow, _loading) { data, error, loading ->
+            State(
+                items = data,
+                error = error,
+                loading = loading
+            )
+        }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = State(loading = true)
+            )
+            .also {
+                viewModelScope.launch {
+                    repository.upsertAll(apiRepository.getProductsDomainRw())
+                    _loading.value = false
+                }
+            }
 
-    val myFlow = combine(
-        uiState, repository.errorFlow, dataFlow
-    ) { state , error, data ->
-        when (state) {
-            is State.Success -> State.Success(items = data, error = error)
-            else -> State.Empty(loading = true)
-        }
-        if (error != null) {
-            event(Event.ShowToast(error.message ?: "Error"))
-            emitFailure(error)
-            state
-        } else {
-            state
-        }
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5_000),
-        initialValue = State.Empty(loading = true)
-    ).also {
-        viewModelScope.launch {
-            repository.upsertAll(apiRepository.getProductsDomainRw())
-        }
-    }
-
-    override suspend fun getDataFlow(): Flow<List<ProductDomainRW>> =
-        repository.fetchProductsAsFlowFromDB
 
     val baskList = uiState.map { state ->
-        when (state) {
-            is State.Success -> state.items.filter { it.quantity > 0 }
-            else -> emptyList()
+        if (!state.items.isNullOrEmpty()) {
+            state.items.filter {
+                it.quantity > 0
+            }
+        } else {
+            emptyList()
         }
     }.stateIn(
         scope = viewModelScope,
@@ -69,20 +56,14 @@ class StoreViewModel @Inject constructor(
         initialValue = emptyList()
     )
 
-    private val _basketList = MutableStateFlow<List<ProductDomainRW>>(emptyList())
-    val basketList = _basketList.stateIn(
-        initialValue = emptyList(),
-        scope = viewModelScope,
-        started = SharingStarted.Eagerly
-    )
-
     sealed class Event : BaseEvent {
         object FetchSkills : Event()
         object OnSoftRefresh : Event()
         object OnHardRefresh : Event()
         data class ShowToast(val message: String) : Event()
-        data class AddProduct(val productId: Int, val quantity: Int = 1) : Event()
-        data class SetItemToBasket(val productId: Int, val quantity: Int = 1) : Event()
+        data class AddProduct(val productDomainRW: ProductDomainRW, val quantity: Int = 1) : Event()
+        data class SetItemToBasket(val productDomainRW: ProductDomainRW, val quantity: Int = 1) :
+            Event()
     }
 
     sealed class Effect : BaseEffect {
@@ -95,7 +76,7 @@ class StoreViewModel @Inject constructor(
     override fun event(event: Event) {
         when (event) {
             is Event.FetchSkills -> {
-                viewModelScope.launch { emitSoftRefresh() }
+                viewModelScope.launch { repository.softRefreshProducts() }
             }
 
             is Event.OnSoftRefresh -> {
@@ -111,52 +92,25 @@ class StoreViewModel @Inject constructor(
             }
 
             is Event.AddProduct -> {
-                addProduct(event.productId, event.quantity)
+                addProduct(event.productDomainRW, event.quantity)
             }
 
             is Event.SetItemToBasket -> {
-                setItemToBasket(event.productId, event.quantity)
+                setItemToBasket(event.productDomainRW, event.quantity)
             }
         }
     }
 
-    private fun updateBasketList(state: State.Success<List<ProductDomainRW>>) {
-        val updatedBasketList = state.items.filter { it.quantity > 0 }
-        _basketList.update { updatedBasketList }
-    }
-
-
-    private fun setItemToBasket(productId: Int, quantity: Int = 1) {
-        try {
-            viewModelScope.launch {
-                val newSkills =
-                    ((uiState.first() as State.Success).items!!.toMutableList()) //uiState.first() as State.Success).items.toMutableList()
-                val foundProduct = newSkills.find { it.product.id == productId }
-                newSkills.indexOf(foundProduct).let {
-                    newSkills[it] = newSkills[it].copy(quantity = quantity)
-                    emitState(newSkills)
-                }
-            }
-        } catch (e: Exception) {
-            Log.d("Exception in SetItemToBasket", "event: ${e.message}")
-        }
-    }
-
-    private fun addProduct(productId: Int, quantity: Int = 1) {
+    private fun setItemToBasket(productDomainRW: ProductDomainRW, quantity: Int = 1) {
         viewModelScope.launch {
-            val newSkills =
-                (uiState.first() as State.Success).items.toMutableList()  //uiState.first() as State.Success).items.toMutableList()
-            val skill = newSkills.find { it.product.id == productId }
-            newSkills.indexOf(skill).let {
-                var newQuantity = 0
-                if (newSkills[it].quantity + skill?.quantity!! >= 0) {
-                    newQuantity = newSkills[it].quantity + quantity
-                }
-                newSkills[it] =
-                    newSkills[it].copy(quantity = newQuantity)
-                emitState(newSkills)
-                //(data as State.Success<*>).items = newSkills
-            }
+            repository.updateProduct(productDomainRW.copy(quantity = quantity))
+        }
+    }
+
+    private fun addProduct(productDomainRW: ProductDomainRW, quantity: Int = 1) {
+        viewModelScope.launch {
+            val newQuantity = productDomainRW.quantity + quantity
+            repository.updateProduct(productDomainRW.copy(quantity = if (newQuantity < 0) 0 else newQuantity))
         }
     }
 }
