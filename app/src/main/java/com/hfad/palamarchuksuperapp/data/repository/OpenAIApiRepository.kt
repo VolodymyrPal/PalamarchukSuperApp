6640476a40138de33026216edcb5373fda38d18e
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-04 22:33:37 +0300

    Create repository for Open AI API.

---
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/OpenAIApiRepository.kt
@@ -0,0 +1,45 @@
+package com.hfad.palamarchuksuperapp.data.repository
+
+import com.hfad.palamarchuksuperapp.data.services.GPTRequest
+import com.hfad.palamarchuksuperapp.data.services.OpenAiAPI
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+@Suppress("MaxLineLength")
+class OpenAIApiRepository @Inject constructor() : OpenAiAPI {
+        private var openAiAPI: OpenAiAPI
+
+        init {
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+            val httpClient: OkHttpClient = OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .build()
+            val retrofit: Retrofit =
+                Retrofit.Builder().baseUrl("https://api.openai.com/")
+                    .addConverterFactory(MoshiConverterFactory.create(moshi).asLenient())  // TODO asLenient good only for testing, not production
+                    .client(httpClient)
+                    .build()
+            openAiAPI = retrofit.create<OpenAiAPI>()
+        }
+
+    override fun getGPTResponse(
+        contentType: String,
+        apiKey: String,
+        body: GPTRequest,
+    ): Call<String> {
+        return openAiAPI.getGPTResponse(contentType, apiKey, body)
+    }
+
+
+}
