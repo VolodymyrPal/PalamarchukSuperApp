b03a32e5cf7b8951a6783766b69d003d69cf1801
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-06 20:04:27 +0300

    Get error with "type", coz it is descriminant in Ktor.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/services/OpenAiAPI.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/services/OpenAiAPI.kt
@@ -1,6 +1,7 @@
 package com.hfad.palamarchuksuperapp.data.services
 
-import com.squareup.moshi.JsonClass
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Header
@@ -15,31 +16,94 @@ interface OpenAiAPI {
     ): Call<String>
 }
 
-@JsonClass(generateAdapter = true)
+@Serializable
 data class GPTRequest(
     val model: String = "gpt-4o-mini",
     val messages: List<GPTRequestMessage>,
     val temperature: Double = 0.7,
 )
 
-@JsonClass(generateAdapter = true)
+@Serializable
 data class GPTRequestMessage (
     val role: String,
-    val content: String
+    val content: TextMessage
+)
+
+@Serializable
+data class TextMessage (
+    val content: String,
+    val type: String = "text"
 )
 
 
 
-data class GPTRespone(
-    val id: String,
-    val objectG: String,
-    val created: Long,
-    val model: String,
-    val choices: List<Choice>,
-    val usage: Usage
+
+
+@Serializable
+data class GPTRequestImage(
+    val model: String = "gpt-4o-mini",
+    val messages: List<GPTRequestImageMessage>,
+    val temperature: Double = 0.7,
 )
 
-//data class OpenAiRequest (
-//    model: String,
-//    messages: List<OpenAiMessage>
-//)
+@Serializable
+data class GPTRequestImageMessage (
+    val role: String,
+    val content: List<ImageMessage>
+)
+
+@Serializable
+data class ImageMessage (
+    val image_url: Image,
+    val type: String = "text"
+)
+
+@Serializable
+data class Image (
+    val url: String
+)
+
+
+
+
+
+@Serializable
+data class GptRequested(
+    val model: String = "gpt-4o-mini",
+    val messages: List<RequestRole>,
+//    val temperature: Double = 0.7,
+)
+
+//@Serializable
+//@JsonClassDiscriminator("typeKtor")
+//sealed class RoleRequest
+
+@Serializable
+data class RequestRole(
+    val role: String,
+    val content: List<ContentRequest>,
+    val maxTokens: Int = 50
+) // : RoleRequest()
+
+@Serializable
+sealed class ContentRequest
+
+@Serializable
+data class ImageMessageRequest (
+    @SerialName("type")
+    val typeImage: String = "image_url",
+    @SerialName("image_url")
+    val image_url: ImageRequest
+) : ContentRequest()
+
+@Serializable
+data class ImageRequest (
+    val url: String
+)
+
+@Serializable
+data class MessageRequest (
+    @SerialName("type")
+    val typeText : String = "text",
+    val text : String
+) : ContentRequest()
