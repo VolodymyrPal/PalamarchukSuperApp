b032598701f35123cafad6834757e423e325f5ef
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-07 22:10:17 +0300

    Update OpenAiApi to serialize JSON to OPEN API with sealed class

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/services/OpenAiAPI.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/services/OpenAiAPI.kt
@@ -2,81 +2,13 @@ package com.hfad.palamarchuksuperapp.data.services
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.POST
-
-interface OpenAiAPI {
-    @POST("v1/chat/completions")
-    fun getGPTResponse(
-        @Header("Content-Type") contentType: String,
-        @Header("Authorization") apiKey: String,
-        @Body body: GPTRequest
-    ): Call<String>
-}
-
-@Serializable
-data class GPTRequest(
-    val model: String = "gpt-4o-mini",
-    val messages: List<GPTRequestMessage>,
-    val temperature: Double = 0.7,
-)
-
-@Serializable
-data class GPTRequestMessage (
-    val role: String,
-    val content: TextMessage
-)
-
-@Serializable
-data class TextMessage (
-    val content: String,
-    val type: String = "text"
-)
-
-
-
-
-
-@Serializable
-data class GPTRequestImage(
-    val model: String = "gpt-4o-mini",
-    val messages: List<GPTRequestImageMessage>,
-    val temperature: Double = 0.7,
-)
-
-@Serializable
-data class GPTRequestImageMessage (
-    val role: String,
-    val content: List<ImageMessage>
-)
-
-@Serializable
-data class ImageMessage (
-    val image_url: Image,
-    val type: String = "text"
-)
-
-@Serializable
-data class Image (
-    val url: String
-)
-
-
-
-
 
 @Serializable
 data class GptRequested(
     val model: String = "gpt-4o-mini",
     val messages: List<RequestRole>,
-//    val temperature: Double = 0.7,
+    val temperature: Double = 0.7,
 )
-
-//@Serializable
-//@JsonClassDiscriminator("typeKtor")
-//sealed class RoleRequest
 
 @Serializable
 data class RequestRole(
@@ -100,10 +32,3 @@ data class ImageMessageRequest (
 data class ImageRequest (
     val url: String
 )
-
-@Serializable
-data class MessageRequest (
-    @SerialName("type")
-    val typeText : String = "text",
-    val text : String
-) : ContentRequest()
