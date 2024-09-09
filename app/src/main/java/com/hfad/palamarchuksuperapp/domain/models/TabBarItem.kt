823816e064af4768b50d331ccccd0d1c76c478f5
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-09 19:22:28 +0300

    Nav TabBarItem has route and passing type as A.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/TabBarItem.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/TabBarItem.kt
@@ -2,8 +2,9 @@ package com.hfad.palamarchuksuperapp.domain.models
 
 import androidx.compose.ui.graphics.painter.Painter
 
-data class TabBarItem(
+data class TabBarItem <T: Any>(
     val title: String,
+    val route: T,
     val selectedIcon: Painter,
     val unselectedIcon: Painter,
     var badgeAmount: Int? = null,
