c8ff979c30b0168a7f8239e462b06e6692bd88bd
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-11 18:20:56 +0300

    I wish to get from Repository ready to use data class.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/SkillRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/SkillRepository.kt
@@ -1,11 +1,10 @@
 package com.hfad.palamarchuksuperapp.domain.repository
 
-import com.hfad.palamarchuksuperapp.data.entities.Skill
 import com.hfad.palamarchuksuperapp.ui.common.SkillDomainRW
 import kotlinx.coroutines.flow.Flow
 
 interface SkillRepository {
-    fun getSkillsFromDB(): Flow<List<Skill>>
+    fun getSkillsFromDB(): Flow<List<SkillDomainRW>>
     suspend fun deleteSkill(skill: SkillDomainRW)
     suspend fun addSkill(skill: SkillDomainRW)
     suspend fun updateSkill(skill: SkillDomainRW)
