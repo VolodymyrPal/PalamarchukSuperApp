626b0e5e5ca88c8663349d0e2880a8381aae8605
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-11 18:21:24 +0300

    SkillsRepository now produce flow of data from database.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/SkillsRepositoryImpl.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/SkillsRepositoryImpl.kt
@@ -4,8 +4,10 @@ import com.hfad.palamarchuksuperapp.data.dao.SkillsDao
 import com.hfad.palamarchuksuperapp.data.entities.Skill
 import com.hfad.palamarchuksuperapp.domain.repository.SkillRepository
 import com.hfad.palamarchuksuperapp.ui.common.SkillDomainRW
+import com.hfad.palamarchuksuperapp.ui.common.toDomainRW
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
 import java.util.UUID
 import javax.inject.Inject
 
@@ -13,9 +15,9 @@ class SkillsRepositoryImpl @Inject constructor(
     private val skillsDao: SkillsDao,
 ) : SkillRepository {
 
-    override fun getSkillsFromDB(): Flow<List<Skill>> {
-        return skillsDao.getAllSkillsFromDB()
-    }
+    override fun getSkillsFromDB(): Flow<List<SkillDomainRW>> =
+        skillsDao.getAllSkillsFromDB().map { list -> list.map { it.toDomainRW() } }
+
 
     override suspend fun deleteSkill(skill: SkillDomainRW) =
         skillsDao.deleteSkill(skill = skill.skill)
@@ -27,44 +29,92 @@ class SkillsRepositoryImpl @Inject constructor(
 }
 
 class SkillsRepositoryImplForPreview : SkillRepository {
-    override fun getSkillsFromDB(): Flow<List<Skill>> {
-        val one =
+    override fun getSkillsFromDB(): Flow<List<SkillDomainRW>> {
+        val one = SkillDomainRW(
             Skill(name = "some skills", description = "One, Two, Three", uuid = UUID.randomUUID())
-        val two = Skill(name = "XML", description = "One, Two, Three", uuid = UUID.randomUUID())
-        val three = Skill(
-            name = "Recycler View",
-            description = "Paging, DiffUtil, Adapter",
-            uuid = UUID.randomUUID()
         )
-        val four =
+        val two = SkillDomainRW(
+            Skill(
+                name = "XML",
+                description = "One, Two, Three",
+                uuid = UUID.randomUUID()
+            )
+        )
+        val three = SkillDomainRW(
+            Skill(
+                name = "Recycler View",
+                description = "Paging, DiffUtil, Adapter",
+                uuid = UUID.randomUUID()
+            )
+        )
+        val four = SkillDomainRW(
             Skill(name = "Retrofit", description = "One, Two, Three", uuid = UUID.randomUUID())
-        val five = Skill(name = "Async", description = "One, Two, Three", uuid = UUID.randomUUID())
-        val six = Skill(
-            name = "Dependency injections",
-            description = "Multi-module architecture, Component Dependencies, Multibindings, " + "Components, Scope, Injections" + " Libraries: Dagger 2, Hilt, ---Kodein, ---Koin",
-            uuid = UUID.randomUUID()
+        )
+        val five = SkillDomainRW(
+            Skill(
+                name = "Async",
+                description = "One, Two, Three",
+                uuid = UUID.randomUUID()
+            )
+        )
+        val six = SkillDomainRW(
+            Skill(
+                name = "Dependency injections",
+                description = "Multi-module architecture, Component Dependencies, Multibindings, " + "Components, Scope, Injections" + " Libraries: Dagger 2, Hilt, ---Kodein, ---Koin",
+                uuid = UUID.randomUUID()
+            )
         )
         val seven =
-            Skill(name = "RxJava", description = "One, Two, Three", uuid = UUID.randomUUID())
-        val eight = Skill(
-            name = "To learn",
-            description = "Firebase!!!, GITFLOW, GIT, SOLID, MVVM, MVP, " + "RESTful API!!!, JSON!, PUSH NOTIFICATIONS!!!, ",
-            uuid = UUID.randomUUID()
+            SkillDomainRW(
+                Skill(
+                    name = "RxJava",
+                    description = "One, Two, Three",
+                    uuid = UUID.randomUUID()
+                )
+            )
+        val eight = SkillDomainRW(
+            Skill(
+                name = "To learn",
+                description = "Firebase!!!, GITFLOW, GIT, SOLID, MVVM, MVP, " + "RESTful API!!!, JSON!, PUSH NOTIFICATIONS!!!, ",
+                uuid = UUID.randomUUID()
+            )
         )
         val nine =
-            Skill(name = "Recycler View", description = "One, Two, Three", uuid = UUID.randomUUID())
-        val ten = Skill(name = "Project Pattern", description = "MVVM", uuid = UUID.randomUUID())
-        val eleven =
-            Skill(name = "Compose", description = "Composition tracing", uuid = UUID.randomUUID())
-        val twelve = Skill(
-            name = "Work with Image",
-            description = "FileProvider,Coil",
-            uuid = UUID.randomUUID()
+            SkillDomainRW(
+                Skill(
+                    name = "Recycler View",
+                    description = "One, Two, Three",
+                    uuid = UUID.randomUUID()
+                )
+            )
+        val ten = SkillDomainRW(
+            Skill(
+                name = "Project Pattern",
+                description = "MVVM",
+                uuid = UUID.randomUUID()
+            )
         )
-        val thirteen = Skill(
-            name = "Work with Image",
-            description = "FileProvider,Coil",
-            uuid = UUID.randomUUID()
+        val eleven =
+            SkillDomainRW(
+                Skill(
+                    name = "Compose",
+                    description = "Composition tracing",
+                    uuid = UUID.randomUUID()
+                )
+            )
+        val twelve = SkillDomainRW(
+            Skill(
+                name = "Work with Image",
+                description = "FileProvider,Coil",
+                uuid = UUID.randomUUID()
+            )
+        )
+        val thirteen = SkillDomainRW(
+            Skill(
+                name = "Work with Image",
+                description = "FileProvider,Coil",
+                uuid = UUID.randomUUID()
+            )
         )
         val _skillsList = MutableStateFlow(
             listOf(
