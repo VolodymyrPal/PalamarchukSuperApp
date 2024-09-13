47c5e746ec562ef93703229170b0885a3091be03
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-13 18:52:03 +0300

    Begin to implement Result sealed interface to deal with database/API calls.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/StoreRepositoryImpl.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/StoreRepositoryImpl.kt
@@ -2,24 +2,32 @@ package com.hfad.palamarchuksuperapp.data.repository
 
 import com.hfad.palamarchuksuperapp.data.dao.StoreDao
 import com.hfad.palamarchuksuperapp.data.services.FakeStoreApi
+import com.hfad.palamarchuksuperapp.domain.models.DataError
+import com.hfad.palamarchuksuperapp.domain.models.Error
 import com.hfad.palamarchuksuperapp.domain.repository.StoreRepository
 import com.hfad.palamarchuksuperapp.ui.common.ProductDomainRW
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
-import kotlin.random.Random
+import com.hfad.palamarchuksuperapp.domain.models.Result
+import retrofit2.HttpException
 
 class StoreRepositoryImpl @Inject constructor(
     private val storeApi: FakeStoreApi,
     private val storeDao: StoreDao,
 ) : StoreRepository {
 
-    override val fetchProductsAsFlowFromDB: Flow<List<ProductDomainRW>> get() {
-        return storeDao.getAllProductsFromDB().catch { errorFlow.update { it } }
+    override val fetchProductsAsFlowFromDB: Flow<List<ProductDomainRW>> =
+        storeDao.getAllProductsFromDB().catch { errorFlow.update { it } }
+
+    suspend fun products(): Result<Flow<List<ProductDomainRW>>, Error> {
+        return try {
+            Result.Success(fetchProductsAsFlowFromDB)
+        } catch (e: HttpException) {
+            Result.Error(DataError.Network.InternalServerError)
+        }
     }
 
     override suspend fun softRefreshProducts() {
@@ -31,16 +39,27 @@ class StoreRepositoryImpl @Inject constructor(
         storeDao.insertOrIgnoreProducts(getProductWithErrors())
     }
 
-    private suspend fun getProductWithErrors(): List<ProductDomainRW> {
+    private suspend fun getProductWithErrors() : Result<List<ProductDomainRW>, Error> { //}: List<ProductDomainRW> {
+
         return try {
-            val storeProducts: List<ProductDomainRW> = storeApi.getProductsDomainRw()
-            delay(1000)
-            if (Random.nextInt(0, 100) < 33) throw Exception("Error")
-            storeProducts
-        } catch (e: Exception) {
-            errorFlow.update { e }
-            fetchProductsAsFlowFromDB.first()
-        }
+                Result.Success(storeApi.getProductsDomainRw())
+            } catch (e: Exception) {
+                try {
+                    Result.Error(DataError.Network.InternalServerError)
+                } catch (e: Exception) {
+                    Result.Error(DataError.Local.DatabaseError)
+                }
+            }
+
+//        return try {
+//            val storeProducts: List<ProductDomainRW> = storeApi.getProductsDomainRw()
+//            delay(1000)
+//            if (Random.nextInt(0, 100) < 33) throw Exception("Error")
+//            storeProducts
+//        } catch (e: Exception) {
+//            errorFlow.update { e }
+//            fetchProductsAsFlowFromDB.first()
+//        }
     }
 
     override suspend fun upsertAll(products: List<ProductDomainRW>) {
