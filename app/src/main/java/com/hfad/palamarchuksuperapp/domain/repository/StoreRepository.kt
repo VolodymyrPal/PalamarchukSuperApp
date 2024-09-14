c827c37111c4012628f3890b2560193a19718e4b
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-14 23:13:49 +0300

    Error class new MutableSharedFlow to emit only last value.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
@@ -2,11 +2,11 @@ package com.hfad.palamarchuksuperapp.domain.repository
 
 import com.hfad.palamarchuksuperapp.ui.common.ProductDomainRW
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
 
 interface StoreRepository {
     val fetchProductsAsFlowFromDB: Flow<List<ProductDomainRW>>
-    val errorFlow : MutableStateFlow<Exception?>
+    val errorFlow : MutableSharedFlow<Exception?>
     suspend fun updateProduct(product: ProductDomainRW)
     suspend fun hardRefreshProducts()
     suspend fun softRefreshProducts()
