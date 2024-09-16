dc13728b69373c5e5b6cf0a472d710fdfc3f87f6
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-16 18:58:06 +0300

    Expanding Gemini Api class for using JsonSerializer.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GeminiAPI.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GeminiAPI.kt
@@ -1,6 +1,11 @@
 package com.hfad.palamarchuksuperapp.data.services
 
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
 
 @Serializable
 data class GeminiRequest(
@@ -8,10 +13,27 @@ data class GeminiRequest(
 )
 @Serializable
 data class GeminiContent(
-    val parts: List<Part> = listOf(Part()),
+    val user: String = "user",
+    val parts: List<Part> = emptyList(),
 )
 
 @Serializable
-data class Part(
-    val text: String = "Is my request completed?",
-)
+sealed interface Part
+
+@Serializable ()
+data class TextPart(val text: String = "Is my request completed?") : Part
+
+@Serializable
+data class ImagePart (val imageUrl: String = "") : Part
+
+
+object PartSerializer : JsonContentPolymorphicSerializer<Part>(Part::class) {
+    override fun selectDeserializer(element: JsonElement): DeserializationStrategy<Part> {
+        val jsonObject = element.jsonObject
+        return when {
+            "text" in jsonObject -> TextPart.serializer()
+            "inlineData" in jsonObject -> ImagePart.serializer()
+            else -> throw SerializationException("Unknown Part type")
+        }
+    }
+}
