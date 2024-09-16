f8bc0062241e8cf4bb30b017e3989b12009260fa
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-16 15:55:22 +0300

    Get geminiKey from BuildConfig.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GeminiApiHandler.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GeminiApiHandler.kt
@@ -1,6 +1,6 @@
 package com.hfad.palamarchuksuperapp.data.services
 
-import com.hfad.palamarchuksuperapp.domain.models.GEMINI_AI_KEY
+import com.hfad.palamarchuksuperapp.BuildConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.post
@@ -10,11 +10,13 @@ import io.ktor.http.contentType
 import javax.inject.Inject
 
 class GeminiApiHandler @Inject constructor(private val httpClient: HttpClient) {
-    val part = Part(text = "Is my request completed?")
-    val geminiContent = GeminiContent(listOf(part))
-    val geminiRequest = GeminiRequest(listOf(geminiContent))
+    private val part = Part(text = "Is my request completed?")
+    private val geminiContent = GeminiContent(listOf(part))
+    private val geminiRequest = GeminiRequest(listOf(geminiContent))
 
-    val url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$GEMINI_AI_KEY"
+    private val apiKey = BuildConfig.GEMINI_AI_KEY
+    private val url =
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$apiKey"
 
     suspend fun sendRequestWithResponse(): String {
         return try {
