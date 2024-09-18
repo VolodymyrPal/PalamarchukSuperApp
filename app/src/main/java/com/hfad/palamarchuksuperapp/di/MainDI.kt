5f3743f77c4ac4e68069711a0796ec098024c8db
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-18 19:59:05 +0300

    Install caching to my HttpClient.

--- app/src/main/java/com/hfad/palamarchuksuperapp/di/MainDI.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/di/MainDI.kt
@@ -34,6 +34,7 @@ import dagger.multibindings.IntoMap
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.cio.endpoint
+import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -125,6 +126,7 @@ object NetworkModule {
                     isLenient = true  //TODO lenient for testing
                 })
             }
+            install(HttpCache) // Добавляем кэширование запросов, чтобы не обращаться к API каждый раз
         }
     }
 }
