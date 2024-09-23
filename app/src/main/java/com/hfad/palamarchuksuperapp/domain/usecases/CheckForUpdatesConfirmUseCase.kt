99e5ee3bc3d4e85aa82cee82314705f92b50d156
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-23 23:08:24 +0300

    UseCase to check updates now call StrongAlertDialog, for further developing.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/CheckForUpdatesConfirmUseCase.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/CheckForUpdatesConfirmUseCase.kt
@@ -5,8 +5,9 @@ import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.appupdate.AppUpdateOptions
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.UpdateAvailability
+import com.hfad.palamarchuksuperapp.ui.reusable.StrongAlertDialog
 
-class CheckForUpdatesConfirmUseCase (private val context: Context) {
+class CheckForUpdatesConfirmUseCase(private val context: Context) {
 
     val appUpdateManager = AppUpdateManagerFactory.create(context)
     val appUpdateInfoTask = appUpdateManager.appUpdateInfo
@@ -14,7 +15,12 @@ class CheckForUpdatesConfirmUseCase (private val context: Context) {
     fun checkForUpdatesConfirm(): Boolean {
         appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
             if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
-                appUpdateManager.startUpdateFlowForResult (appUpdateInfo, activityResultLaucher, AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build())
+                StrongAlertDialog(context).show("Update available", "Please update to continue") //TODO
+//                appUpdateManager.startUpdateFlowForResult(
+//                    appUpdateInfo, activityResultLaucher, AppUpdateOptions.newBuilder(
+//                        AppUpdateType.IMMEDIATE
+//                    ).build()
+//                )
             }
         }
         return true
