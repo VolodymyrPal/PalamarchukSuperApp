7178dcf0cff47dd37bafa6ff8e159e2cd6e9574c
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-24 23:56:45 +0300

    Passing via CompositionLocal navController to NavBar.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/compose/utils/BottomNavBar.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/compose/utils/BottomNavBar.kt
@@ -10,6 +10,8 @@ import android.os.VibratorManager
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Icon
@@ -26,24 +28,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.hfad.palamarchuksuperapp.R
 import com.hfad.palamarchuksuperapp.ui.compose.Routes
 import com.hfad.palamarchuksuperapp.domain.models.AppImages
 import com.hfad.palamarchuksuperapp.domain.models.TabBarItem
+import com.hfad.palamarchuksuperapp.ui.compose.LocalNavController
 import kotlinx.coroutines.launch
 
 @Composable
 fun BottomNavBar(
     modifier: Modifier = Modifier,
-    navController: NavHostController,
 ) {
+    val navController = LocalNavController.current
     val context: Context = LocalContext.current
     val vibe: Vibrator = remember {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -153,7 +157,7 @@ fun BottomNavBar(
 
     val tabBarItems = remember { listOf(HomeTopLevelRoute, CameraTopLevelRoute, SettingTopLevelRoute) }
     NavigationBar(
-        modifier = modifier,
+        modifier = modifier.padding(bottom = 25.dp, start = 25.dp, end = 25.dp).clip(RoundedCornerShape(25)),
         containerColor = MaterialTheme.colorScheme.primaryContainer
     ) {
         tabBarItems.forEachIndexed { index, tabBarItem ->
@@ -211,12 +215,12 @@ fun TabBarIconView(
 @Preview
 @Composable
 fun MyNavBarPreviewElement() {
-    //BottomNavBar()
+    BottomNavBar()
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun MyNavBarPreviewApp() {
-    Scaffold(bottomBar = { }) {}
+    Scaffold(bottomBar = { BottomNavBar() }) {}
 }
