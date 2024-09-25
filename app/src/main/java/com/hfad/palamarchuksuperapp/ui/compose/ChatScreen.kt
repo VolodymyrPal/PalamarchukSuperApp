06e3e0b3360427cb8ad86d496b58a022d2499186
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-25 23:19:59 +0200

    Remove functional code from UI, passing ViewModel.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/compose/ChatScreen.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/compose/ChatScreen.kt
@@ -6,10 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,15 +19,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.navigation.NavHostController
 import com.hfad.palamarchuksuperapp.BuildConfig
-import com.hfad.palamarchuksuperapp.data.services.ChatCompletionResponse
-import com.hfad.palamarchuksuperapp.data.services.GroqApi
 import com.hfad.palamarchuksuperapp.data.services.GroqRequest
 import com.hfad.palamarchuksuperapp.data.services.Message
 import com.hfad.palamarchuksuperapp.ui.compose.utils.BottomNavBar
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.hfad.palamarchuksuperapp.ui.viewModels.ChatBotViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.post
@@ -39,17 +32,13 @@ import io.ktor.client.request.url
 import io.ktor.http.headers
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 @Suppress("detekt.FunctionNaming", "detekt.LongMethod")
 @Composable
 fun ChatScreen(
     modifier: Modifier = Modifier,
+    chatBotViewModel: ChatBotViewModel = ChatBotViewModel(),
 ) {
     val navController = LocalNavController.current
     Scaffold(
@@ -57,16 +46,6 @@ fun ChatScreen(
         bottomBar = {
             BottomNavBar()
         },
-        floatingActionButton = {
-            FloatingActionButton(
-                shape = RoundedCornerShape(33),
-                modifier = Modifier,
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                onClick = {},
-                content = {
-                }
-            )
-        }
     ) { paddingValues ->
         Surface(
             color = Color.Transparent, modifier = modifier
@@ -145,52 +124,7 @@ fun ChatScreen(
                 item {
                     Button(
                         onClick = {
-                            val moshi = Moshi.Builder()
-                                .add(KotlinJsonAdapterFactory())
-                                .build()
 
-
-                            val retrofit = Retrofit.Builder()
-                                .baseUrl("https://api.groq.com")
-                                .addConverterFactory(
-                                    MoshiConverterFactory.create(moshi).asLenient()
-                                )
-                                .build()
-
-                            val groqApi = retrofit.create(GroqApi::class.java)
-
-                            val message =
-                                Message(role = "user", content = promptText)
-                            val request = GroqRequest(
-                                messages = listOf(message),
-                                model = "llama3-8b-8192"
-                            )
-
-                            val call = groqApi.getChatCompletion(
-                                apiKey = BuildConfig.GROQ_KEY,  // Ваш API ключ
-                                body = request
-                            )
-
-                            call.enqueue(object : Callback<ChatCompletionResponse> {
-
-                                override fun onResponse(
-                                    p0: Call<ChatCompletionResponse>,
-                                    p1: Response<ChatCompletionResponse>,
-                                ) {
-                                    if (p1.isSuccessful) {
-                                        responseText = p1.body()!!.choices[0].message.content
-                                    } else {
-                                        Log.d("TAG", "onResponseNotSuccessful: ${p1.code()}")
-                                    }
-                                }
-
-                                override fun onFailure(
-                                    p0: Call<ChatCompletionResponse>,
-                                    p1: Throwable,
-                                ) {
-                                    Log.d("Tag", "OnFailure: $p1")
-                                }
-                            })
                         },
                         modifier = Modifier.fillMaxWidth()
                     ) {
