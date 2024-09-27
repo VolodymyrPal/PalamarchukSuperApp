f063583f5126e9a725f1a70c176694c3dc0a709f
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-27 21:38:13 +0300

    VM get injected GroqApihandler, event for sendint message, and following flow from groq.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/viewModels/ChatBotViewModel.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/viewModels/ChatBotViewModel.kt
@@ -1,26 +1,32 @@
 package com.hfad.palamarchuksuperapp.ui.viewModels
 
+import androidx.lifecycle.viewModelScope
+import com.hfad.palamarchuksuperapp.data.services.GroqApiHandler
+import com.hfad.palamarchuksuperapp.data.services.GroqContentBuilder
 import com.hfad.palamarchuksuperapp.domain.models.DataError
 import com.hfad.palamarchuksuperapp.domain.models.Result
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ChatBotViewModel @Inject constructor(
-
+    val groqApi: GroqApiHandler
 ) : GenericViewModel<String, ChatBotViewModel.Event, ChatBotViewModel.Effect>() {
 
     sealed class Event : BaseEvent {
         object SendImage : Event()
+        data class SentText(val text: String) : Event()
     }
 
     sealed class Effect : BaseEffect {
 
     }
 
-    override val _dataFlow: Flow<Result<String, DataError>>
-        get() = TODO("Not yet implemented")
+    val message_flow = groqApi.chatHistory
+    override val _dataFlow: Flow<Result<String, DataError>> = emptyFlow()
     override val _errorFlow: MutableSharedFlow<Exception?>
         get() = TODO("Not yet implemented")
     override val uiState: StateFlow<State<String>>
@@ -28,11 +34,19 @@ class ChatBotViewModel @Inject constructor(
 
     override fun event(event: Event) {
         when (event) {
-            Event.SendImage -> sendImage()
+            is Event.SendImage -> {  }
+            is Event.SentText -> sendText(event.text)
         }
     }
 
-    private fun sendImage() {
-
+    private fun sendText(text: String) {
+        viewModelScope.launch {
+            val request = GroqContentBuilder.Builder().let {
+                it.role = "user"
+                //it.text(text)
+                it.buildText(text)
+            }
+            groqApi.sendMessageChatImage(request)
+        }
     }
 }
