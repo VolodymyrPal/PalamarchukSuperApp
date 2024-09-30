df1c65f711fd5537d9e20b31ea78533a9fde7243
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-30 22:37:30 +0300

    Add data class of State to ChatBot view model,

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/viewModels/ChatBotViewModel.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/viewModels/ChatBotViewModel.kt
@@ -3,18 +3,49 @@ package com.hfad.palamarchuksuperapp.ui.viewModels
 import androidx.lifecycle.viewModelScope
 import com.hfad.palamarchuksuperapp.data.services.GroqApiHandler
 import com.hfad.palamarchuksuperapp.data.services.GroqContentBuilder
+import com.hfad.palamarchuksuperapp.data.services.Message
 import com.hfad.palamarchuksuperapp.domain.models.DataError
 import com.hfad.palamarchuksuperapp.domain.models.Result
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ChatBotViewModel @Inject constructor(
-    val groqApi: GroqApiHandler
-) : GenericViewModel<String, ChatBotViewModel.Event, ChatBotViewModel.Effect>() {
+    private val groqApi: GroqApiHandler
+) : GenericViewModel<List<Message>, ChatBotViewModel.Event, ChatBotViewModel.Effect>() {
+
+    data class StateChat(
+        val listMessage: List<Message>,
+        val isLoading: Boolean,
+        val error: DataError?
+    ): State<List<Message>>
+
+    val isLoading: MutableStateFlow<Boolean> = MutableStateFlow(false)
+
+    override val uiState: StateFlow<StateChat> = combine(
+        groqApi.chatHistory, isLoading
+    ) { chatHistory, isLoading ->
+        StateChat(
+            listMessage = chatHistory,
+            isLoading = isLoading,
+            error = null
+        )
+    }.stateIn(viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = StateChat(
+            listMessage = emptyList(),
+            isLoading = false,
+            error = null
+        )
+    )
 
     sealed class Event : BaseEvent {
         object SendImage : Event()
