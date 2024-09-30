678e00131110fc016e39f000a74a7413dd77a9fb
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-30 22:36:10 +0300

    For storing data my interface Result when error has data parameter.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/Result.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/Result.kt
@@ -4,5 +4,5 @@ typealias RootError = Error
 
 sealed interface Result<out D, out E: RootError> {
     data class Success<out D, out E: RootError>(val data: D) : Result<D, E>
-    data class Error<out D, out E: RootError>(val error: E) : Result<D, E>
+    data class Error<out D, out E: RootError>(val error: E, val data: D? = null) : Result<D, E>
 }
