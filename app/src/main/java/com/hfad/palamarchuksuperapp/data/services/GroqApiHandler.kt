603ad8cc7cfd23d0288b5dd7d2f43a2333098806
palamarchukhome <palamarchuk.office@gmail.com>
2024-09-30 22:39:45 +0300

    Sometimes there are exceptions with internet connection without api request. To handle this add try/catch block.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GroqApiHandler.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GroqApiHandler.kt
@@ -2,6 +2,7 @@ package com.hfad.palamarchuksuperapp.data.services
 
 import android.util.Log
 import com.hfad.palamarchuksuperapp.BuildConfig
+import com.hfad.palamarchuksuperapp.domain.models.DataError
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.header
@@ -19,13 +20,14 @@ import javax.inject.Inject
 class GroqApiHandler @Inject constructor(
     private val httpClient: HttpClient,
 ) {
-
     private val apiKey = BuildConfig.GROQ_KEY
     private val max_tokens: Int = 100
     private val adminRoleMessage: Message = GroqContentBuilder.Builder().let {
         it.role = "system"
         it.text("You are tutor and trying to solve users problem on image")
     }.buildChat()
+
+    val errorFlow = MutableStateFlow<DataError?>(null)
 
     val chatHistory: MutableStateFlow<List<Message>> =
         MutableStateFlow(emptyList())
@@ -35,44 +37,52 @@ class GroqApiHandler @Inject constructor(
     val url_image = "https://api.groq.com/openai/v1/chat/generate"
 
 
-    suspend fun sendMessageChatImage(message: Message) {
-        chatHistory.update {
-            chatHistory.value.plus(message)
-        }
-        val requestBody = Json.encodeToString(
-            value = GroqRequest(
-                model = Models.GROQ_IMAGE.value,
-                messages = chatHistory.value
-            )
-        )
-        Log.d("Request Body", requestBody) // Логируем тело запроса
-
-        val request = httpClient.post(url) {
-            header("Authorization", "Bearer $apiKey")
-            contentType(ContentType.Application.Json)
-            setBody(GroqRequest(model = Models.GROQ_SIMPLE_TEXT.value, messages = chatHistory.value))
-        }
-        Log.d("Request", request.body<String>())
-        if (request.status == HttpStatusCode.OK) {
-            val response = request.body<ChatCompletionResponse>()
-
-            val responseMessage = GroqContentBuilder.Builder().let {
-                it.role = "assistant"
-                it.buildText("${(response.choices[0].message as MessageText).content}")
-            }
+    suspend fun getRespondChatImage(message: Message) {
+        try {
             chatHistory.update {
-                chatHistory.value.plus(responseMessage)
+                chatHistory.value.plus(message)
             }
-        } else if (request.status != HttpStatusCode.BadRequest) {
-            Log.d("TAG", "error in text ${request.status}")
-        } else if (request.status.value > 400 || request.status.value < 500) {
-            Log.d("TAG", "request error ${request.status}")
-        } else if (request.status.value > 500 || request.status.value < 600) {
-            Log.d("TAG", "server error ${request.status}")
-        } else {
-            Log.d("TAG", "unknown error ${request.status}")
-        }
+            val requestBody = Json.encodeToString(
+                value = GroqRequest(
+                    model = Models.GROQ_IMAGE.value,
+                    messages = chatHistory.value
+                )
+            )
+            Log.d("Request Body", requestBody) // Логируем тело запроса
 
+            val request = httpClient.post(url) {
+                header("Authorization", "Bearer $apiKey")
+                contentType(ContentType.Application.Json)
+                setBody(
+                    GroqRequest(
+                        model = Models.GROQ_SIMPLE_TEXT.value,
+                        messages = chatHistory.value
+                    )
+                )
+            }
+            Log.d("Request", request.body<String>())
+            if (request.status == HttpStatusCode.OK) {
+                val response = request.body<ChatCompletionResponse>()
+
+                val responseMessage = GroqContentBuilder.Builder().let {
+                    it.role = "assistant"
+                    it.buildText((response.choices[0].message as MessageText).content)
+                }
+                chatHistory.update {
+                    chatHistory.value.plus(responseMessage)
+                }
+            } else if (request.status != HttpStatusCode.BadRequest) {
+                Log.d("TAG", "error in text ${request.status}")
+            } else if (request.status.value > 400 || request.status.value < 500) {
+                Log.d("TAG", "request error ${request.status}")
+            } else if (request.status.value > 500 || request.status.value < 600) {
+                Log.d("TAG", "server error ${request.status}")
+            } else {
+                Log.d("TAG", "unknown error ${request.status}")
+            }
+        } catch (e: Exception) {
+            Log.d("TAG", "Error in sendText: $e")
+        }
     }
 }
 
@@ -94,7 +104,7 @@ class GroqContentBuilder {
         @JvmName("addImage")
         fun image(image: Base64) = content(ContentImage(image_url = ImageUrl(image)))
 
-//        fun build(): MessageText = MessageText(content = contents, role = role)
+        //        fun build(): MessageText = MessageText(content = contents, role = role)
         fun buildChat(): MessageChat = MessageChat(content = contents, role = role)
 
         fun buildText(request: String): MessageText = MessageText(content = request, role = role)
