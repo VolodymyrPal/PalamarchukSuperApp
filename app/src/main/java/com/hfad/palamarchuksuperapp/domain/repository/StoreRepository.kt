e40179892ead07e38b1bd5ef0332e30ca6934166
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-09 23:52:57 +0300

    Store Repositroy now produce error with State, not Shared.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/StoreRepository.kt
@@ -3,11 +3,11 @@ package com.hfad.palamarchuksuperapp.domain.repository
 import com.hfad.palamarchuksuperapp.domain.models.DataError
 import com.hfad.palamarchuksuperapp.ui.common.ProductDomainRW
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 
 interface StoreRepository {
     val fetchProductsAsFlowFromDB: Flow<List<ProductDomainRW>>
-    val errorFlow : MutableSharedFlow<DataError?>
+    val errorFlow : MutableStateFlow<DataError?>
     suspend fun updateProduct(product: ProductDomainRW)
     suspend fun hardRefreshProducts()
     suspend fun softRefreshProducts()
