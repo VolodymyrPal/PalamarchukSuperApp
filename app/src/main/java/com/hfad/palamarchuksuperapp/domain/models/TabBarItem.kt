748a8de4ea457b0fa61ed0c7a89bd1695ed2f4f5
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-12 23:22:30 +0200

    Mark data class as Stable to remove recompositions.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/TabBarItem.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/TabBarItem.kt
@@ -1,7 +1,9 @@
 package com.hfad.palamarchuksuperapp.domain.models
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.painter.Painter
 
+@Stable
 data class TabBarItem <T: Any>(
     val title: String,
     val route: T,
