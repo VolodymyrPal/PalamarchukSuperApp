be7e8d482e5021f9c062769d5c9e24dc87bd555c
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-18 18:56:30 +0300

    Add straightforward values for ChatAiRepository

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/ChatAiRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/ChatAiRepository.kt
@@ -1,12 +1,12 @@
 package com.hfad.palamarchuksuperapp.domain.repository
 
-import com.hfad.palamarchuksuperapp.data.services.Message
+import com.hfad.palamarchuksuperapp.data.entities.MessageAI
+import com.hfad.palamarchuksuperapp.domain.models.DataError
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 interface ChatAiRepository {
-    val chatAiChatFlow: StateFlow<*>
-    val errorFlow: MutableStateFlow<PersistentList<*>>
-    fun getRespondChatOrImage(message : Message)
+    val chatAiChatFlow: MutableStateFlow<PersistentList<MessageAI>>
+    val errorFlow: MutableStateFlow<DataError?>
+    suspend fun getRespondChatOrImage(message: String)
 }
