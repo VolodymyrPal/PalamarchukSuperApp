35e56bdf2c54a79234264160f748f4e593284ea8
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-20 21:35:31 +0300

    Add current model val to AI Repository and handling it with fun.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/ChatAiRepositoryImpl.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/ChatAiRepositoryImpl.kt
@@ -34,8 +34,29 @@ class ChatAiRepositoryImpl @Inject constructor(
             errorFlow.emit(DataError.CustomError(e.message ?: "Error"))
         }
 
+    }
+
+    private fun sendRequestToAI(message: String) {
+
+        when (currentModel) {
+            is AiModels.GroqModels -> groqApiHandler.getRespondChatOrImage(message)
+
+            is AiModels.GeminiModels,
+                -> geminiApiHandler.sendRequestWithResponse(message)
+
+            is AiModels.OpenAIModels,
+                -> openAIApiHandler.sendRequestWithResponse(message)
+        }
+
+        val aiMessage = MessageAI(role = "assistant", content = message)
+
+        chatAiChatFlow.update { chatAiChatFlow.value.add(aiMessage) }
 
     }
 
+    private val currentModel: AiModels = AiModels.GroqModels.BASE_IMAGE_MODEL
+
+
+}
 
 }
