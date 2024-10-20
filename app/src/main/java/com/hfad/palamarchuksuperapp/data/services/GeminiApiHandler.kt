a6d1c34fa7d29899a41b6c98244658f994af1849
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-20 21:40:21 +0300

    Gemini Apo Handler now AiModelHandler with overriding of getResponse().

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GeminiApiHandler.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GeminiApiHandler.kt
@@ -1,21 +1,27 @@
 package com.hfad.palamarchuksuperapp.data.services
 
 import android.util.Log
+import coil.network.HttpException
 import com.hfad.palamarchuksuperapp.BuildConfig
 import com.hfad.palamarchuksuperapp.data.entities.MessageAI
 import com.hfad.palamarchuksuperapp.data.entities.MessageType
+import com.hfad.palamarchuksuperapp.data.repository.AiModels
+import com.hfad.palamarchuksuperapp.domain.models.DataError
+import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandler
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
+import kotlinx.collections.immutable.PersistentList
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
-class GeminiApiHandler @Inject constructor(private val httpClient: HttpClient) {
+class GeminiApiHandler @Inject constructor(private val httpClient: HttpClient) : AiModelHandler {
     private val apiKey = BuildConfig.GEMINI_AI_KEY
     private val url =
         "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$apiKey"
@@ -55,5 +61,89 @@ class GeminiApiHandler @Inject constructor(private val httpClient: HttpClient) {
         } catch (e: Exception) {
             return MessageAI(e.message ?: "Error")
         }
+    }
+
+
+
+    override suspend fun getResponse(
+        messageList: PersistentList<MessageAI>,
+        model: AiModels?,
+    ): MessageAI {
+
+        val request = httpClient.post(url) {
+            header("Authorization", "Bearer $apiKey")
+            contentType(ContentType.Application.Json)
+            setBody(
+                messageList.toGeminiRequest(model = model ?: AiModels.GeminiModels.BASE_MODEL)
+            )
+        }
+        try {
+
+            Log.d("Groq response:", request.body<String>())
+
+            if (request.status == HttpStatusCode.OK) {
+
+                val response = request.body<ChatCompletionResponse>()
+
+                val responseMessage = GroqContentBuilder.Builder().let {
+                    it.role = "assistant"
+                    it.buildText((response.choices[0].message as MessageText).content)
+                }
+
+            } else {
+                throw CodeError(request.status.value)
+            }
+        } catch (e: Exception) {
+            errorFlow.emit(
+                when (e) {
+                    is HttpException -> {
+                        DataError.Network.InternalServerError
+                    }
+
+                    is CodeError -> {
+                        when (e.value) {
+                            400 -> {
+                                //DataError.Network.BadRequest
+                                DataError.CustomError(e.value.toString())
+                            }
+
+                            401 -> {
+                                DataError.CustomError(e.value.toString())
+                                //DataError.Network.Unauthorized
+                            }
+
+                            403 -> {
+                                //DataError.Network.Forbidden
+                                DataError.CustomError(e.value.toString())
+
+                            }
+
+                            in 400..500 -> {
+                                DataError.CustomError(e.value.toString())
+                                //DataError.Network.InternalServerError
+                            }
+
+                            in 500..600 -> {
+                                DataError.CustomError(e.value.toString())
+                                //DataError.Network.Unknown
+                            }
+
+                            else -> {
+                                DataError.CustomError(e.value.toString())
+                                //DataError.Network.Unknown
+                            }
+                        }
+                    }
+
+                    else -> {
+                        DataError.CustomError(e.toString())
+                    }
+                }
+            )
+        }
+
+        return MessageAI("", "")
+
+
     }
 }
