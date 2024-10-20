30cffed9a52df12ea8cd492efe78b03b0029e7b4
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-20 21:32:16 +0300

    For future logic add model valuable with model to use by Ai.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/AiModelHandler.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/AiModelHandler.kt
@@ -1,8 +1,12 @@
 package com.hfad.palamarchuksuperapp.domain.repository
 
 import com.hfad.palamarchuksuperapp.data.entities.MessageAI
+import com.hfad.palamarchuksuperapp.data.repository.AiModels
 import kotlinx.collections.immutable.PersistentList
 
 interface AiModelHandler {
-    suspend fun getResponse(messageList: PersistentList<MessageAI>): MessageAI
+    suspend fun getResponse(
+        messageList: PersistentList<MessageAI>,
+        model: AiModels? = null,
+    ): MessageAI
 }
