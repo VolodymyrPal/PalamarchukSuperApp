da988043f5409c6f374d4a94d799693ce90dc597
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-22 23:33:14 +0300

    Remove moshi from ProductDomainRW, deal with it with  kotlinx.serialization.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/common/ProductDomainRW.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/common/ProductDomainRW.kt
@@ -6,29 +6,28 @@ import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.hfad.palamarchuksuperapp.data.database.DATABASE_MAIN_ENTITY_PRODUCT
 import com.hfad.palamarchuksuperapp.data.entities.Product
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-import com.squareup.moshi.Moshi
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Entity(tableName = DATABASE_MAIN_ENTITY_PRODUCT)
-@JsonClass(generateAdapter = true)
 @TypeConverters(ProductDomainRWConverters::class)
 data class ProductDomainRW (
-    @PrimaryKey @Json(name="id") val id : Int = 0,
-    @Json(name="product") val product: Product,
-    @Json(name = "quantity") val quantity: Int = 0,
-    @Json(name = "liked") val liked: Boolean = false,
+    @PrimaryKey
+    val id : Int = 0,
+    val product: Product,
+    val quantity: Int = 0,
+    val liked: Boolean = false,
 )
-
+//
 class ProductDomainRWConverters {
 
     @TypeConverter
     fun fromProduct(product: Product): String {
-        return Moshi.Builder().build().adapter(Product::class.java).toJson(product)
+        return Json.encodeToString(product)
     }
     @TypeConverter
     fun toProduct (productDomainRW: String): Product {
-        return Moshi.Builder().build().adapter(Product::class.java).fromJson(productDomainRW)!!
+        return Json.decodeFromString<Product>(productDomainRW)
     }
 }
 
