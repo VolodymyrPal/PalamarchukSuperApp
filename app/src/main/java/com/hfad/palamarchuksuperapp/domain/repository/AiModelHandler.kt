95c62217c698bab2defea09e66ded880dc590810
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-24 23:12:00 +0300

    Testing pass Message with Result wrapper.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/AiModelHandler.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/AiModelHandler.kt
@@ -2,11 +2,13 @@ package com.hfad.palamarchuksuperapp.domain.repository
 
 import com.hfad.palamarchuksuperapp.data.entities.MessageAI
 import com.hfad.palamarchuksuperapp.data.repository.AiModels
+import com.hfad.palamarchuksuperapp.domain.models.DataError
+import com.hfad.palamarchuksuperapp.domain.models.Result
 import kotlinx.collections.immutable.PersistentList
 
 interface AiModelHandler {
     suspend fun getResponse(
         messageList: PersistentList<MessageAI>,
         model: AiModels? = null,
-    ): MessageAI
+    ): Result<MessageAI, DataError>
 }
