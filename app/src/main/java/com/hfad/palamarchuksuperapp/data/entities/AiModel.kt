9dc7339c1a2f36dd91b1d941c7f99bb0a385773b
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-29 16:59:44 +0200

    Remove moshi annotation Json + add boolean var for supported models.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/AiModel.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/AiModel.kt
@@ -1,45 +1,49 @@
 package com.hfad.palamarchuksuperapp.data.entities
 
-import com.squareup.moshi.Json
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 interface AiModel {
 
+    @SerialName(value = "name")
     val modelName: String
+    val isSupported: Boolean
 
     @Serializable
     data class GroqModel(
-        @Json(name = "name")
+        @SerialName(value = "name")
         override val modelName: String = "llama-3.2-11b-vision-preview",
+        override val isSupported: Boolean = true
     ) : AiModel
 
     @Serializable
     data class GeminiModel(
-        override val modelName: String = "gemini-1.5-flash",
+        @SerialName(value = "name") override val modelName: String,
         val version: String = "1.0.0",
         val displayName: String = "Gemini",
         val description: String = "Gemini is a language model that can generate images using the LLM",
         val supportedGenerationMethods: List<String> = emptyList(),
-        val isSupported: Boolean = supportedGenerationMethods.contains("generateContent"),
+        override val isSupported: Boolean = supportedGenerationMethods.contains("generateContent"),
     ) : AiModel
 
     @Serializable
     data class OpenAIModel(
         override val modelName: String = "openai-1",
+        override val isSupported: Boolean = true,
     ) : AiModel
 
 
-    enum class GroqModels(override val modelName: String) : AiModel {
-        BASE_MODEL("llama-3.2-11b-vision-preview"),
+    enum class GroqModels(override val modelName: String, override val isSupported: Boolean = true) : AiModel {
+        BASE_MODEL("llama-3.2-11b-vision-preview" ),
         TEXT_MODEL("llama3-groq-8b-8192-tool-use-preview")
     }
 
-    enum class GeminiModels(override val modelName: String) : AiModel {
-        BASE_MODEL("gemini-1.5-flash"),
+    enum class GeminiModels(override val modelName: String, override val isSupported: Boolean = true) : AiModel {
+        BASE_MODEL("models/gemini-1.5-flash"),
         GEMINI_IMAGE("")
     }
 
-    enum class OpenAIModels(override val modelName: String) : AiModel {
+    enum class OpenAIModels(override val modelName: String, override val isSupported: Boolean = true) : AiModel {
         BASE_MODEL(""),
         OPENAI_IMAGE("")
     }
