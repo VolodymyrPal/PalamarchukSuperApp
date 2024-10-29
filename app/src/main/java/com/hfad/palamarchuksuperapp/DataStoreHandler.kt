323f3e5119ba183ef1980bf11f1deadb5ace7d29
palamarchukhome <palamarchuk.office@gmail.com>
2024-10-29 17:09:36 +0200

    Add new DataStoreHandler class to deal with saving data in future.

---
+++ app/src/main/java/com/hfad/palamarchuksuperapp/DataStoreHandler.kt
@@ -0,0 +1,15 @@
+package com.hfad.palamarchuksuperapp
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import javax.inject.Inject
+
+class DataStoreHandler @Inject constructor (val context: Context) {
+    val appSettings = context.appSettingsStore
+    val chatScreenInfo = context.chatScreenInfoStore
+}
+
+val Context.appSettingsStore: DataStore<Preferences> by preferencesDataStore(name = "app_settings")
+val Context.chatScreenInfoStore: DataStore<Preferences> by preferencesDataStore(name = "chat_screen_info")
