69612611644c17ebbf1f271903009e29d396b8e2
palamarchukhome <palamarchuk.office@gmail.com>
2024-11-06 22:00:33 +0200

    Add class to get response from OpenAI chat.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/services/OpenAiAPI.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/services/OpenAiAPI.kt
@@ -36,3 +36,49 @@ data class ImageMessageRequest (
 data class ImageRequest (
     val url: String
 )
+
+
+
+@Serializable
+data class ChatCompletionResponse(
+    val id: String,
+    val `object`: String,
+    val created: Long,
+    val model: String,
+    val choices: List<Choice>,
+    val usage: Usage,
+    @SerialName("system_fingerprint") val systemFingerprint: String
+)
+
+@Serializable
+data class Choice(
+    val index: Int,
+    val message: Message,
+    @SerialName("logprobs") val logProbs: Int? = null,
+    @SerialName("finish_reason") val finishReason: String
+)
+
+@Serializable
+data class Message(
+    val role: String,
+    val content: String,
+    val refusal: String? = null
+)
+
+@Serializable
+data class Usage(
+    @SerialName("prompt_tokens") val promptTokens: Int,
+    @SerialName("completion_tokens") val completionTokens: Int,
+    @SerialName("total_tokens") val totalTokens: Int,
+    @SerialName("prompt_tokens_details") val promptTokensDetails: TokensDetails,
+    @SerialName("completion_tokens_details") val completionTokensDetails: TokensDetails
+)
+
+@Serializable
+data class TokensDetails(
+    @SerialName("cached_tokens") val cachedTokens: Int? = null ,
+    @SerialName("audio_tokens") val audioTokens: Int? = null,
+    @SerialName("reasoning_tokens") val reasoningTokens: Int? = null,
+    @SerialName("accepted_prediction_tokens") val acceptedPredictionTokens: Int? = null,
+    @SerialName("rejected_prediction_tokens") val rejectedPredictionTokens: Int? = null
+)
