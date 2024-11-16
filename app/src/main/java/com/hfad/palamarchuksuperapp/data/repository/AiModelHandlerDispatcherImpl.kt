c5d3b6c9c8739164e9b169a8fb86a6fdc821e3cf
palamarchukhome <palamarchuk.office@gmail.com>
2024-11-16 23:56:46 +0100

    Trying to add data store for dispatcher to keep list of handlers (will refactor to make it stateless).

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/AiModelHandlerDispatcherImpl.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/AiModelHandlerDispatcherImpl.kt
@@ -1,19 +1,27 @@
 package com.hfad.palamarchuksuperapp.data.repository
 
+import androidx.datastore.core.DataStore
 import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandler
 import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandlerDispatcher
+import kotlinx.collections.immutable.PersistentList
 import javax.inject.Inject
 
 class AiModelHandlerDispatcherImpl @Inject constructor(
     private val apiHandlers: Set<@JvmSuppressWildcards AiModelHandler>,
+    val handlerSettingStore: DataStore<AiHandlersSettings>,
 ) : AiModelHandlerDispatcher {
 
     override val handlerList: List<AiModelHandler> = apiHandlers.toMutableList().onEach {
-        it.chosen = false
+
     }
+    //TODO ("In planning - add server call to get handlers information")
 
     override fun getHandler(aiModelHandler: AiModelHandler): AiModelHandler {
-        TODO("Not yet implemented")
+        return handlerList[0] //TODO("Not yet implemented")
     }
 
 }
+
+data class AiHandlersSettings(
+    val handlers: PersistentList<String>,
+)
