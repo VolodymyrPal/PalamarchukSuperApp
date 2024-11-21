2d9d048d0d4634cf27159be322c7c8e92a20fe87
palamarchukhome <palamarchuk.office@gmail.com>
2024-11-21 22:38:07 +0200

    Start figuring out how to make ChangeAiMessage work as i wish.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/ChangeAiMessageUseCase.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/ChangeAiMessageUseCase.kt
@@ -1,15 +1,20 @@
 package com.hfad.palamarchuksuperapp.domain.usecases
 
+import com.hfad.palamarchuksuperapp.data.entities.MessageAI
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 interface ChangeAiMessageUseCase {
-    suspend operator fun invoke()
+    suspend operator fun invoke(list: PersistentList<MessageAI>, messageIndex: Int)
 }
 
 class ChangeAiMessageUseCaseImpl @Inject constructor(
-
+    private val getAiChatUseCase: GetAiChatUseCase
 ) : ChangeAiMessageUseCase {
-    override suspend operator fun invoke() {
-
+    override suspend operator fun invoke(list: PersistentList<MessageAI>, messageIndex: Int) {
+        getAiChatUseCase().update {
+            list
+        }
     }
 }
