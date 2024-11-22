7932f94bf10ddd9cbbd02e44693aa5540e9c1ee0
palamarchukhome <palamarchuk.office@gmail.com>
2024-11-22 23:15:33 +0200

    Repository need to have methods to modify it content, which will called by usecases.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/ChatAiRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/ChatAiRepository.kt
@@ -6,11 +6,12 @@ import com.hfad.palamarchuksuperapp.domain.models.AppError
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 interface ChatAiRepository {
     val chatAiChatFlow: MutableStateFlow<PersistentList<MessageAI>>
     val errorFlow: MutableSharedFlow<AppError?>
     suspend fun getModels(handler: AiModelHandler): List<AiModel>
     suspend fun getModels(handlers: List<AiModelHandler>): List<AiModel>
+    suspend fun addMessage(messageAI: MessageAI)
+    suspend fun updateMessage(index: Int, updatedContent: MessageAI)
 }
