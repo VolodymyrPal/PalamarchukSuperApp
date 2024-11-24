296603769d6cf74df975b0d89871009a237fde2e
palamarchukhome <palamarchuk.office@gmail.com>
2024-11-24 15:17:50 +0200

    There will be 2 entity: provider (for different Ai) and ai handler for model.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GroqApiHandler.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/services/GroqApiHandler.kt
@@ -10,7 +10,7 @@ import com.hfad.palamarchuksuperapp.data.entities.SubMessageAI
 import com.hfad.palamarchuksuperapp.domain.models.AppError
 import com.hfad.palamarchuksuperapp.domain.models.Result
 import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandler
-import com.hfad.palamarchuksuperapp.domain.repository.HandlerName
+import com.hfad.palamarchuksuperapp.domain.repository.AiProviderName
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
