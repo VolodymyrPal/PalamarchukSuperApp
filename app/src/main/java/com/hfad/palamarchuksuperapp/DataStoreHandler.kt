078782bcf3fbb2850c77535ba54d11c8021dd57d
palamarchukhome <palamarchuk.office@gmail.com>
2024-11-25 23:17:15 +0200

    Add future aiHandlerList to keep handlers between lifecycle.

--- app/src/main/java/com/hfad/palamarchuksuperapp/DataStoreHandler.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/DataStoreHandler.kt
@@ -9,7 +9,9 @@ import javax.inject.Inject
 class DataStoreHandler @Inject constructor (val context: Context) {
     val appSettings = context.appSettingsStore
     val chatScreenInfo = context.chatScreenInfoStore
+    val aiHandlerList = context.aiHandlerList
 }
 
 val Context.appSettingsStore: DataStore<Preferences> by preferencesDataStore(name = "app_settings")
 val Context.chatScreenInfoStore: DataStore<Preferences> by preferencesDataStore(name = "chat_screen_info")
+val Context.aiHandlerList: DataStore<Preferences> by preferencesDataStore(name = "ai_handler_list")
