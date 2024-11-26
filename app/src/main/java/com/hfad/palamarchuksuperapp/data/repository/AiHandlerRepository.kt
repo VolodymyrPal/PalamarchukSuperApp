8d32c9decb9993244ecb97cd09d5e9894c30241f
palamarchukhome <palamarchuk.office@gmail.com>
2024-11-26 23:46:44 +0200

    Begin to implement data store of ai handlers in to repository.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/AiHandlerRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/AiHandlerRepository.kt
@@ -1,10 +1,14 @@
 package com.hfad.palamarchuksuperapp.data.repository
 
+import com.hfad.palamarchuksuperapp.DataStoreHandler
 import com.hfad.palamarchuksuperapp.data.entities.AiModel
+import com.hfad.palamarchuksuperapp.domain.models.AiHandler
 import com.hfad.palamarchuksuperapp.domain.models.AppError
 import com.hfad.palamarchuksuperapp.domain.models.Result
 import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandler
 import com.hfad.palamarchuksuperapp.domain.usecases.GetModelsUseCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 interface AiHandlerRepository {
@@ -13,12 +17,23 @@ interface AiHandlerRepository {
 }
 
 class AiHandlerRepositoryImpl @Inject constructor(
-    private val apiHandlers: Set<@JvmSuppressWildcards AiModelHandler>,
-    private val getModelsUseCase: GetModelsUseCase
+    //private val apiHandlers: Set<@JvmSuppressWildcards AiModelHandler>,
+    private val dataStoreHandler: DataStoreHandler,
+    private val getModelsUseCase: GetModelsUseCase,
 ) : AiHandlerRepository {
 
     override val handlerList: List<AiModelHandler> =
-        apiHandlers.filter { it.enabled }.sortedBy { it.modelName }
+        dataStoreHandler.aiHandlerList.data.map { prefernces ->
+            prefernces["enabled"] ?: listOf(
+                AiHandler(
+                    modelName = AiProviderName.GEMINI,
+                    chosen = true,
+                    enabled = true,
+                    model = AiModel.GEMINI_BASE_MODEL,
+                )
+            )
+        }.first()
+    //apiHandlers.filter { it.enabled }.sortedBy { it.modelName }
 
     /**
      * Describe how to get handlers and sort them
