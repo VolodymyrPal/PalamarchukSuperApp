48a4f6d92abaab4b1a5465c7f699e523d9e3c795
palamarchukhome <palamarchuk.office@gmail.com>
2024-11-27 22:08:54 +0200

    Create polymorphic serialization for AiModel sealed class.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/AiModel.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/AiModel.kt
@@ -2,8 +2,15 @@ package com.hfad.palamarchuksuperapp.data.entities
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
-interface AiModel {
+@Serializable(with = AiModel.AiModelSerializer::class)
+sealed interface AiModel {
 
     @SerialName(value = "name")
     val modelName: String
@@ -37,6 +44,16 @@ interface AiModel {
         override val modelName: String = "openai-1",
         override val isSupported: Boolean = true,
     ) : AiModel
+
+    object AiModelSerializer : JsonContentPolymorphicSerializer<AiModel>(AiModel::class) {
+        override fun selectDeserializer(element: JsonElement) =
+            when (element.jsonObject["llmName"]?.jsonPrimitive?.contentOrNull) {
+                LLMName.GROQ.name -> GroqModel.serializer()
+                LLMName.GEMINI.name -> GeminiModel.serializer()
+                LLMName.OPENAI.name-> OpenAIModel.serializer()
+                else -> throw SerializationException("Unknown Model type")
+            }
+    }
 }
 
 enum class AiProviderName {
