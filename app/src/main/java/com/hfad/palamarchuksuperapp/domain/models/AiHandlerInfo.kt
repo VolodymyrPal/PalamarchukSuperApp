0d199a92fe54356921ecf0ef2d154587ec90355e
palamarchukhome <palamarchuk.office@gmail.com>
2024-12-06 20:40:00 +0200

    Add name parameter to allow user call their handlers as they wish.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AiHandlerInfo.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AiHandlerInfo.kt
@@ -6,27 +6,34 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AiHandlerInfo(
-    val isSelected : Boolean,
-    val isActive : Boolean,
-    val model: AiModel
+    val name: String,
+    val isSelected: Boolean,
+    val isActive: Boolean,
+    val model: AiModel,
 ) {
+
     companion object {
-        val DEFAULT_LIST_AI_HANDLER_INFO = persistentListOf(
-            AiHandlerInfo(
-                isSelected = true,
-                isActive = true,
-                model = AiModel.OPENAI_BASE_MODEL
-            ),
-            AiHandlerInfo(
-                isSelected = true,
-                isActive = true,
-                model = AiModel.GEMINI_BASE_MODEL
-            ),
-            AiHandlerInfo(
-                isSelected = true,
-                isActive = true,
-                model = AiModel.GROQ_BASE_MODEL
+        val DEFAULT_LIST_AI_HANDLER_INFO by lazy {
+            persistentListOf(
+                AiHandlerInfo(
+                    name = "OpenAI",
+                    isSelected = true,
+                    isActive = true,
+                    model = AiModel.OPENAI_BASE_MODEL
+                ),
+                AiHandlerInfo(
+                    name = "Gemini",
+                    isSelected = true,
+                    isActive = true,
+                    model = AiModel.GEMINI_BASE_MODEL
+                ),
+                AiHandlerInfo(
+                    name = "Groq",
+                    isSelected = true,
+                    isActive = true,
+                    model = AiModel.GROQ_BASE_MODEL
+                )
             )
-        )
+        }
     }
 }
