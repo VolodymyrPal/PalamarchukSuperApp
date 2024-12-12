e71a92d344ed2258521c667c04fa34c1826cb9b7
palamarchukhome <palamarchuk.office@gmail.com>
2024-12-12 19:45:27 +0200

    Trying to add new ai handler in repository. Contains some bugs.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/AiHandlerRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/AiHandlerRepository.kt
@@ -8,6 +8,7 @@ import com.hfad.palamarchuksuperapp.domain.models.AppError
 import com.hfad.palamarchuksuperapp.domain.models.Result
 import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandler
 import com.hfad.palamarchuksuperapp.domain.usecases.GetModelsUseCase
+import com.hfad.palamarchuksuperapp.domain.usecases.MapAiModelHandlerUseCase
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,7 +19,7 @@ import javax.inject.Inject
 interface AiHandlerRepository {
     suspend fun getHandlerFlow(): StateFlow<PersistentList<AiModelHandler>>
     suspend fun getModelsFromHandler(handler: AiModelHandler): Result<List<AiModel>, AppError>
-    suspend fun addHandler(handler: AiModelHandler)
+    suspend fun addHandler(handlerInfo: AiHandlerInfo)
     suspend fun removeHandler(handler: AiModelHandler)
     suspend fun updateHandler(handler: AiModelHandler, aiHandlerInfo: AiHandlerInfo)
 }
@@ -26,14 +27,16 @@ interface AiHandlerRepository {
 class AiHandlerRepositoryImpl @Inject constructor(
     private val dataStoreHandler: DataStoreHandler,
     private val getModelsUseCase: GetModelsUseCase,
+    private val mapAiModelHandlerUseCase: MapAiModelHandlerUseCase,
 ) : AiHandlerRepository {
 
     override suspend fun getHandlerFlow(): StateFlow<PersistentList<AiModelHandler>> =
         _handlerFlow()
 
+
     private suspend fun _handlerFlow(): MutableStateFlow<PersistentList<AiModelHandler>> {
         return MutableStateFlow(
-            dataStoreHandler.getAiHandlerList().toPersistentList()
+            dataStoreHandler.getAiHandlerList().toPersistentList() //TODO переделать, что бы функция не вызывалась каждый раз
         )
     }
 
@@ -41,10 +44,13 @@ class AiHandlerRepositoryImpl @Inject constructor(
         return getModelsUseCase(handler)
     }
 
-    override suspend fun addHandler(handler: AiModelHandler) {
-        _handlerFlow().update {
-            it.add(handler)
+    override suspend fun addHandler(handlerInfo: AiHandlerInfo) {
+        val newHandler = mapAiModelHandlerUseCase(handlerInfo)
+        _handlerFlow().update { currentList ->
+            val newList = currentList.add(newHandler)
+            newList
         }
+        Log.d("Handler", _handlerFlow().value.toString())
         dataStoreHandler.saveAiHandlerList(_handlerFlow().value)
     }
 
@@ -57,7 +63,6 @@ class AiHandlerRepositoryImpl @Inject constructor(
     }
 
     override suspend fun updateHandler(handler: AiModelHandler, aiHandlerInfo: AiHandlerInfo) {
-        Log.d("AiHandler Repository", "updateHandler: $aiHandlerInfo")
         handler.setAiHandlerInfo(
             aiHandlerInfo
         )
