14691326ad037317839109a5e1c71022b18771da
palamarchukhome <palamarchuk.office@gmail.com>
2024-12-12 19:39:48 +0200

    Move initializing of Api Key to AiHandler Info.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AiHandlerInfo.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AiHandlerInfo.kt
@@ -1,6 +1,7 @@
 package com.hfad.palamarchuksuperapp.domain.models
 
 import androidx.compose.runtime.Stable
+import com.hfad.palamarchuksuperapp.BuildConfig
 import com.hfad.palamarchuksuperapp.data.entities.AiModel
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.serialization.Serializable
@@ -12,6 +13,7 @@ data class AiHandlerInfo(
     val isSelected: Boolean,
     val isActive: Boolean,
     val model: AiModel,
+    val aiApiKey: String = "",
 ) {
 
     companion object {
@@ -21,19 +23,22 @@ data class AiHandlerInfo(
                     name = "OpenAI",
                     isSelected = true,
                     isActive = true,
-                    model = AiModel.OPENAI_BASE_MODEL
+                    model = AiModel.OPENAI_BASE_MODEL,
+                    aiApiKey = "123"//BuildConfig.OPEN_AI_KEY_USER
                 ),
                 AiHandlerInfo(
                     name = "Gemini",
                     isSelected = true,
                     isActive = true,
-                    model = AiModel.GEMINI_BASE_MODEL
+                    model = AiModel.GEMINI_BASE_MODEL,
+                    aiApiKey = BuildConfig.GEMINI_AI_KEY
                 ),
                 AiHandlerInfo(
                     name = "Groq",
                     isSelected = true,
                     isActive = true,
-                    model = AiModel.GROQ_BASE_MODEL
+                    model = AiModel.GROQ_BASE_MODEL,
+                    aiApiKey = BuildConfig.GROQ_KEY
                 )
             )
         }
