f4621375b3244a039a10b642f2bd4cf3b3068907
palamarchukhome <palamarchuk.office@gmail.com>
2024-12-13 21:55:31 +0200

    Ai handlers now get without suspending and producing flow without wrappers.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/GetAiHandlersUseCase.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/GetAiHandlersUseCase.kt
@@ -2,17 +2,17 @@ package com.hfad.palamarchuksuperapp.domain.usecases
 
 import com.hfad.palamarchuksuperapp.data.repository.AiHandlerRepository
 import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandler
-import kotlinx.collections.immutable.PersistentList
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 interface GetAiHandlersUseCase {
-    suspend operator fun invoke(): StateFlow<PersistentList<AiModelHandler>>
+    operator fun invoke(): Flow<List<AiModelHandler>>
 }
 
 class GetAiHandlersUseCaseImpl @Inject constructor(
     private val aiHandlerRepository: AiHandlerRepository,
 ) : GetAiHandlersUseCase {
-    override suspend operator fun invoke(): StateFlow<PersistentList<AiModelHandler>> =
-        aiHandlerRepository.getHandlerFlow()
+
+    override operator fun invoke(): Flow<List<AiModelHandler>> =
+        aiHandlerRepository.aiHandlerFlow
 }
