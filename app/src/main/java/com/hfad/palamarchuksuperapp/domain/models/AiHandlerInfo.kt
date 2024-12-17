39b89ba7b0037c496d1016cfb79b93fcbc1ee6cc
palamarchukhome <palamarchuk.office@gmail.com>
2024-12-17 23:31:24 +0200

    Create companion object for default handlers info.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AiHandlerInfo.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AiHandlerInfo.kt
@@ -10,7 +10,7 @@ import kotlin.random.Random
 @Serializable
 @Stable
 data class AiHandlerInfo(
-    val id : String = "${System.currentTimeMillis()}-${Random.nextInt(0, 9999)}",
+    val id: String = "${System.currentTimeMillis()}-${Random.nextInt(0, 9999)}",
     val name: String,
     val isSelected: Boolean,
     val isActive: Boolean,
@@ -19,32 +19,42 @@ data class AiHandlerInfo(
 ) {
 
     companion object {
+        val DEFAULT_AI_HANDLER_INFO_OPEN_AI by lazy {
+            AiHandlerInfo(
+                id = "0",
+                name = "OpenAI",
+                isSelected = true,
+                isActive = true,
+                model = AiModel.OPENAI_BASE_MODEL,
+                aiApiKey = BuildConfig.OPEN_AI_KEY_USER
+            )
+        }
+        val DEFAULT_AI_HANDLER_INFO_GEMINI by lazy {
+            AiHandlerInfo(
+                id = "1",
+                name = "Gemini",
+                isSelected = true,
+                isActive = true,
+                model = AiModel.GEMINI_BASE_MODEL,
+                aiApiKey = BuildConfig.GEMINI_AI_KEY
+            )
+        }
+        val DEFAULT_AI_HANDLER_INFO_GROQ by lazy {
+            AiHandlerInfo(
+                id = "2",
+                name = "Groq",
+                isSelected = true,
+                isActive = true,
+                model = AiModel.GROQ_BASE_MODEL,
+                aiApiKey = BuildConfig.GROQ_KEY
+            )
+        }
+
         val DEFAULT_LIST_AI_HANDLER_INFO by lazy {
             persistentListOf(
-                AiHandlerInfo(
-                    id = "0",
-                    name = "OpenAI",
-                    isSelected = true,
-                    isActive = true,
-                    model = AiModel.OPENAI_BASE_MODEL,
-                    aiApiKey = "123"//BuildConfig.OPEN_AI_KEY_USER
-                ),
-                AiHandlerInfo(
-                    id = "1",
-                    name = "Gemini",
-                    isSelected = true,
-                    isActive = true,
-                    model = AiModel.GEMINI_BASE_MODEL,
-                    aiApiKey = BuildConfig.GEMINI_AI_KEY
-                ),
-                AiHandlerInfo(
-                    id = "2",
-                    name = "Groq",
-                    isSelected = true,
-                    isActive = true,
-                    model = AiModel.GROQ_BASE_MODEL,
-                    aiApiKey = BuildConfig.GROQ_KEY
-                )
+                DEFAULT_AI_HANDLER_INFO_GROQ,
+                DEFAULT_AI_HANDLER_INFO_GEMINI,
+                DEFAULT_AI_HANDLER_INFO_OPEN_AI
             )
         }
     }
