742b1f5977517c2f5acf14615f5c0506cc46d04b
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-02 18:46:22 +0200

    Testing new loading effect of messages.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/ChatAiRepositoryImpl.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/ChatAiRepositoryImpl.kt
@@ -1,24 +1,23 @@
 package com.hfad.palamarchuksuperapp.data.repository
 
 import com.hfad.palamarchuksuperapp.domain.models.AiModel
-import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
-import com.hfad.palamarchuksuperapp.domain.models.SubMessageAI
 import com.hfad.palamarchuksuperapp.domain.models.AppError
+import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
+import com.hfad.palamarchuksuperapp.domain.models.Result
+import com.hfad.palamarchuksuperapp.domain.models.SubMessageAI
+import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandler
 import com.hfad.palamarchuksuperapp.domain.repository.ChatAiRepository
 import kotlinx.collections.immutable.PersistentList
-import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import javax.inject.Inject
-import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandler
-import com.hfad.palamarchuksuperapp.domain.models.Result
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
 
 class ChatAiRepositoryImpl @Inject constructor() : ChatAiRepository {
 
     override val chatAiChatFlow: MutableStateFlow<PersistentList<MessageGroup>> =
-        MutableStateFlow(persistentListOf())
+        MutableStateFlow(MockChat())
 
     override val errorFlow: MutableSharedFlow<AppError?> = MutableSharedFlow()
 
