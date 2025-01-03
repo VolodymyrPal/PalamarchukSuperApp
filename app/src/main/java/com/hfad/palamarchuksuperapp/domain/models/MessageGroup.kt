dfd305ecdbd3ca090f1bf7dd1a95b4fea41c8530
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-03 23:06:02 +0100

    Update constructors for MessageGroup.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/MessageGroup.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/MessageGroup.kt
@@ -2,6 +2,7 @@ package com.hfad.palamarchuksuperapp.domain.models
 
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toPersistentList
 import kotlinx.datetime.Clock
 
 typealias Base64 = String
@@ -9,21 +10,27 @@ typealias Base64 = String
 data class MessageGroup(
     val id: Int = 0,
     val role: Role = Role.USER,
-    val content: PersistentList<SubMessageAI> = persistentListOf(),
+    val content: PersistentList<MessageAI> = persistentListOf(),
+    val chatId: Int,
     val type: MessageType = MessageType.TEXT,
 ) {
     constructor(
-        id: Int, role: Role = Role.USER, content: String, type: MessageType = MessageType.TEXT,
+        id: Int,
+        role: Role = Role.USER,
+        content: String,
+        type: MessageType = MessageType.TEXT,
+        chatGroupId: Int,
     ) : this(
         id = id,
         role = role,
         content = persistentListOf(
-            SubMessageAI(
+            MessageAI(
                 timestamp = Clock.System.now().toString(),
                 message = content,
-                messageAiID = id
+                messageGroupId = id,
             )
         ),
+        chatId = chatGroupId,
         type = type
     )
 
@@ -33,18 +40,46 @@ data class MessageGroup(
         content: String,
         otherContent: Base64,
         type: MessageType = MessageType.IMAGE,
+        chatGroupId: Int,
     ) : this(
         id = id,
         role = role,
         content = persistentListOf(
-            SubMessageAI(
+            MessageAI(
                 timestamp = Clock.System.now().toString(),
                 message = content,
                 otherContent = MessageAiContent.Image(otherContent),
-                messageAiID = id
+                messageGroupId = id
             ),
         ),
+        chatId = chatGroupId,
         type = type
+    )
+
+    constructor(
+        id: Int,
+        role: Role = Role.USER,
+        content: List<MessageAI>,
+        type: MessageType = MessageType.TEXT,
+        chatId: Int,
+    ) : this(
+        id = id,
+        role = role,
+        content = content.toPersistentList(),
+        chatId = chatId,
+        type = type
+    )
+
+    constructor(
+        id: Int,
+        role: Role = Role.USER,
+        type: MessageType = MessageType.TEXT,
+        content: List<MessageAI>,
+    ) : this(
+        id = id,
+        role = role,
+        content = content.toPersistentList(),
+        chatId = 0
     )
 }
 
