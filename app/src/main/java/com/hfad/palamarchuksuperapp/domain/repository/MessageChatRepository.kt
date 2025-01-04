d3b718f94012d4299bd46b3012166f19387315b6
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-04 23:47:55 +0100

    Remove MessageChatWithMessages entity to simplify logic.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/MessageChatRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/MessageChatRepository.kt
@@ -1,13 +1,12 @@
 package com.hfad.palamarchuksuperapp.domain.repository
 
-import com.hfad.palamarchuksuperapp.data.entities.MessageChatWithMessages
 import com.hfad.palamarchuksuperapp.domain.models.MessageChat
 import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
 import com.hfad.palamarchuksuperapp.domain.models.MessageAI
 
 interface MessageChatRepository { //TODO
     fun getAllChats(): List<MessageChat>
-    suspend fun getChatById(chatId: Int): MessageChatWithMessages?
+    suspend fun getChatById(chatId: Int): MessageChat?
     suspend fun createChat(name: String)
     suspend fun addMessageToChat(chatId: Int, message: MessageGroup)
     suspend fun updateChat(chat: MessageChat)
