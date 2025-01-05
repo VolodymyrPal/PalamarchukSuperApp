9bf11fe56dc6f7a50a9eb92c6eed4cc5ae2ca158
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-05 23:47:33 +0100

    Add glue class for mapping from database entity to full domain class.

---
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/MessageChatWithRelations.kt
@@ -0,0 +1,48 @@
+package com.hfad.palamarchuksuperapp.data.entities
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.hfad.palamarchuksuperapp.domain.models.MessageChat
+import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
+
+data class MessageChatWithRelations(
+    @Embedded
+    val chat: MessageChatEntity, // Данные чата
+
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "chatId",
+        entity = MessageGroupEntity::class
+    )
+    val messages: List<MessageGroupWithMessages> // Группы сообщений, связанные с чатом
+) {
+    fun toDomainModel(): MessageChat {
+        return MessageChat(
+            id = chat.id,
+            name = chat.name,
+            messages = messages.map { messages -> messages.toDomainModel() }
+        )
+    }
+}
+
+data class MessageGroupWithMessages(
+    @Embedded
+    val group: MessageGroupEntity, // Данные группы сообщений
+
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "messageGroupId",
+        entity = MessageAiEntity::class
+    )
+    val messages: List<MessageAiEntity> // Сообщения, связанные с группой
+) {
+    fun toDomainModel(): MessageGroup {
+        return MessageGroup(
+            id = group.id,
+            content = messages.map{ it.toDomainModel() },
+            role = group.role,
+            type = group.type,
+            chatId = group.chatId
+        )
+    }
+}
