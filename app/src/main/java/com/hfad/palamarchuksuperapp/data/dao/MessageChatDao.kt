3b6425d3ccfe44be73dd40d7604a5a4fb1eaa4a7
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-05 23:44:57 +0100

    Update dao for new entities.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/dao/MessageChatDao.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/dao/MessageChatDao.kt
@@ -1,8 +1,38 @@
 package com.hfad.palamarchuksuperapp.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.hfad.palamarchuksuperapp.data.entities.MessageChatEntity
+import com.hfad.palamarchuksuperapp.data.entities.MessageChatWithRelations
 
 @Dao
-interface MessageChatDao { //TODO
+interface MessageChatDao {
+
+    @Query("SELECT * FROM MessageChat")
+    suspend fun getAllChatsInfo(): List<MessageChatEntity>
+
+    /**
+     * Получение всех чатов с сообщениями.
+     *
+     * Transaction позволяет сохранить целосность между родительским и дочерним элементом
+     * Без аннотации возможен исход, когда чат получен, а сообщения еще нет.
+     */
+    @Transaction
+    @Query("SELECT * FROM MessageChat")
+    fun getAllChatsWithMessages(): List<MessageChatWithRelations>
+
+    @Transaction
+    @Query("SELECT * FROM MessageChat WHERE id = :chatId")
+    suspend fun getChatWithMessages(chatId: Int): MessageChatWithRelations
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertChat(chat: MessageChatEntity): Long
+
+    @Delete
+    suspend fun deleteChat(chat: MessageChatEntity)
 
 } 
