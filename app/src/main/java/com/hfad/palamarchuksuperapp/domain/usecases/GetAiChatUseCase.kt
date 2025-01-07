bedd48e3c779bf345ca0d4bc66da72037eb972d3
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-07 23:03:11 +0200

    Use case now return MessageChat.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/GetAiChatUseCase.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/GetAiChatUseCase.kt
@@ -1,19 +1,18 @@
 package com.hfad.palamarchuksuperapp.domain.usecases
 
-import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
+import com.hfad.palamarchuksuperapp.domain.models.MessageChat
 import com.hfad.palamarchuksuperapp.domain.repository.ChatAiRepository
-import kotlinx.collections.immutable.PersistentList
-import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 interface GetAiChatUseCase {
-    operator fun invoke(): MutableStateFlow<PersistentList<MessageGroup>>
+    suspend operator fun invoke(chatId: Int): MessageChat?
 }
 
 class GetAiChatUseCaseImpl @Inject constructor(
     private val chatAiRepository: ChatAiRepository,
 ) : GetAiChatUseCase {
-    override operator fun invoke(): MutableStateFlow<PersistentList<MessageGroup>> {
-        return chatAiRepository.chatAiChatFlow
+
+    override suspend operator fun invoke(chatId: Int): MessageChat {
+        return chatAiRepository.getChatById(chatId)
     }
 }
