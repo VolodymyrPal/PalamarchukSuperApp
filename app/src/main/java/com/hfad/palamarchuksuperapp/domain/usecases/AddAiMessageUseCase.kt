12b3240e70220609e1332f22f665b8f15508adb1
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-07 23:30:28 +0200

    Update add ai message to pass new message group.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/AddAiMessageUseCase.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/AddAiMessageUseCase.kt
@@ -2,17 +2,16 @@ package com.hfad.palamarchuksuperapp.domain.usecases
 
 import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
 import com.hfad.palamarchuksuperapp.domain.repository.ChatAiRepository
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 interface AddAiMessageUseCase {
-    suspend operator fun invoke(message: MessageGroup)
+    suspend operator fun invoke(chatId: Int, message: MessageGroup)
 }
 
 class AddAiMessageUseCaseImpl @Inject constructor(
     private val chatAiRepository: ChatAiRepository,
 ) : AddAiMessageUseCase {
-    override suspend operator fun invoke(message: MessageGroup) {
-        chatAiRepository.chatAiChatFlow.update { it.add(message) }
+    override suspend operator fun invoke(chatId: Int, message: MessageGroup) {
+        chatAiRepository.addMessageGroup(chatId, message)
     }
 }
