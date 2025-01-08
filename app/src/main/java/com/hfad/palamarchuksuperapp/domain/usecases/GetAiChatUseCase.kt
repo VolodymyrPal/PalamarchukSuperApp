724f58ec6e05b2d951274ceebba5d27ebf0ab4a9
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-08 21:37:26 +0200

    Function to get chat from repository/dao.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/GetAiChatUseCase.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/GetAiChatUseCase.kt
@@ -1,18 +1,23 @@
 package com.hfad.palamarchuksuperapp.domain.usecases
 
 import com.hfad.palamarchuksuperapp.domain.models.MessageChat
+import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
 import com.hfad.palamarchuksuperapp.domain.repository.ChatAiRepository
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 interface GetAiChatUseCase {
     suspend operator fun invoke(chatId: Int): MessageChat?
+    operator fun invoke(): StateFlow<PersistentList<MessageGroup>>
 }
 
 class GetAiChatUseCaseImpl @Inject constructor(
     private val chatAiRepository: ChatAiRepository,
 ) : GetAiChatUseCase {
+    override operator fun invoke() = chatAiRepository.chatAiFlow
 
-    override suspend operator fun invoke(chatId: Int): MessageChat {
+    override suspend operator fun invoke(chatId: Int): MessageChat? {
         return chatAiRepository.getChatById(chatId)
     }
 }
