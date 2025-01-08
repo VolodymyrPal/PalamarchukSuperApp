53a0996e2b9178832f119d16e2a12799d4e542c9
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-08 21:36:23 +0200

    Add mapping.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/MessageGroupEntity.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/entities/MessageGroupEntity.kt
@@ -4,6 +4,7 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
 import com.hfad.palamarchuksuperapp.domain.models.MessageType
 import com.hfad.palamarchuksuperapp.domain.models.Role
 
@@ -30,4 +31,15 @@ data class MessageGroupEntity(
     val role: Role = Role.USER,
     val chatId: Int,
     val type: MessageType = MessageType.TEXT,
-) 
+) {
+    companion object {
+        fun from(chat: MessageGroup): MessageGroupEntity {
+            return MessageGroupEntity(
+                id = chat.id,
+                role = chat.role,
+                chatId = chat.chatId,
+                type = chat.type
+            )
+        }
+    }
+}
