2c92bee13476919f9c9e399cfb6a59346dce1693
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-18 23:49:34 +0200

    Rename for clear understanding.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/ObserveErrorUseCase.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/ObserveErrorUseCase.kt
@@ -5,13 +5,13 @@ import com.hfad.palamarchuksuperapp.domain.repository.ChatAiRepository
 import kotlinx.coroutines.flow.MutableSharedFlow
 import javax.inject.Inject
 
-interface GetErrorUseCase {
+interface ObserveErrorUseCase {
     operator fun invoke(): MutableSharedFlow<AppError?>
 }
 
-class GetErrorUseCaseImpl @Inject constructor(
+class ObserveErrorUseCaseImpl @Inject constructor(
     private val chatAiRepository: ChatAiRepository,
-    ) : GetErrorUseCase {
+) : ObserveErrorUseCase {
     override fun invoke(): MutableSharedFlow<AppError?> {
         return chatAiRepository.errorFlow
     }
