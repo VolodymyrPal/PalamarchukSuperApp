22844abc42821bb557868cef0e2e6770fa9c8edb
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-18 23:48:52 +0200

    Better use .from for object, it's more straightforward.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/MessageChat.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/MessageChat.kt
@@ -1,8 +1,34 @@
 package com.hfad.palamarchuksuperapp.domain.models
 
+import com.hfad.palamarchuksuperapp.data.entities.MessageChatEntity
+import com.hfad.palamarchuksuperapp.data.entities.MessageChatWithRelationsEntity
+
 data class MessageChat(
     val id: Int = 0,
-    val name: String = "",
+    val name: String = "New chat",
     val messageGroups: List<MessageGroup> = emptyList(),
     val timestamp: Long = System.currentTimeMillis(),
-)
+) {
+    companion object {
+        fun from(messageChatEntity: MessageChatEntity): MessageChat {
+            return MessageChat(
+                name = messageChatEntity.name,
+                timestamp = messageChatEntity.timestamp,
+                id = messageChatEntity.id
+            )
+        }
+
+        fun from(messageChatWithRelationsEntity: MessageChatWithRelationsEntity): MessageChat {
+            return MessageChat(
+                name = messageChatWithRelationsEntity.chat.name,
+                timestamp = messageChatWithRelationsEntity.chat.timestamp,
+                id = messageChatWithRelationsEntity.chat.id,
+                messageGroups = messageChatWithRelationsEntity.messageGroupsWithMessageEntity.map {
+                    MessageGroup.from(
+                        it
+                    )
+                }
+            )
+        }
+    }
+}
