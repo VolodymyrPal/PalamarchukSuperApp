84106e178584ed0b6aaefbd01da5c040fd9f4fe9
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-19 18:04:57 +0200

    Update use case.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/GetModelsUseCase.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/GetModelsUseCase.kt
@@ -1,24 +1,19 @@
 package com.hfad.palamarchuksuperapp.domain.usecases
 
-import com.hfad.palamarchuksuperapp.domain.models.AiModel
-import com.hfad.palamarchuksuperapp.domain.models.LLMName
 import com.hfad.palamarchuksuperapp.data.repository.AiHandlerRepository
+import com.hfad.palamarchuksuperapp.domain.models.AiModel
 import com.hfad.palamarchuksuperapp.domain.models.AppError
+import com.hfad.palamarchuksuperapp.domain.models.LLMName
 import com.hfad.palamarchuksuperapp.domain.models.Result
-import com.hfad.palamarchuksuperapp.domain.repository.AiModelHandler
 import javax.inject.Inject
 
 interface GetModelsUseCase {
-    suspend operator fun invoke(handler: AiModelHandler): Result<List<AiModel>, AppError>
     suspend operator fun invoke(llmName: LLMName): Result<List<AiModel>, AppError>
 }
 
 class GetModelsUseCaseImpl @Inject constructor(
     private val aiHandlerRepository: AiHandlerRepository,
 ) : GetModelsUseCase {
-    override suspend operator fun invoke(handler: AiModelHandler): Result<List<AiModel>, AppError> {
-        return handler.getModels()
-    }
 
     override suspend fun invoke(llmName: LLMName): Result<List<AiModel>, AppError> {
         return aiHandlerRepository.getBaseModels(llmName)
