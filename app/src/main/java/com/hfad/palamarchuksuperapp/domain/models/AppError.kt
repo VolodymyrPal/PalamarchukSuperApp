0a3e13736102bebba2b2f60485dc256278339f64
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-23 22:26:14 +0200

    Move mapSQLException to AppError file.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AppError.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AppError.kt
@@ -1,12 +1,10 @@
 package com.hfad.palamarchuksuperapp.domain.models
 
+import android.database.SQLException
 import android.database.sqlite.SQLiteConstraintException
 import android.database.sqlite.SQLiteDiskIOException
 import android.database.sqlite.SQLiteFullException
-import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
+import com.hfad.palamarchuksuperapp.domain.models.AppError.DatabaseError
 
 sealed interface AppError : Error {
     interface Network : AppError {
@@ -32,4 +30,24 @@ sealed interface AppError : Error {
     }
 
     data class CustomError(val errorText: String? = null, val error: Throwable? = null) : AppError
+}
+
+/** Maps an [SQLException] to a specific [AppError.DatabaseError] subtype.
+ *
+ * @param e the SQLException to be mapped.
+ *
+ * @return A specific subtype of [AppError.DatabaseError] corresponding to the given SQLException.
+ * Possible outcomes include:
+ * - [DatabaseError.ConstraintViolation]
+ * - [DatabaseError.DiskIOException]
+ * - [DatabaseError.OutOfMemoryException]
+ * - [DatabaseError.UnhandledSQLException]
+ */
+fun mapSQLException(e: SQLException): DatabaseError {
+    return when (e) {
+        is SQLiteConstraintException -> DatabaseError.ConstraintViolation(e.message, e)
+        is SQLiteDiskIOException -> DatabaseError.DiskIOException(e.message, e)
+        is SQLiteFullException -> DatabaseError.OutOfMemoryException(e.message, e)
+        else -> DatabaseError.UnhandledSQLException(e.message, e)
+    }
 }
