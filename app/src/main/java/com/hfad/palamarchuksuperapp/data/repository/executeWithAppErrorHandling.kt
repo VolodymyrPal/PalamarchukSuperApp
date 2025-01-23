df3107c8ebf98e7a67552af7e929b5783bbf72d8
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-23 22:34:43 +0200

    Create wrapper for database call with error returning.

---
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/executeWithAppErrorHandling.kt
@@ -0,0 +1,16 @@
+package com.hfad.palamarchuksuperapp.data.repository
+
+import android.database.SQLException
+import com.hfad.palamarchuksuperapp.domain.models.AppError
+import com.hfad.palamarchuksuperapp.domain.models.Result
+import com.hfad.palamarchuksuperapp.domain.models.mapSQLException
+
+suspend fun <T> executeWithAppErrorHandling(
+    block: suspend () -> T,
+): Result<T, AppError> {
+    return try {
+        Result.Success(block())
+    } catch (e: SQLException) {
+        Result.Error(mapSQLException(e))
+    }
+}
