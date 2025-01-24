22fea4d33e7fb90a02d7267e716c2a6aad8528ae
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-24 22:53:48 +0200

    Now Result use AppError class for errors.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/Result.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/Result.kt
@@ -2,7 +2,7 @@ package com.hfad.palamarchuksuperapp.domain.models
 
 typealias RootError = Error
 
-sealed interface Result<out D, out E: RootError> {
-    data class Success<out D, out E: RootError>(val data: D) : Result<D, E>
-    data class Error<out D, out E: RootError>(val error: E, val data: D? = null) : Result<D, E>
+sealed interface Result<out D, out E : AppError> {
+    data class Success<out D, out E : AppError>(val data: D) : Result<D, E>
+    data class Error<out D, out E : AppError>(val error: E, val data: D? = null) : Result<D, E>
 }
