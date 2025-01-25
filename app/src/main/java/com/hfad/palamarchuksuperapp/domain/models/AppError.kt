06df02f0d85a5fb96c4016408c5b07fe3c86ac6d
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-25 23:51:06 +0200

    Update class to separate business logic from data.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AppError.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/models/AppError.kt
@@ -1,17 +1,11 @@
 package com.hfad.palamarchuksuperapp.domain.models
 
-import android.database.SQLException
-import android.database.sqlite.SQLiteConstraintException
-import android.database.sqlite.SQLiteDiskIOException
-import android.database.sqlite.SQLiteFullException
-import com.hfad.palamarchuksuperapp.domain.models.AppError.DatabaseError
+sealed interface AppError : Error {
 
-sealed class AppError(
-    override val cause: Throwable? = null,
-    override val message: String? = null,
-) : Throwable(message, cause), Error {
+    data class CustomError(val errorText: String? = null, val error: Throwable? = null) : AppError
 
-    sealed class NetworkException : AppError() {
+    sealed interface NetworkException : AppError {
+
         /**
          * Represents a sealed hierarchy of request-related errors.
          * Each subclass corresponds to a specific type of request error.
@@ -20,20 +14,20 @@ sealed class AppError(
          * @property cause The root cause of the error, if available (optional).
          */
         sealed class RequestError(
-            override val message: String? = null,
-            override val cause: Throwable? = null,
-        ) : NetworkException() {
+            val message: String? = null,
+            val cause: Throwable? = null,
+        ) : NetworkException {
             class BadRequest(message: String? = null, cause: Throwable? = null) :
-                RequestError(message, cause)//400
+                RequestError(message, cause) //400
 
             class Unauthorized(message: String? = null, cause: Throwable? = null) :
-                RequestError(message, cause)//401
+                RequestError(message, cause) //401
 
             class Forbidden(message: String? = null, cause: Throwable? = null) :
-                RequestError(message, cause)//403 лимиты, ограничения по IP
+                RequestError(message, cause) //403 лимиты, ограничения по IP
 
             class NotFound(message: String? = null, cause: Throwable? = null) :
-                RequestError(message, cause)//404
+                RequestError(message, cause) //404
         }
 
         /**
@@ -44,9 +38,9 @@ sealed class AppError(
          * @property cause The root cause of the error, if available (optional).
          */
         sealed class ServerError(
-            override val message: String? = null,
-            override val cause: Throwable? = null,
-        ) : NetworkException() {
+            val message: String? = null,
+            val cause: Throwable? = null,
+        ) : NetworkException {
             class InternalServerError(message: String? = null, cause: Throwable? = null) :
                 ServerError(message, cause)//500
 
@@ -62,11 +56,6 @@ sealed class AppError(
 
     }
 
-    data class CustomError(val errorText: String? = null, val error: Throwable? = null) : AppError(
-        cause = error,
-        message = errorText
-    )
-
     /**
      * Represents a sealed hierarchy of database-related errors.
      * Each subclass corresponds to a specific type of database error.
@@ -76,11 +65,13 @@ sealed class AppError(
      */
 
     sealed class DatabaseError(
-        override val message: String? = null, override val cause: Throwable? = null,
-    ) : AppError(cause, message) {
+        val message: String? = null, val cause: Throwable? = null,
+    ) : AppError {
 
         /**
          * Represents a general SQL error or other database-related issue.
+         *
+         * This error is used as a fallback for exceptions that do not match other specific error types.
          *
          * @param message Detailed error message (optional).
          * @param cause Root cause of the error, if available (optional).
@@ -120,59 +111,44 @@ sealed class AppError(
             message: String? = null,
             cause: Throwable? = null,
         ) : DatabaseError(message, cause)
+
+        /**
+         * Represents a permission error, such as an attempt to access a database file without proper permissions.
+         *
+         * This error occurs when the database file is inaccessible due to file system permissions.
+         *
+         * @param message Detailed error message (optional).
+         * @param cause Root cause of the error, if available (optional).
+         */
+        class AccessPermissionError(
+            message: String? = null,
+            cause: Throwable? = null,
+        ) : DatabaseError(message, cause)
+
+        /**
+         * Represents an error caused by attempting to modify a database in read-only mode.
+         *
+         * This error occurs when a write operation is attempted on a read-only database connection.
+         *
+         * @param message Detailed error message (optional).
+         * @param cause Root cause of the error, if available (optional).
+         */
+        class ReadOnlyDatabaseError(
+            message: String? = null,
+            cause: Throwable? = null,
+        ) : DatabaseError(message, cause)
+
+        /**
+         * Represents a database corruption error.
+         *
+         * This error is triggered when the database file is found to be corrupted, making operations unreliable.
+         *
+         * @param message Detailed error message (optional).
+         * @param cause Root cause of the error, if available (optional).
+         */
+        class DatabaseCorruptionError(
+            message: String? = null,
+            cause: Throwable? = null,
+        ) : DatabaseError(message, cause)
     }
 }
-
-/** Maps an [SQLException] to a specific [AppError.DatabaseError] subtype.
- *
- * @param e the SQLException to be mapped.
- *
- * @return A specific subtype of [AppError.DatabaseError] corresponding to the given SQLException.
- * Possible outcomes include:
- * - [DatabaseError.ConstraintViolation]
- * - [DatabaseError.DiskIOException]
- * - [DatabaseError.OutOfMemoryException]
- * - [DatabaseError.UnhandledSQLException]
- */
-fun mapSQLException(e: SQLException): DatabaseError {
-    return when (e) {
-        is SQLiteConstraintException -> DatabaseError.ConstraintViolation(e.message, e)
-        is SQLiteDiskIOException -> DatabaseError.DiskIOException(e.message, e)
-        is SQLiteFullException -> DatabaseError.OutOfMemoryException(e.message, e)
-        else -> DatabaseError.UnhandledSQLException(e.message, e)
-    }
-}
-
-/** Maps an [Exception] to a specific [AppError.NetworkException] subtype.
- *
- * @param e the Exception to be mapped.
- *
- * @return A specific subtype of [AppError.NetworkException] corresponding to the given Exception.
- * Possible outcomes include:
- * - [AppError.NetworkException.RequestError.BadRequest]
- * - [AppError.NetworkException.RequestError.Unauthorized]
- * - [AppError.NetworkException.RequestError.Forbidden]
- * - [AppError.NetworkException.ServerError.InternalServerError]
- * - [AppError.NetworkException.ServerError.BadGateway]
- * - [AppError.NetworkException.ServerError.ServiceUnavailable]
- * - [AppError.NetworkException.ServerError.GatewayTimeout]
- * - [AppError.CustomError]
- */
-fun mapNetworkRequestException(e: Exception): AppError {
-    return when (e) {
-        is HttpStatusCodeError -> when (e.value) {
-            400 -> AppError.NetworkException.RequestError.BadRequest(e.message, e)
-            401 -> AppError.NetworkException.RequestError.Unauthorized(e.message, e)
-            403 -> AppError.NetworkException.RequestError.Forbidden(e.message, e)
-            500 -> AppError.NetworkException.ServerError.InternalServerError(e.message, e)
-            502 -> AppError.NetworkException.ServerError.BadGateway(e.message, e)
-            503 -> AppError.NetworkException.ServerError.ServiceUnavailable(e.message, e)
-            504 -> AppError.NetworkException.ServerError.GatewayTimeout(e.message, e)
-            else -> AppError.CustomError("Неизвестная ошибка", e)
-        }
-
-        else -> AppError.CustomError(error = e)
-    }
-}
-
-class HttpStatusCodeError(val value: Int) : Exception()
