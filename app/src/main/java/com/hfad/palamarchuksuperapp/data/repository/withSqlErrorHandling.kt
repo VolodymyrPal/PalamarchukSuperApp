22619c167ca774f7a5b93bf2d947985c3363148d
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-25 23:54:40 +0200

    Rename to show better logic.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/withSqlErrorHandling.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/withSqlErrorHandling.kt
@@ -3,9 +3,8 @@ package com.hfad.palamarchuksuperapp.data.repository
 import android.database.SQLException
 import com.hfad.palamarchuksuperapp.domain.models.AppError
 import com.hfad.palamarchuksuperapp.domain.models.Result
-import com.hfad.palamarchuksuperapp.domain.models.mapSQLException
 
-suspend fun <T> executeDBWithAppErrorHandling(
+suspend fun <T> withSqlErrorHandling(
     block: suspend () -> T,
 ): Result<T, AppError> {
     return try {
