1adb0070a1f24f898a70aae73471c73df3415927
palamarchukhome <palamarchuk.office@gmail.com>
2025-01-31 23:22:36 +0200

    Update repository to return flow.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/ChatAiRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/ChatAiRepository.kt
@@ -12,7 +12,7 @@ interface ChatAiRepository {
 
     // Методы для работы с чатами
     suspend fun getAllChats(): Result<List<MessageChat>, AppError>
-    suspend fun getAllChatsInfo(): Result<List<MessageChat>, AppError>
+    suspend fun getAllChatsInfo(): Result<Flow<List<MessageChat>>, AppError>
     suspend fun getChatWithMessagesById(chatId: Int): Result<MessageChat, AppError>
     suspend fun getChatFlowById(chatId: Int): Result<Flow<MessageChat>, AppError>
     suspend fun createChat(emptyChat: MessageChat): Result<Unit, AppError>
