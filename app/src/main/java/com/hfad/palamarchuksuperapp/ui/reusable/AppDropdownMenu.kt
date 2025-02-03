f3b5dda0d6f54896b2956f15cb5805b320bb8ec7
palamarchukhome <palamarchuk.office@gmail.com>
2025-02-03 21:20:29 +0200

    Create base implementation of AppDialog.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/reusable/AppDropdownMenu.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/reusable/AppDropdownMenu.kt
@@ -1,16 +1,48 @@
 package com.hfad.palamarchuksuperapp.ui.reusable
 
-import androidx.compose.foundation.layout.Box
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -39,34 +71,21 @@ interface AppDialogScope {
 // Внутренний State для хранения элементов
 private class AppDialogState : AppDialogScope {
     var titlePart: (@Composable () -> Unit)? = null
-    var titlePartModifier: Modifier = Modifier
     var contentPart: (@Composable () -> Unit)? = null
-    var contentPartModifier: Modifier = Modifier
     var buttonPart: (@Composable () -> Unit)? = null
-    var buttonPartModifier: Modifier = Modifier
 
-    override fun TitlePart(
-        modifier: Modifier,
-        content: @Composable (() -> Unit),
-    ) {
+    override fun Header(content: @Composable (() -> Unit)) {
         titlePart = content
-        titlePartModifier = modifier
     }
 
-    override fun ContentPart(
-        modifier: Modifier,
-        content: @Composable (() -> Unit),
-    ) {
+    override fun Content(content: @Composable (() -> Unit)) {
         contentPart = content
-        contentPartModifier = modifier
     }
 
-    override fun ButtonPart(
-        modifier: Modifier,
+    override fun Actions(
         content: @Composable (() -> Unit),
     ) {
         buttonPart = content
-        buttonPartModifier = modifier
     }
 
 }
@@ -78,55 +97,28 @@ fun AppDialog(
     dialogProperties: DialogProperties = DialogProperties(),
     content: @Composable AppDialogScope.() -> Unit,
 ) {
-    val dialogState = remember { AppDialogState() }
-    dialogState.content() // Собираем контент через Scope
+    val dialogState = remember { AppDialogState() }.apply { content() }
 
     Dialog(
         onDismissRequest = onDismissRequest,
         properties = dialogProperties
     ) {
         Surface(
-            modifier = modifier.widthIn(max = 480.dp),
-            shape = MaterialTheme.shapes.extraLarge
+            modifier = modifier
+                .widthIn(max = 240.dp)
+                .heightIn(max = 240.dp),
+            shape = MaterialTheme.shapes.extraLarge,
+            color = MaterialTheme.colorScheme.onPrimary,
         ) {
-            Column(modifier = Modifier.padding(24.dp)) {
-
-                dialogState.titlePart?.let { content ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(0.1f)
-                            .then(
-                                dialogState.titlePartModifier
-                            )
-                    ) {
-                        content()
-                    }
-                }
-                dialogState.contentPart?.let { content ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(0.8f)
-                            .then(
-                                dialogState.contentPartModifier
-                            )
-                    ) {
-                        content()
-                    }
-                }
-                dialogState.buttonPart?.let { content ->
-                    Box(
-                        modifier = dialogState.buttonPartModifier
-                            .fillMaxWidth()
-                            .weight(0.1f)
-                            .then(
-                                dialogState.buttonPartModifier
-                            )
-                    ) {
-                        content()
-                    }
-                }
+            Column(
+                modifier = Modifier
+                    .padding(24.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                dialogState.titlePart?.invoke()
+                dialogState.contentPart?.invoke()
+                dialogState.buttonPart?.invoke()
             }
         }
     }
