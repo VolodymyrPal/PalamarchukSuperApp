51aa2a080ca03a79173fb01634b257445a744f68
palamarchukhome <palamarchuk.office@gmail.com>
2025-02-12 18:42:47 +0200

    Add interface repository for messageAi.

---
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/MessageAiRepository.kt
@@ -0,0 +1,18 @@
+package com.hfad.palamarchuksuperapp.domain.repository
+
+import com.hfad.palamarchuksuperapp.data.entities.MessageAiEntity
+import com.hfad.palamarchuksuperapp.data.entities.MessageStatus
+import com.hfad.palamarchuksuperapp.domain.models.AppError
+import com.hfad.palamarchuksuperapp.domain.models.MessageAI
+import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
+import com.hfad.palamarchuksuperapp.domain.models.Result
+
+interface MessageAiRepository {
+    suspend fun addMessageAiEntity(messageAiEntity: MessageAiEntity) : Result<Long, AppError>
+    suspend fun addAndGetMessageAi(messageAI: MessageAI): Result<MessageAI, AppError>
+    suspend fun updateMessageAi(messageAI: MessageAI): Result<Unit, AppError>
+    suspend fun getAllMessagesWithStatus(
+        chatId: Int,
+        status: MessageStatus,
+    ): Result<List<MessageAI>, AppError>
+}
