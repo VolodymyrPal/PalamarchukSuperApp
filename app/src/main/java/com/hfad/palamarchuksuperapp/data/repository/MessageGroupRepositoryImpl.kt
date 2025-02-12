55c818524a1e310050c87a18fb15a37be5d3afd7
palamarchukhome <palamarchuk.office@gmail.com>
2025-02-12 18:47:50 +0200

    Create interface implementation for MessageGroup.

---
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/MessageGroupRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package com.hfad.palamarchuksuperapp.data.repository
+
+import com.hfad.palamarchuksuperapp.data.dao.MessageGroupDao
+import com.hfad.palamarchuksuperapp.data.entities.MessageGroupEntity
+import com.hfad.palamarchuksuperapp.domain.models.AppError
+import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
+import com.hfad.palamarchuksuperapp.domain.models.Result
+import com.hfad.palamarchuksuperapp.domain.repository.MessageGroupRepository
+
+class MessageGroupRepositoryImpl(
+    private val messageGroupDao: MessageGroupDao,
+) : MessageGroupRepository {
+
+    override suspend fun getMessageGroup(chatId: Int): Result<MessageGroup, AppError> {
+        return withSqlErrorHandling {
+            MessageGroup.from(
+                messageGroupDao.getMessageGroup(chatId)
+            )
+        }
+    }
+
+    override suspend fun addMessageGroupEntity(messageGroupEntity: MessageGroupEntity): Result<Long, AppError> {
+        return withSqlErrorHandling {
+            messageGroupDao.insertMessageGroup(messageGroupEntity)
+        }
+    }
+
+    override suspend fun updateMessageGroupEntity(messageGroupEntity: MessageGroupEntity): Result<Unit, AppError> {
+        return withSqlErrorHandling {
+            messageGroupDao.updateMessageGroup(messageGroupEntity)
+        }
+    }
+
+}
