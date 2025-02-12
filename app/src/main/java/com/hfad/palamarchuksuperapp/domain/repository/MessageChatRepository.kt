2614e5df6fc360552cd67d64c65994eecff3eea4
palamarchukhome <palamarchuk.office@gmail.com>
2025-02-12 18:44:53 +0200

    Separate repository class to deal with crp and KISS principles.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/MessageChatRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/MessageChatRepository.kt
@@ -1,14 +1,11 @@
 package com.hfad.palamarchuksuperapp.domain.repository
 
-import com.hfad.palamarchuksuperapp.data.entities.MessageStatus
 import com.hfad.palamarchuksuperapp.domain.models.AppError
-import com.hfad.palamarchuksuperapp.domain.models.MessageAI
 import com.hfad.palamarchuksuperapp.domain.models.MessageChat
-import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
 import com.hfad.palamarchuksuperapp.domain.models.Result
 import kotlinx.coroutines.flow.Flow
 
-interface ChatAiRepository {
+interface MessageChatRepository {
 
     // Методы для работы с чатами
     suspend fun getAllChats(): Result<List<MessageChat>, AppError>
@@ -21,14 +18,4 @@ interface ChatAiRepository {
     suspend fun deleteChat(chatId: Int): Result<Unit, AppError>
     suspend fun clearAllChats(): Result<Unit, AppError>
     suspend fun isChatExist(chatId: Int): Result<Boolean, AppError>
-
-    // Методы для работы с группами сообщений
-    suspend fun getMessageGroup(chatId: Int): Result<MessageGroup, AppError>
-    suspend fun addMessageGroup(messageGroupWithChatID: MessageGroup): Result<Long, AppError>
-    suspend fun updateMessageGroup(messageGroup: MessageGroup): Result<Unit, AppError>
-
-    // Методы для работы с сообщениями
-    suspend fun addAndGetMessageAi(messageAI: MessageAI): Result<MessageAI, AppError>
-    suspend fun updateMessageAi(messageAI: MessageAI): Result<Unit, AppError>
-    suspend fun getAllMessagesWithStatus(chatId: Int, status: MessageStatus): Result<List<MessageAI>, AppError>
 }
