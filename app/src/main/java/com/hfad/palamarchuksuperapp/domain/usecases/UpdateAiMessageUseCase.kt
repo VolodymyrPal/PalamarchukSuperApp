f107fd69d2b69b1267618993757aa7e6a7b58350
palamarchukhome <palamarchuk.office@gmail.com>
2025-02-12 18:48:51 +0200

    Update use case for messages ai.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/UpdateAiMessageUseCase.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/usecases/UpdateAiMessageUseCase.kt
@@ -3,7 +3,7 @@ package com.hfad.palamarchuksuperapp.domain.usecases
 import com.hfad.palamarchuksuperapp.domain.models.AppError
 import com.hfad.palamarchuksuperapp.domain.models.MessageAI
 import com.hfad.palamarchuksuperapp.domain.models.Result
-import com.hfad.palamarchuksuperapp.domain.repository.ChatAiRepository
+import com.hfad.palamarchuksuperapp.domain.repository.MessageAiRepository
 import javax.inject.Inject
 
 interface UpdateAiMessageUseCase {
@@ -11,11 +11,11 @@ interface UpdateAiMessageUseCase {
 }
 
 class UpdateAiMessageUseCaseImpl @Inject constructor(
-    private val chatAiRepository: ChatAiRepository,
+    private val messageAiRepository: MessageAiRepository,
 ) : UpdateAiMessageUseCase {
 
     override suspend operator fun invoke(messageAI: MessageAI): Result<Unit, AppError> {
-        val updatedMessage = chatAiRepository.updateMessageAi(messageAI).getOrHandleAppError {
+        val updatedMessage = messageAiRepository.updateMessageAi(messageAI).getOrHandleAppError {
             return Result.Error(it)
         }
         return Result.Success(updatedMessage)
