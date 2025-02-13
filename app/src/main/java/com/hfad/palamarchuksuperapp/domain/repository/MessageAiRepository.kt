e69fd0e471c28069c9608dc3e93a140c6b45820f
palamarchukhome <palamarchuk.office@gmail.com>
2025-02-13 17:26:10 +0200

    Move interface implementation to repository package of data.

--- app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/MessageAiRepository.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/domain/repository/MessageAiRepository.kt
@@ -4,11 +4,10 @@ import com.hfad.palamarchuksuperapp.data.entities.MessageAiEntity
 import com.hfad.palamarchuksuperapp.data.entities.MessageStatus
 import com.hfad.palamarchuksuperapp.domain.models.AppError
 import com.hfad.palamarchuksuperapp.domain.models.MessageAI
-import com.hfad.palamarchuksuperapp.domain.models.MessageGroup
 import com.hfad.palamarchuksuperapp.domain.models.Result
 
 interface MessageAiRepository {
-    suspend fun addMessageAiEntity(messageAiEntity: MessageAiEntity) : Result<Long, AppError>
+    suspend fun addMessageAiEntity(messageAiEntity: MessageAiEntity): Result<Long, AppError>
     suspend fun addAndGetMessageAi(messageAI: MessageAI): Result<MessageAI, AppError>
     suspend fun updateMessageAi(messageAI: MessageAI): Result<Unit, AppError>
     suspend fun getAllMessagesWithStatus(
