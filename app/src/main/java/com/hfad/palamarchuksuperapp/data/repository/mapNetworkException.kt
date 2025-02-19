161b9e71390f2d00182f7b7263f645b042953278
palamarchukhome <palamarchuk.office@gmail.com>
2025-02-19 18:57:42 +0200

    Using new ApiError sealed class.

--- app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/mapNetworkException.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/data/repository/mapNetworkException.kt
@@ -2,13 +2,13 @@ package com.hfad.palamarchuksuperapp.data.repository
 
 import com.hfad.palamarchuksuperapp.domain.models.AppError
 import com.hfad.palamarchuksuperapp.domain.models.AppError.CustomError
-import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.RequestError.BadRequest
-import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.RequestError.Forbidden
-import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.RequestError.Unauthorized
-import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ServerError.BadGateway
-import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ServerError.GatewayTimeout
-import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ServerError.InternalServerError
-import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ServerError.ServiceUnavailable
+import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ApiError.BadApi
+import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ApiError.Forbidden
+import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ApiError.Unauthorized
+import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ApiError.BadGateway
+import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ApiError.GatewayTimeout
+import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ApiError.InternalApiError
+import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.ApiError.ServiceUnavailable
 
 /** Maps an [Exception] to a specific [AppError.NetworkException] subtype.
  *
@@ -16,22 +16,22 @@ import com.hfad.palamarchuksuperapp.domain.models.AppError.NetworkException.Serv
  *
  * @return A specific subtype of [AppError.NetworkException] corresponding to the given Exception.
  * Possible outcomes include:
- * - [AppError.NetworkException.RequestError.BadRequest]
- * - [AppError.NetworkException.RequestError.Unauthorized]
- * - [AppError.NetworkException.RequestError.Forbidden]
- * - [AppError.NetworkException.ServerError.InternalServerError]
- * - [AppError.NetworkException.ServerError.BadGateway]
- * - [AppError.NetworkException.ServerError.ServiceUnavailable]
- * - [AppError.NetworkException.ServerError.GatewayTimeout]
+ * - [AppError.NetworkException.ApiError.BadApi]
+ * - [AppError.NetworkException.ApiError.Unauthorized]
+ * - [AppError.NetworkException.ApiError.Forbidden]
+ * - [AppError.NetworkException.ApiError.InternalApiError]
+ * - [AppError.NetworkException.ApiError.BadGateway]
+ * - [AppError.NetworkException.ApiError.ServiceUnavailable]
+ * - [AppError.NetworkException.ApiError.GatewayTimeout]
  * - [AppError.CustomError]
  */
 fun mapNetworkCodeToException(e: Exception): AppError {
     return when (e) {
         is HttpStatusCodeError -> when (e.value) {
-            400 -> BadRequest(e.message, e)
+            400 -> BadApi(e.message, e)
             401 -> Unauthorized(e.message, e)
             403 -> Forbidden(e.message, e)
-            500 -> InternalServerError(e.message, e)
+            500 -> InternalApiError(e.message, e)
             502 -> BadGateway(e.message, e)
             503 -> ServiceUnavailable(e.message, e)
             504 -> GatewayTimeout(e.message, e)
