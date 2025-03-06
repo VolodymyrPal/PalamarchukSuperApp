bee3cfd69482195bcecd1ed04bb80a67859b7cbb
palamarchukhome <palamarchuk.office@gmail.com>
2025-03-06 22:55:59 +0200

    Update main screen, create box for orders.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/compose/BoneScreen.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/compose/BoneScreen.kt
@@ -2,12 +2,44 @@ package com.hfad.palamarchuksuperapp.ui.compose
 
 import android.content.Context
 import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Divider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.example.compose.AppTheme
+import com.hfad.palamarchuksuperapp.ui.reusable.elements.AppText
+import com.hfad.palamarchuksuperapp.ui.reusable.elements.appTextConfig
+import com.hfad.palamarchuksuperapp.ui.viewModels.BusinessEntity
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
@@ -27,7 +59,7 @@ fun BoneScreenRoot(
                 this.rememberSharedContentState("bone"),
                 animatedContentScope,
             ),
-            viewModel = ViewModel(),
+//            viewModel = ViewModel(),
             navController = navController,
         )
     }
@@ -36,8 +68,61 @@ fun BoneScreenRoot(
 @Composable
 fun BoneScreen(
     modifier: Modifier = Modifier,
-    viewModel: ViewModel? = null,
-    navController: NavController = LocalNavController.current
+//    viewModel: ViewModel? = null,
+    navController: NavController? = null,
 ) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+
+        },
+        floatingActionButton = {
+
+        },
+        bottomBar = {
+
+        }
+    ) { paddingValues ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = paddingValues.calculateTopPadding()
+                )
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                AppText(
+                    modifier = Modifier.padding(8.dp),
+                    value = "Orders",
+                    appTextConfig = appTextConfig(
+                        textStyle = MaterialTheme.typography.displayMedium,
+                    )
+                )
+                val items = listOf(
+                    BusinessEntity(name = "43222", manager = "VP +3806338875"),
+                    BusinessEntity(name = "42224", manager = "VP +3806338875"),
+                    BusinessEntity(name = "43226", manager = "VP +3806338875")
+                )
+                Box(modifier = Modifier.padding(16.dp)) {
+                    LazyColumn {
+                        items(items = items) {
+                            OrderCard(
+                                modifier = Modifier
+                                    .padding(top = 8.dp)
+                                    .shadow(
+                                        elevation = 8.dp,
+                                    ),
+                                entity = it
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 }
