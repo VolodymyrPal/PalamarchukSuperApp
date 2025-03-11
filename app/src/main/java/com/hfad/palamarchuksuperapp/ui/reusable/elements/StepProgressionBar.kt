64c631f18c009f7b79558abd0c76c663999d765c
palamarchukhome <palamarchuk.office@gmail.com>
2025-03-11 22:19:21 +0200

    Set painter for different service type, add text measurer for placing text fields, other properties.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/reusable/elements/StepProgressionBar.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/reusable/elements/StepProgressionBar.kt
@@ -1,13 +1,31 @@
 package com.hfad.palamarchuksuperapp.ui.reusable.elements
 
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -15,12 +33,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.compose.AppTheme
-import com.hfad.palamarchuksuperapp.ui.viewModels.OrderService
+import com.hfad.palamarchuksuperapp.R
+import com.hfad.palamarchuksuperapp.ui.viewModels.BusinessEntity
+import com.hfad.palamarchuksuperapp.ui.viewModels.ServiceType
 import com.hfad.palamarchuksuperapp.ui.viewModels.Stepper
+import com.hfad.palamarchuksuperapp.ui.viewModels.StepperStatus
+import com.hfad.palamarchuksuperapp.ui.viewModels.orderServiceList
 
 @Composable
 fun StepProgressionBar(
@@ -29,8 +57,27 @@ fun StepProgressionBar(
     currentStep: Int = 0,
 ) {
 
-    val stepperHeight = 36.dp
-    val painter = rememberVectorPainter(Icons.Default.Check)
+    val painterServiceTypeMap = ServiceType.entries.associateWith {
+        when (it) {
+            ServiceType.FREIGHT -> painterResource(R.drawable.sea_freight)
+            ServiceType.FORWARDING -> painterResource(R.drawable.freight)
+            ServiceType.STORAGE -> painterResource(R.drawable.warehouse)
+            ServiceType.PRR -> painterResource(R.drawable.loading_boxes)
+            ServiceType.CUSTOMS -> painterResource(R.drawable.freight)
+            ServiceType.TRANSPORT -> painterResource(R.drawable.truck)
+            ServiceType.EUROPE_TRANSPORT -> painterResource(R.drawable.truck)
+            ServiceType.UKRAINE_TRANSPORT -> painterResource(R.drawable.truck)
+            ServiceType.OTHER -> rememberVectorPainter(Icons.Default.Search)
+        }
+    }
+
+    val painterStatusDone = rememberVectorPainter(image = Icons.Default.Check)
+    val textMeasurer = rememberTextMeasurer()
+    val textStyle =
+        MaterialTheme.typography.bodySmall.copy(fontSize = 8.sp, letterSpacing = -0.4.sp)
+    val lineColorPrimary = MaterialTheme.colorScheme.primary
+
+
 
     Canvas(
         modifier = modifier
