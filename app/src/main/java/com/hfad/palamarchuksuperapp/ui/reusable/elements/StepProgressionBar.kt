aa7f733704cb930dea2480067ac01563953d71c4
palamarchukhome <palamarchuk.office@gmail.com>
2025-03-20 18:40:28 +0200

    Remove redundant elements.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/reusable/elements/StepProgressionBar.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/reusable/elements/StepProgressionBar.kt
@@ -1,46 +1,21 @@
 package com.hfad.palamarchuksuperapp.ui.reusable.elements
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -48,7 +23,6 @@ import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
@@ -57,10 +31,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compose.AppTheme
-import com.hfad.palamarchuksuperapp.R
-import com.hfad.palamarchuksuperapp.ui.compose.BoneScreen
-import com.hfad.palamarchuksuperapp.ui.viewModels.BusinessEntity
-import com.hfad.palamarchuksuperapp.ui.viewModels.ServiceType
+import com.hfad.palamarchuksuperapp.ui.compose.boneScreen.BoneScreen
+import com.hfad.palamarchuksuperapp.ui.compose.boneScreen.painterServiceTypeMap
 import com.hfad.palamarchuksuperapp.ui.viewModels.Stepper
 import com.hfad.palamarchuksuperapp.ui.viewModels.StepperStatus
 import com.hfad.palamarchuksuperapp.ui.viewModels.orderServiceList
@@ -267,149 +239,6 @@ fun StepProgressionBar(
 }
 
 @Composable
-fun OrderCard(
-    modifier: Modifier = Modifier,
-    entity: BusinessEntity,
-) {
-    var expanded = remember { mutableStateOf(false) }
-
-    val rotationState by animateFloatAsState(
-        targetValue = if (expanded.value) 180f else 0f,
-        label = "rotationAnimation"
-    )
-
-    val elevation by animateDpAsState(
-        targetValue = if (expanded.value) 6.dp else 2.dp,
-        label = "elevationAnimation"
-    )
-
-    val colorScheme = MaterialTheme.colorScheme
-
-
-    Card(
-        modifier = modifier
-            .fillMaxWidth()
-            .clickable(
-                onClick = {
-                    expanded.value = !expanded.value
-                },
-                indication = null,
-                interactionSource = MutableInteractionSource()
-            ),
-        shape = MaterialTheme.shapes.medium,
-        border = BorderStroke(1.dp, colorScheme.outline),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.7f),
-            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
-        ),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 0.dp
-        ),
-    ) {
-        val containerIcon = painterResource(R.drawable.container_svgrepo_com)
-
-        Column {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-
-                Icon(
-                    painter = painterResource(R.drawable.container_svgrepo_com),
-                    contentDescription = "Container Icon",
-                    tint = Color.Unspecified,
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .size(24.dp)
-                )
-
-                AppText(
-                    modifier = Modifier,
-                    value = "Order: ${entity.name}",
-                    appTextConfig = appTextConfig(
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                )
-                Icon(
-                    imageVector = Icons.Default.ArrowDropDown,
-                    contentDescription = "Expand",
-                    modifier = Modifier.rotate(rotationState),
-                )
-            }
-
-            AnimatedVisibility(
-                enter = fadeIn(animationSpec = tween(300)) +
-                        expandVertically(
-                            expandFrom = Alignment.Top,
-                            animationSpec = tween(300)
-                        ),
-                exit = fadeOut(animationSpec = tween(300)) +
-                        shrinkVertically(
-                            shrinkTowards = Alignment.Top,
-                            animationSpec = tween(300)
-                        ),
-                visible = expanded.value
-            ) {
-                Column(modifier = Modifier.padding(horizontal = 16.dp)) {
-                    TableOrderInfo(
-                        modifier = Modifier.fillMaxWidth(),
-                        orderInfoList = listOf(
-                            OrderInfo(
-                                "Origin",
-                                "Shanghai, China",
-                                painterResource(R.drawable.freight)
-                            ),
-                            OrderInfo(
-                                "Destination",
-                                "Odessa, Ukraine",
-                                painterResource(R.drawable.truck)
-                            ),
-                            OrderInfo("Status", "In Transit", painterResource(R.drawable.truck)),
-                            OrderInfo("ETA", "24.02.2025", painterResource(R.drawable.freight)),
-                            OrderInfo(
-                                "Container",
-                                "40HC-7865425",
-                                painterResource(R.drawable.container_svgrepo_com)
-                            ),
-                            OrderInfo("Cargo", "Electronics", painterResource(R.drawable.warehouse))
-                        )
-                    )
-                }
-            }
-
-            StepProgressionBar(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(64.dp)
-                    .padding(start = 12.dp, end = 12.dp, bottom = 8.dp),
-                listOfSteps = orderServiceList.subList(0, 8),
-                currentStep = 2
-            )
-        }
-    }
-}
-
-@Composable
-fun painterServiceTypeMap() = ServiceType.entries.associateWith {
-    when (it) {
-        ServiceType.FREIGHT -> painterResource(R.drawable.sea_freight)
-        ServiceType.FORWARDING -> painterResource(R.drawable.freight)
-        ServiceType.STORAGE -> painterResource(R.drawable.warehouse)
-        ServiceType.PRR -> painterResource(R.drawable.loading_boxes)
-        ServiceType.CUSTOMS -> painterResource(R.drawable.freight)
-        ServiceType.TRANSPORT -> painterResource(R.drawable.truck)
-        ServiceType.EUROPE_TRANSPORT -> painterResource(R.drawable.truck)
-        ServiceType.UKRAINE_TRANSPORT -> painterResource(R.drawable.truck)
-        ServiceType.OTHER -> rememberVectorPainter(Icons.Default.Search)
-    }
-}
-
-@Composable
 @Preview
 fun StepProgressionBarNewPreview(
 
@@ -428,13 +257,5 @@ fun StepProgressionBarNewPreview(
                 currentStep = 2
             )
         }
-    }
-}
-
-@Preview
-@Composable
-fun BoneScreenPreview() {
-    AppTheme(useDarkTheme = false) {
-        BoneScreen()
     }
 }
