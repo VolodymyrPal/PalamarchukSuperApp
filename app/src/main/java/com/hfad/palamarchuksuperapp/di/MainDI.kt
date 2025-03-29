31564254d8bc93d15ddf0ac6479eb64a3921c28b
palamarchukhome <palamarchuk.office@gmail.com>
2025-03-29 22:48:50 +0200

    Don't need to provide http client in app module, now core module produce it.

--- app/src/main/java/com/hfad/palamarchuksuperapp/di/MainDI.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/di/MainDI.kt
@@ -228,8 +228,10 @@ abstract class ViewModelsModule {
 @Module
 object NetworkModule {
     @Provides
-    fun platziApiImpl(): FakeStoreApi {
-        return FakeStoreApiRepository(provideHttpClient())
+    fun platziApiImpl(
+        httpClient: HttpClient,
+    ): FakeStoreApi {
+        return FakeStoreApiRepository(httpClient)
     }
 
     @Provides
@@ -238,45 +240,6 @@ object NetworkModule {
         storeDao: StoreDao,
     ): StoreRepository {
         return StoreRepositoryImpl(storeApi, storeDao)
-    }
-
-    @Singleton
-    @Provides
-    fun provideHttpClient(): HttpClient {
-        return HttpClient(CIO) {
-            install(Logging) {
-                logger = Logger.SIMPLE
-                level = LogLevel.ALL
-            }
-            engine {
-                endpoint {
-                    socketTimeout =
-                        60_000       // Максимальное время ожидания соединения 30 секунд
-                    connectTimeout = 60_000        // Время ожидания соединения 30 секунд
-                    requestTimeout = 60_000       // Максимальное время ожидания запроса 30 секунд
-                    keepAliveTime =
-                        60_0000        // Время жизни соединения после использования 300 секунд
-                    maxConnectionsPerRoute = 10 // Максимум 10 соединений на маршрут
-                    pipelineMaxSize = 10        // Максимум 10 запросов в пайплайне
-                }
-                maxConnectionsCount = 10 // Максимум 10 соединений
-                https {
-                    trustManager // Настройки проверки сертификата, что бы не перехватывать запросы посредине
-                } // Настройки HTTPS, которые
-                // позволяют конфигурировать параметры TLS/SSL, используемые для защищенных соединений.
-                pipelining = false // Отключение пайпелинга
-                proxy // Настройки прокси
-            }
-            install(ContentNegotiation) {
-                json(Json {
-                    ignoreUnknownKeys = true
-                    encodeDefaults = true
-                    prettyPrint = true
-                    isLenient = true  //TODO lenient for testing
-                })
-            }
-            install(HttpCache) // Добавляем кэширование запросов, чтобы не обращаться к API каждый раз
-        }
     }
 }
 
