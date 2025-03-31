c7e8818d54e0cd57a52e843db25d7020819c4573
palamarchukhome <palamarchuk.office@gmail.com>
2025-03-31 18:46:04 +0300

    Update imports with using core library.

--- app/src/main/java/com/hfad/palamarchuksuperapp/ui/screens/SkillsFragment.kt
+++ app/src/main/java/com/hfad/palamarchuksuperapp/ui/screens/SkillsFragment.kt
@@ -1,19 +1,19 @@
 package com.hfad.palamarchuksuperapp.ui.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hfad.palamarchuksuperapp.appComponent
+import com.hfad.palamarchuksuperapp.core.ui.genericViewModel.GenericViewModelFactory
 import com.hfad.palamarchuksuperapp.databinding.FragmentSkillsBinding
-import com.hfad.palamarchuksuperapp.ui.viewModels.GenericViewModelFactory
 import com.hfad.palamarchuksuperapp.domain.models.AppVibrator
 import com.hfad.palamarchuksuperapp.ui.screens.adapters.SkillsListAdapter
 import com.hfad.palamarchuksuperapp.ui.viewModels.SkillsViewModel
