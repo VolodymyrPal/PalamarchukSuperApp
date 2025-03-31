0e20543ed312d5414f48cd828497668b470a77d3
palamarchukhome <palamarchuk.office@gmail.com>
2025-03-31 19:17:06 +0300

    Looking for a way to produce dependencies for gradle modules beyond using fragment or activity?

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/FeatureBoneScreen.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/FeatureBoneScreen.kt
@@ -3,37 +3,31 @@ package com.hfad.palamarchuksuperapp.feature.bone.ui
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.hfad.palamarchuksuperapp.core.ui.composables.basic.AppText
 import com.hfad.palamarchuksuperapp.core.ui.composables.basic.appTextConfig
-import com.hfad.palamarchuksuperapp.core.ui.genericViewModel.daggerViewModel
+import com.hfad.palamarchuksuperapp.feature.bone.di.BoneDeps
 import com.hfad.palamarchuksuperapp.feature.bone.di.BoneDepsProvider
 import com.hfad.palamarchuksuperapp.feature.bone.di.DaggerBoneComponent
-import com.hfad.palamarchuksuperapp.feature.bone.domain.models.BoneFeatureViewModel
 
 @Composable
 fun FeatureBoneScreen() {
-    val component = rememberSaveable {
+    val component = remember {
         DaggerBoneComponent.builder().deps(BoneDepsProvider.deps).build()
     }
 
-    BoneScreen(
-        viewModel = daggerViewModel<BoneFeatureViewModel>(
-            factory = component.viewModelFactory
-        ),
-    )
 
     Surface(
         modifier = Modifier.fillMaxSize(), content =
             {
-
                 AppText(
                     modifier = Modifier,
-                    value = "123",
+                    value = "viewModel.hashCode()",
                     appTextConfig = appTextConfig(
                         textStyle = TextStyle.Default.copy(
                             fontSize = 24.sp,
@@ -45,9 +39,6 @@ fun FeatureBoneScreen() {
     )
 }
 
-@Composable
-fun BoneScreen(
-    viewModel: BoneFeatureViewModel,
-) {
-    FeatureBoneScreen()
+internal val LocalBoneDependencies = compositionLocalOf<BoneDeps> {
+    error("BoneDependencies not provided!")
 }
