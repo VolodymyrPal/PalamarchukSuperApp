787cb3483b7e6d1bffe4b0b80401ea8f771c8cfb
palamarchukhome <palamarchuk.office@gmail.com>
2025-04-13 19:17:55 +0300

    Replace old logic of calculating composable size with SubcomposeLayout to calculate size in Layout stage.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/TableOrderInfo.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/TableOrderInfo.kt
@@ -3,7 +3,10 @@ package com.hfad.palamarchuksuperapp.feature.bone.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
@@ -12,25 +15,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hfad.palamarchuksuperapp.core.ui.composables.basic.AppIconInfoField
+import com.hfad.palamarchuksuperapp.core.ui.theme.AppTheme
 import com.hfad.palamarchuksuperapp.feature.bone.R
 import kotlin.math.max
 
 @Composable
 fun TableOrderInfo(
     modifier: Modifier = Modifier,
-    orderInfoList: List<OrderInfo> = emptyList<OrderInfo>(),
+    orderInfoList: List<OrderInfo> = emptyList(),
 ) {
     BoxWithConstraints(
         modifier = modifier
-            .padding(10.dp)
+            .padding(4.dp)
             .heightIn(max = 2500.dp),
     ) {
         val density = LocalDensity.current
@@ -40,21 +43,19 @@ fun TableOrderInfo(
         }
 
 
-        Box(modifier = Modifier.alpha(0f)) {
-            orderInfoList.forEach { orderInfo ->
-                AppIconInfoField(
-                    modifier = Modifier.onSizeChanged { size ->
-                        if (size.width > maxGridWidth.value) {
-                            maxGridWidth.value = size.width
-                        }
-                    },
-                    icon = orderInfo.icon,
-                    title = orderInfo.title,
-                    description = orderInfo.description
-                )
-            }
+        SubcomposeLayout { constraints ->
+            val placeable = orderInfoList.take(10).map { orderInfo ->
+                subcompose("measure $orderInfo") {
+                    AppIconInfoField(
+                        icon = orderInfo.icon,
+                        title = orderInfo.title,
+                        description = orderInfo.description
+                    )
+                }.map { it.measure(constraints) }
+            }.flatten()
+            maxGridWidth.value = placeable.maxOfOrNull { it.width } ?: 0
+            layout(0, 0) {}
         }
-
 
         LazyVerticalStaggeredGrid(
             modifier = Modifier,
