59e73f36854d5c86178061a22bb855b60be5df13
palamarchukhome <palamarchuk.office@gmail.com>
2025-04-14 17:41:52 +0300

    Passing new Payment statistic data class to ui.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/screens/PaymentsPage.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/screens/PaymentsPage.kt
@@ -1,17 +1,25 @@
 package com.hfad.palamarchuksuperapp.feature.bone.ui.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
@@ -26,25 +34,31 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import com.hfad.palamarchuksuperapp.core.ui.composables.basic.AppText
 import com.hfad.palamarchuksuperapp.core.ui.composables.basic.appTextConfig
 import com.hfad.palamarchuksuperapp.core.ui.theme.AppTheme
+import com.hfad.palamarchuksuperapp.feature.bone.R
+import com.hfad.palamarchuksuperapp.feature.bone.ui.viewModels.PaymentStatistic
 import kotlin.random.Random
 
 @Composable
 fun PaymentsPage(
     modifier: Modifier = Modifier,
+    paymentPageState: PaymentPageState = PaymentPageState(),
 ) {
-    val payments = generateSamplePayments()
 
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
@@ -54,6 +68,7 @@ fun PaymentsPage(
     ) {
         item {
             PaymentsStatistics()
+                paymentPageState.paymentStatistic
         }
         items(payments.size) { index ->
             PaymentCard(
@@ -65,7 +80,9 @@ fun PaymentsPage(
 }
 
 @Composable
-fun PaymentsStatistics() {
+fun PaymentsStatistics(
+    paymentStatistic: PaymentStatistic = PaymentStatistic(),
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
