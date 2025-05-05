cc3956814d338f49539a5ff5ab10bbd2da722b7d
palamarchukhome <palamarchuk.office@gmail.com>
2025-05-05 23:50:35 +0200

    Update imports.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/SalesPageState.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/SalesPageState.kt
@@ -1,7 +1,7 @@
 package com.hfad.palamarchuksuperapp.feature.bone.ui.viewModels
 
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SaleOrder
-import com.hfad.palamarchuksuperapp.feature.bone.ui.screens.SalesStatistics
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SalesStatistics
 import com.hfad.palamarchuksuperapp.feature.bone.ui.composables.generateSampleProductSaleItems
 
 data class SalesPageState(
