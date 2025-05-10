ee9ce138a9c1aa2898d3b8765d93a1ef00e5dc67
palamarchukhome <palamarchuk.office@gmail.com>
2025-05-10 23:47:16 +0200

    Extend each money operation with AmountCurrency class.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/PaymentOrder.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/PaymentOrder.kt
@@ -4,16 +4,14 @@ import java.util.Date
 
 data class PaymentOrder(
     override val id: Int,
-    val amountCurrency: AmountCurrency,
     val factory: String,
     val productType: String,
-    val batchInfo: String,
     val paymentDate: String,
     val dueDate: String,
     val status: PaymentStatus,
-    override val type: TransactionType = TransactionType.CREDIT,
-    override val amount: Float = amountCurrency.amount,
+    override val type: TransactionType,
     override val billingDate: Date = Date(),
+    override val amountCurrency: AmountCurrency,
 ) : TypedTransaction
 
 enum class PaymentStatus {
