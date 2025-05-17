47c7144f435813be02def7f3bff26b84d553a57c
palamarchukhome <palamarchuk.office@gmail.com>
2025-05-17 22:45:30 +0300

    Add exchange rate for data class.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/ExchangeOrder.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/ExchangeOrder.kt
@@ -7,7 +7,7 @@ data class ExchangeOrder(
     val amountToExchange: AmountCurrency,
     val typeToChange: TransactionType = TransactionType.DEBIT,
     val date: Date,
-    val exchangeRate: Float,
+    val exchangeRate: Float = amountToExchange.amount/amountCurrency.amount,
     override val type: TransactionType = TransactionType.CREDIT,
     override val amountCurrency: AmountCurrency,
     override val billingDate: Date,
@@ -21,7 +21,6 @@ fun generateExchangeOrderItems(): List<ExchangeOrder> {
             date = Date(),
             amountCurrency = AmountCurrency(amount = 1000f, currency = Currency.USD),
             billingDate = Date(),
-            exchangeRate = 41f,
             id = Random.nextInt(),
             type = TransactionType.CREDIT,
             typeToChange = TransactionType.DEBIT
@@ -31,7 +30,24 @@ fun generateExchangeOrderItems(): List<ExchangeOrder> {
             date = Date(),
             amountCurrency = AmountCurrency(amount = 80000f, currency = Currency.USD),
             billingDate = Date(),
-            exchangeRate = 80000f,
+            id = Random.nextInt(),
+            type = TransactionType.CREDIT,
+            typeToChange = TransactionType.DEBIT
+        ),
+        ExchangeOrder(
+            amountToExchange = AmountCurrency(amount = 27349.85f, currency = Currency.USD),
+            date = Date(),
+            amountCurrency = AmountCurrency(amount = 24500f, currency = Currency.EUR),
+            billingDate = Date(),
+            id = Random.nextInt(),
+            type = TransactionType.CREDIT,
+            typeToChange = TransactionType.DEBIT
+        ),
+        ExchangeOrder(
+            amountToExchange = AmountCurrency(amount = 11000f, currency = Currency.CNY),
+            date = Date(),
+            amountCurrency = AmountCurrency(amount = 14500f, currency = Currency.PLN),
+            billingDate = Date(),
             id = Random.nextInt(),
             type = TransactionType.CREDIT,
             typeToChange = TransactionType.DEBIT
