6c5ae429ce1243c8e425c20f342714b385483e6b
palamarchukhome <palamarchuk.office@gmail.com>
2025-05-23 18:25:32 +0300

    Add preview to test changes in color.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/theme/FeatureTheme.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/theme/FeatureTheme.kt
@@ -1,4 +1,6 @@
 package com.example.compose
+
+import android.content.res.Configuration
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,6 +13,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ui.theme.AppTypography
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.TransactionType
@@ -22,30 +25,30 @@ fun FeatureTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
-    content: @Composable() () -> Unit
+    content: @Composable() () -> Unit,
 ) {
-  val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-          val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      
-      darkTheme -> darkScheme
-      else -> lightScheme
-  }
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
 
-  MaterialTheme(
-    colorScheme = colorScheme,
-    typography = AppTypography,
-      shapes = shapes,
-    content = content
-  )
+        darkTheme -> darkScheme
+        else -> lightScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = AppTypography,
+        shapes = shapes,
+        content = content
+    )
 }
 
 val shapes = Shapes(
     extraSmall = RoundedCornerShape(1.dp), // For card
     small = RoundedCornerShape(2.dp), // For button
-    medium = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp), //
     large = RoundedCornerShape(24.dp),
     extraLarge = RoundedCornerShape(32.dp)
 )
@@ -136,3 +139,24 @@ private val darkScheme = darkColorScheme(
     surfaceContainerHigh = surfaceContainerHighDark,
     surfaceContainerHighest = surfaceContainerHighestDark,
 )
+
+
+@Preview(name = "Complete Color Scheme Light", showBackground = true)
+@Composable
+fun CompleteColorSchemeLightPreview2() {
+    FeatureTheme(darkTheme = false) {
+        ComprehensiveColorPreview()
+    }
+}
+
+@Preview(
+    name = "Complete Color Scheme Dark",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Composable
+fun CompleteColorSchemeDarkPreview2() {
+    FeatureTheme(darkTheme = true) {
+        ComprehensiveColorPreview()
+    }
+}
