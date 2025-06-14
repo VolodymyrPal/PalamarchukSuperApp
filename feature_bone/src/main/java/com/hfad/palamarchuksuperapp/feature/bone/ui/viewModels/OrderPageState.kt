ffc6f3e2632c5ab7b4a6e52b37cece9f7df62933
palamarchukhome <palamarchuk.office@gmail.com>
2025-06-14 23:50:19 +0300

    Update abstract view model to keep state value.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/OrderPageState.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/OrderPageState.kt
@@ -1,6 +1,6 @@
 package com.hfad.palamarchuksuperapp.feature.bone.ui.viewModels
 
-import com.hfad.palamarchuksuperapp.core.ui.genericViewModel.State
+import com.hfad.palamarchuksuperapp.core.ui.genericViewModel.ScreenState
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistic
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.generateOrderItems
@@ -8,4 +8,4 @@ import com.hfad.palamarchuksuperapp.feature.bone.domain.models.generateOrderItem
 data class OrderPageState(
     val orderMetrics: OrderStatistic = generateOrderStatistic(),// OrderStatistic(), TODO for testing
     val orders: List<Order> = generateOrderItems(),// emptyList(), TODO for testing
-) : State<Order>
+) : ScreenState
