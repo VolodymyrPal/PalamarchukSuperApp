cf5909f80c0262ed91e07f857d346d9b2e0d78fd
palamarchukhome <palamarchuk.office@gmail.com>
2025-06-26 23:40:24 +0300

    Trying to implement Junit 5 to add unit test with app.

--- feature_bone/build.gradle.kts
+++ feature_bone/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlinx.serialization)
+    id("de.mannodermaus.android-junit5") version "1.13.0.0"
 }
 
 android {
@@ -47,6 +48,19 @@ dependencies {
 
     implementation("androidx.biometric:biometric-ktx:1.4.0-alpha02")
     implementation("androidx.security:security-crypto:1.0.0")
+
+//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.2")
+//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.2")
+//    testImplementation("org.junit.jupiter:junit-jupiter-params:5.13.2")
+//    testImplementation("junit:junit:4.13.2")
+//    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.13.0")
+
+
+//    testImplementation(libs.bundles.test)
+//    testRuntimeOnly(libs.junit.platform.launcher)
+//    testImplementation(libs.mockk)
+//    testImplementation("androidx.test:core:${androidXTestVersion}")
+//    testImplementation("io.mockk:mockk:${mockkVersion}")
 
     implementation(project(":core"))
 }
