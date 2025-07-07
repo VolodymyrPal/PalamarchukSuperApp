89e21daaa69f6877283f4f11056e03df17746654
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-07 23:30:06 +0300

    Add repository/ base implementation for orders.

---
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/remote/api/OrdersRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.hfad.palamarchuksuperapp.feature.bone.data.remote.api
+
+import com.hfad.palamarchuksuperapp.core.domain.AppError
+import com.hfad.palamarchuksuperapp.core.domain.AppResult
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistic
+import com.hfad.palamarchuksuperapp.feature.bone.domain.repository.OrdersRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class OrdersRepositoryImpl @Inject constructor(
+
+) : OrdersRepository {
+    override val orderStatistic: AppResult<Flow<OrderStatistic>, AppError> = AppResult.Success(flow {  })
+    override val orders: AppResult<Flow<List<Order>>, AppError> = AppResult.Success(flow {  })
+}
