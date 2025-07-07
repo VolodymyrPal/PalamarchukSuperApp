d2632ad6ee49de29d0c29b3f27e1d253985a1d32
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-07 23:31:03 +0300

    Add repository/ base implementation for payment operations.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/PaymentsRepository.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/PaymentsRepository.kt
@@ -1,5 +1,12 @@
 package com.hfad.palamarchuksuperapp.feature.bone.domain.repository
 
+import com.hfad.palamarchuksuperapp.core.domain.AppError
+import com.hfad.palamarchuksuperapp.core.domain.AppResult
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.PaymentOrder
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.PaymentStatistic
+import kotlinx.coroutines.flow.Flow
+
 interface PaymentsRepository {
-//    fun getPayments(): Result<Flow<List<Payment>, AppError>>
+    val payment: AppResult<Flow<List<PaymentOrder>>, AppError>
+    val paymentStatistic: AppResult<Flow<List<PaymentStatistic>>, AppError>
 }
