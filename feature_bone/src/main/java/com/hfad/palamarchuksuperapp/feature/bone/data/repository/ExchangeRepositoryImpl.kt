5be9a7015d54f2c5244ce90f1b570769727fd112
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-09 23:52:06 +0300

    Exchange repository with implementations.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/repository/ExchangeRepositoryImpl.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/repository/ExchangeRepositoryImpl.kt
@@ -1,14 +1,51 @@
 package com.hfad.palamarchuksuperapp.feature.bone.data.repository
 
+import com.hfad.palamarchuksuperapp.core.data.safeApiCall
+import com.hfad.palamarchuksuperapp.core.data.withSqlErrorHandling
 import com.hfad.palamarchuksuperapp.core.domain.AppError
 import com.hfad.palamarchuksuperapp.core.domain.AppResult
+import com.hfad.palamarchuksuperapp.feature.bone.data.local.dao.BoneDao
+import com.hfad.palamarchuksuperapp.feature.bone.data.remote.api.BoneApi
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.ExchangeOrder
 import com.hfad.palamarchuksuperapp.feature.bone.domain.repository.ExchangeRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ExchangeRepositoryImpl @Inject constructor(
+    private val boneDao: BoneDao,
+    private val boneApi: BoneApi,
 ) : ExchangeRepository {
-    override val exchanges: AppResult<Flow<List<ExchangeOrder>>, AppError> = AppResult.Success(flow {  })
+
+    override val exchanges: AppResult<Flow<List<ExchangeOrder>>, AppError> =
+        trySqlApp {
+            boneDao.exchanges
+        }
+
+    override suspend fun getExchangeById(id: Int): AppResult<ExchangeOrder, AppError> {
+        return withSqlErrorHandling {
+            boneDao.getExchangeById(id)
+        }
+    }
+
+    override suspend fun softRefreshExchanges() {
+        val exchangesResultApi = getExchangesResultApiWithError()
+        if (exchangesResultApi is AppResult.Success) {
+            boneDao.insertOrIgnoreExchanges(exchangesResultApi.data)
+        }
+    }
+
+    override suspend fun hardRefreshExchanges() {
+        boneDao.deleteAllExchanges()
+        val exchangesResultApi = getExchangesResultApiWithError()
+        if (exchangesResultApi is AppResult.Success) {
+            boneDao.insertOrIgnoreExchanges(exchangesResultApi.data)
+        }
+    }
+
+    private suspend fun getExchangesResultApiWithError(): AppResult<List<ExchangeOrder>, AppError> {
+        return safeApiCall {
+            val exchanges: List<ExchangeOrder> = boneApi.getExchangesApi()
+            AppResult.Success(exchanges)
+        }
+    }
 }
