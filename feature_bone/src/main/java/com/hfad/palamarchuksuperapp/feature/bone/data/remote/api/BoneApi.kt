34c961463732c43925123d69b3586706c8ff506f
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-12 23:18:57 +0300

    Add function to sync objects with api call - syncTypedTransactionsInRange.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/remote/api/BoneApi.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/remote/api/BoneApi.kt
@@ -1,7 +1,6 @@
 package com.hfad.palamarchuksuperapp.feature.bone.data.remote.api
 
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.ExchangeOrder
-import com.hfad.palamarchuksuperapp.feature.bone.domain.models.FinanceStatistic
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistic
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.PaymentOrder
@@ -9,6 +8,7 @@ import com.hfad.palamarchuksuperapp.feature.bone.domain.models.PaymentStatistic
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SaleOrder
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SalesStatistics
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.TypedTransaction
+import java.util.Date
 
 interface BoneApi {
     fun getOrdersApi(): List<Order>
@@ -23,9 +23,18 @@ interface BoneApi {
     fun getPaymentById(id: Int): PaymentOrder
     fun getPaymentStatisticApi(): PaymentStatistic
 
-    fun getOperationsApi(): List<TypedTransaction>
-    fun getFinanceStatisticApi(): FinanceStatistic
-
     fun getExchangesApi(): List<ExchangeOrder>
     fun getExchangeById(id: Int): ExchangeOrder
+
+    fun syncTypedTransactionsInRange(
+        from: Date,
+        to: Date,
+        transactionHashesByTypeAndId: Map<String, String>, // key = "${type}:${id}", value = backand hash
+    ): SyncResponse
 }
+
+data class SyncResponse(
+    val new: List<TypedTransaction>,
+    val updated: List<TypedTransaction>,
+    val deleted: List<String>,
+)
