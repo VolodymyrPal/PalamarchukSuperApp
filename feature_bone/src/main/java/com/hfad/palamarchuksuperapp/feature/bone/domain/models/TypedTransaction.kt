d361cff08839e06c924c29deb324ca991e28cc33
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-12 23:16:13 +0300

    Add type and versionHash of each typedTransaction to control version of each request with polymorphic access.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/TypedTransaction.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/TypedTransaction.kt
@@ -3,10 +3,12 @@ package com.hfad.palamarchuksuperapp.feature.bone.domain.models
 import java.util.Date
 
 sealed interface TypedTransaction {
-    val type: TransactionType
+    val type: String
+    val transactionType: TransactionType
     val id: Int
     val amountCurrency: AmountCurrency
     val billingDate: Date
+    val versionHash: String
 }
 
 enum class TransactionType {
