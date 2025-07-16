04dfcbe662b89b6ce4d5051431f000d21b6eb13c
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-16 22:29:20 +0300

    Add range operations name.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/OrdersRepository.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/OrdersRepository.kt
@@ -5,10 +5,12 @@ import com.hfad.palamarchuksuperapp.core.domain.AppResult
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistic
 import kotlinx.coroutines.flow.Flow
+import java.util.Date
 
 interface OrdersRepository {
     val orders: AppResult<Flow<List<Order>>, AppError>
     val orderStatistic: AppResult<Flow<OrderStatistic>, AppError>
+    fun ordersInRange(from: Date, to: Date): Flow<List<Order>>
     suspend fun getOrderById(id: Int): AppResult<Order, AppError>
     suspend fun softRefreshOrders()
     suspend fun hardRefreshOrders()
