e1acd83f703a2f4588cbd7e4efb5abe3bb17d159
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-22 18:26:56 +0300

    Create separate file to keep table names.

---
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/database/TableNames.kt
@@ -0,0 +1,5 @@
+package com.hfad.palamarchuksuperapp.feature.bone.data.local.database
+
+const val ORDERS = "orders"
+const val REMOTE_KEYS = "remote_order_keys"
+const val PAYMENTS = "payment_order"
