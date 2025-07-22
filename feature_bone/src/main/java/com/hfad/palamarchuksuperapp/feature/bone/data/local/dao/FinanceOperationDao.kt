658306813718d52083dd4c8cdda88d4135cf21c3
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-22 18:28:10 +0300

    Mark classes as DAO.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/dao/FinanceOperationDao.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/dao/FinanceOperationDao.kt
@@ -1,10 +1,12 @@
 package com.hfad.palamarchuksuperapp.feature.bone.data.local.dao
 
+import androidx.room.Dao
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.FinanceStatistics
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.TypedTransaction
 import kotlinx.coroutines.flow.Flow
 import java.util.Date
 
+@Dao
 interface FinanceOperationDao {
     fun operationsInRange(from: Date, to: Date) : Flow<List<TypedTransaction>>
     val operations : Flow<List<TypedTransaction>>
