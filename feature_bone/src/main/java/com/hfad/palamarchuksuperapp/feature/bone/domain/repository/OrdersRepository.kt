195780e8d59d7b097fbebbda59b36d104be7f27d
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-24 19:47:04 +0300

    Remove useless repository functions.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/OrdersRepository.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/OrdersRepository.kt
@@ -1,18 +1,17 @@
 package com.hfad.palamarchuksuperapp.feature.bone.domain.repository
 
+import androidx.paging.PagingData
 import com.hfad.palamarchuksuperapp.core.domain.AppError
 import com.hfad.palamarchuksuperapp.core.domain.AppResult
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
-import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistics
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatus
 import kotlinx.coroutines.flow.Flow
 import java.util.Date
 
 interface OrdersRepository {
-    val cachedOrders: Flow<AppResult<List<Order>, AppError>>
-    val cachedOrderStatistics: Flow<AppResult<OrderStatistics, AppError>>
-    suspend fun ordersByPage(page: Int, size: Int): Flow<AppResult<List<Order>, AppError>>
     suspend fun ordersInRange(from: Date, to: Date): Flow<AppResult<List<Order>, AppError>>
     suspend fun getOrderById(id: Int): Flow<AppResult<Order?, AppError>>
-    suspend fun softRefreshOrders()
-    suspend fun hardRefreshOrders()
+    suspend fun softRefreshStatistic()
+
+    fun pagingOrders(status: OrderStatus?): Flow<PagingData<Order>>
 }
