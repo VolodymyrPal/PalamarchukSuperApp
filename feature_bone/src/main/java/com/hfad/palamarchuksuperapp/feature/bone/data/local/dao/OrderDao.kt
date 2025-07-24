ad80ac7fa8ce4756de7e7354a0e128bbad7df486
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-24 19:46:20 +0300

    Dao interface with paging support.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/dao/OrderDao.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/dao/OrderDao.kt
@@ -1,22 +1,26 @@
 package com.hfad.palamarchuksuperapp.feature.bone.data.local.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Query
+import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.ORDERS
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistics
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatus
 import kotlinx.coroutines.flow.Flow
 import java.util.Date
 
 @Dao
 interface OrderDao {
-    val cachedOrders : Flow<List<Order>>
-    val cachedOrderStatistics : Flow<OrderStatistics>
-    fun ordersInRange(from: Date, to: Date) : Flow<List<Order>>
+    @Query("SELECT * FROM $ORDERS WHERE :orderStatus IS NULL OR status = :orderStatus")
+    fun getOrders(orderStatus: OrderStatus?): PagingSource<Int, Order>
+
+    fun ordersInRange(from: Date, to: Date): Flow<List<Order>>
     suspend fun getOrderById(id: Int): Flow<Order?>
-    suspend fun getOrdersByPage(size: Int) : List<Order>
     suspend fun geAllOrders(): List<Order>
-    suspend fun insertOrIgnoreOrders(orders : List<Order>)
+    suspend fun insertOrIgnoreOrders(orders: List<Order>)
     suspend fun deleteAllOrders()
 
     suspend fun clearOrderStatistics()
-    suspend fun insertOrIgnoreOrderStatistic(statistic : OrderStatistics)
+    suspend fun insertOrIgnoreOrderStatistic(statistic: OrderStatistics)
 }
