bd717994c8f851e96512e774cc136df1353f2924
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-25 22:31:37 +0300

    Remotes key dao now pass with order dao as used with same packages.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/dao/OrderDao.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/dao/OrderDao.kt
@@ -2,8 +2,12 @@ package com.hfad.palamarchuksuperapp.feature.bone.data.local.dao
 
 import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.ORDERS
+import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.DATABASE_ORDERS
+import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.OrderRemoteKeys
+import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.DATABASE_REMOTE_KEYS
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistics
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatus
@@ -12,7 +16,7 @@ import java.util.Date
 
 @Dao
 interface OrderDao {
-    @Query("SELECT * FROM $ORDERS WHERE :orderStatus IS NULL OR status = :orderStatus")
+    @Query("SELECT * FROM $DATABASE_ORDERS WHERE :orderStatus IS NULL OR status = :orderStatus")
     fun getOrders(orderStatus: OrderStatus?): PagingSource<Int, Order>
 
     fun ordersInRange(from: Date, to: Date): Flow<List<Order>>
@@ -23,4 +27,17 @@ interface OrderDao {
 
     suspend fun clearOrderStatistics()
     suspend fun insertOrIgnoreOrderStatistic(statistic: OrderStatistics)
+}
+
+@Dao
+interface RemoteKeysDao {
+
+    @Query("SELECT * FROM ${DATABASE_REMOTE_KEYS} WHERE id = :orderId AND `filter` = :filterStatus")
+    suspend fun remoteKeysOrderId(orderId: Int, filterStatus: OrderStatus?): OrderRemoteKeys?
+
+    @Insert(onConflict = OnConflictStrategy.Companion.REPLACE)
+    suspend fun insertAll(keys: List<OrderRemoteKeys>)
+
+    @Query("DELETE FROM ${DATABASE_REMOTE_KEYS}")
+    suspend fun clearRemoteKeys()
 }
