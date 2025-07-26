77d70371d3029195b9180a94fa11bd02b805cf94
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-26 23:29:34 +0300

    Add entity to combine OrderEntity with ServiceOrder entity.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/dao/OrderDao.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/dao/OrderDao.kt
@@ -6,8 +6,8 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.DATABASE_ORDERS
-import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.OrderRemoteKeys
 import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.DATABASE_REMOTE_KEYS
+import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.OrderRemoteKeys
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistics
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatus
@@ -32,12 +32,12 @@ interface OrderDao {
 @Dao
 interface RemoteKeysDao {
 
-    @Query("SELECT * FROM ${DATABASE_REMOTE_KEYS} WHERE id = :orderId AND `filter` = :filterStatus")
+    @Query("SELECT * FROM $DATABASE_REMOTE_KEYS WHERE id = :orderId AND `filter` = :filterStatus")
     suspend fun remoteKeysOrderId(orderId: Int, filterStatus: OrderStatus?): OrderRemoteKeys?
 
     @Insert(onConflict = OnConflictStrategy.Companion.REPLACE)
     suspend fun insertAll(keys: List<OrderRemoteKeys>)
 
-    @Query("DELETE FROM ${DATABASE_REMOTE_KEYS}")
+    @Query("DELETE FROM $DATABASE_REMOTE_KEYS")
     suspend fun clearRemoteKeys()
 }
