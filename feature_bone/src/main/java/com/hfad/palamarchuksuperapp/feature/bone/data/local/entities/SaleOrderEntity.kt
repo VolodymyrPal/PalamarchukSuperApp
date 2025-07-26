dead683ac264d58237855aa8b8d1b7079b218365
palamarchukhome <palamarchuk.office@gmail.com>
2025-07-26 23:26:51 +0300

    Create entity for sale orders.

---
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/entities/SaleOrderEntity.kt
@@ -0,0 +1,46 @@
+package com.hfad.palamarchuksuperapp.feature.bone.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.DATABASE_SALES
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Currency
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SaleStatus
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.TransactionType
+import java.util.Date
+
+@Entity(
+    tableName = DATABASE_SALES,
+    foreignKeys = [
+        ForeignKey(
+            entity = OrderEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["orderId"],
+            onDelete = ForeignKey.SET_NULL
+        )
+    ],
+    indices = [
+        Index("orderId")
+    ]
+)
+data class SaleOrderEntity(
+    @PrimaryKey
+    val id: Int,
+    val productName: String,
+    val cargoCategory: String,
+    val customerName: String,
+    val status: SaleStatus,
+    val requestDate: String,
+    val documentDate: String,
+    val companyName: String,
+    val commissionPercent: Double,
+    val prepayment: Boolean,
+    val orderId: Int? = null,
+    val vat: Float = 0.20f,
+    val sum: Float = 0.0f,
+    val currency: Currency = Currency.UAH,
+    val billingDate: Date = Date(),
+    val transactionType: TransactionType = TransactionType.DEBIT,
+    val versionHash: String = "",
+)
