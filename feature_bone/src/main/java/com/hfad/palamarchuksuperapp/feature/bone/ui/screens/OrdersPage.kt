bb017f347700ea785b89af2b2c125249f720d5aa
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-06 19:22:06 +0300

    Other small changes.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/screens/OrdersPage.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/screens/OrdersPage.kt
@@ -1,6 +1,5 @@
 package com.hfad.palamarchuksuperapp.feature.bone.ui.screens
 
-import android.util.Log
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
