202145d28cfaec51df518507137bd3e90f4ddd9f
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-08 23:43:05 +0300

    Remove redundant fields from bone api class.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/remote/api/BoneApi.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/remote/api/BoneApi.kt
@@ -2,36 +2,14 @@ package com.hfad.palamarchuksuperapp.feature.bone.data.remote.api
 
 import com.hfad.palamarchuksuperapp.core.domain.AppError
 import com.hfad.palamarchuksuperapp.core.domain.AppResult
-import com.hfad.palamarchuksuperapp.feature.bone.data.remote.dto.OrderDto
-import com.hfad.palamarchuksuperapp.feature.bone.data.toDto
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.ExchangeOrder
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
-import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistics
-import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatus
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.PaymentOrder
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.PaymentStatistic
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SaleOrder
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SalesStatistics
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.TypedTransaction
-import com.hfad.palamarchuksuperapp.feature.bone.domain.models.generateOrderItems
-import com.hfad.palamarchuksuperapp.feature.bone.ui.viewModels.generateOrderStatistic
-import kotlinx.coroutines.delay
 import java.util.Date
-import javax.inject.Inject
-
-class OrderApi @Inject constructor() {
-    suspend fun getOrdersByPage(page: Int, size: Int, status: OrderStatus?): List<OrderDto> {
-        delay(1500) //TODO
-        return generateOrderItems().map { it.toDto() }
-    }
-
-    fun getOrder(id: Int): OrderDto? = null
-    fun getOrdersWithRange(from: Date, to: Date): List<OrderDto> = emptyList()
-    suspend fun getOrderStatistics(): OrderStatistics {
-        delay(1500)
-        return generateOrderStatistic()
-    }
-}
 
 interface SaleOrderApi {
     fun getSaleOrdersByPage(page: Int): AppResult<List<SaleOrder>, AppError.NetworkException>
