b5dbbb14ac597ea76223e50aefce55f6340e2824
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-10 23:06:51 +0300

    State now keeps filter status.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/OrderPageState.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/OrderPageState.kt
@@ -2,7 +2,9 @@ package com.hfad.palamarchuksuperapp.feature.bone.ui.viewModels
 
 import com.hfad.palamarchuksuperapp.core.ui.genericViewModel.ScreenState
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistics
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatus
 
 data class OrderPageState(
     val orderMetrics: OrderStatistics = OrderStatistics(), // TODO for testing
+    val orderStatusFilter: OrderStatus? = null
 ) : ScreenState
