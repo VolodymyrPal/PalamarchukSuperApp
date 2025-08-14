f335c7fdec13468e8fc7b06a19671e0b0e6ca283
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-14 23:53:04 +0300

    Domain statistic don't need be marked as entity.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/OrderStatistics.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/OrderStatistics.kt
@@ -1,14 +1,6 @@
 package com.hfad.palamarchuksuperapp.feature.bone.domain.models
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.DATABASE_STATISTICS
-
-@Entity (
-    tableName = DATABASE_STATISTICS,
-)
 data class OrderStatistics(
-    @PrimaryKey
     val id: Int = 1,
     val totalOrders: Int = 0,
     val completedOrders: Int = 0,
