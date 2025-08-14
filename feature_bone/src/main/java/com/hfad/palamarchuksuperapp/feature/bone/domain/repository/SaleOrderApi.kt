d929eceac0a196b130b202ce8b1f19d1a3f80b96
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-14 23:50:50 +0300

    Make API interface more straightforward, same for sale class.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/SaleOrderApi.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/SaleOrderApi.kt
@@ -1,19 +1,13 @@
 package com.hfad.palamarchuksuperapp.feature.bone.domain.repository
 
-import com.hfad.palamarchuksuperapp.core.domain.AppError
-import com.hfad.palamarchuksuperapp.core.domain.AppResult
-import com.hfad.palamarchuksuperapp.feature.bone.domain.models.Order
-import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SaleOrder
+import com.hfad.palamarchuksuperapp.feature.bone.data.remote.dto.SaleOrderDto
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SaleStatus
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SalesStatistics
 import java.util.Date
 
 interface SaleOrderApi {
-    fun getSaleOrdersByPage(page: Int): AppResult<List<SaleOrder>, AppError.NetworkException>
-    fun getSaleOrder(id: Int): AppResult<SaleOrder, AppError.NetworkException>
-    fun getSaleOrdersWithRange(
-        from: Date,
-        to: Date,
-    ): AppResult<List<Order>, AppError.NetworkException>
-
-    fun syncSaleStatistics(): AppResult<SalesStatistics, AppError.NetworkException>
+    suspend fun getSalesByPage(page: Int, size: Int, status: SaleStatus?): List<SaleOrderDto>
+    suspend fun getSaleOrder(id: Int): SaleOrderDto?
+    suspend fun getSalesWithRange(from: Date, to: Date): List<SaleOrderDto>
+    suspend fun getSalesStatistics(): SalesStatistics
 }
