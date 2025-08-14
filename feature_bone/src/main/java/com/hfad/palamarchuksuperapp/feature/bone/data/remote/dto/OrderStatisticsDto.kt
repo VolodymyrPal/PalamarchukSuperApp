905a0725ac746304143c5280887d2ca080190d92
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-14 23:55:37 +0300

    Add dto object for order statistic.

---
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/remote/dto/OrderStatisticsDto.kt
@@ -0,0 +1,7 @@
+package com.hfad.palamarchuksuperapp.feature.bone.data.remote.dto
+
+data class OrderStatisticsDto(
+    val inProgressOrders: Int = 0,
+    val completedOrders: Int = 0,
+    val totalOrderWeight: Float = 0f,
+)
