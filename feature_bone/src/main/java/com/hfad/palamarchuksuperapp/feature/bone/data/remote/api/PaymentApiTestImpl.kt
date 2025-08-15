0f00f89e44492931b3144f9749238dda0e35bae1
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-15 18:52:11 +0300

    Add test implementation for api call of sales, payments.

---
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/remote/api/PaymentApiTestImpl.kt
@@ -0,0 +1,32 @@
+package com.hfad.palamarchuksuperapp.feature.bone.data.remote.api
+
+import com.hfad.palamarchuksuperapp.feature.bone.data.remote.dto.PaymentOrderDto
+import com.hfad.palamarchuksuperapp.feature.bone.data.toDto
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.PaymentStatus
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.PaymentStatistic
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.generatePaymentOrderItems
+import com.hfad.palamarchuksuperapp.feature.bone.domain.repository.PaymentOrderApi
+import com.hfad.palamarchuksuperapp.feature.bone.domain.models.generatePaymentStatistic
+import kotlinx.coroutines.delay
+import java.util.Date
+import javax.inject.Inject
+
+class PaymentApiTestImpl @Inject constructor(
+
+) : PaymentOrderApi {
+    override suspend fun getPaymentsByPage(
+        page: Int,
+        size: Int,
+        status: PaymentStatus?,
+    ): List<PaymentOrderDto> {
+        delay(1500) //TODO
+        return generatePaymentOrderItems().map { it.toDto() }
+    }
+
+    override suspend fun getPaymentOrder(id: Int): PaymentOrderDto? = null
+    override suspend fun getPaymentsWithRange(from: Date, to: Date): List<PaymentOrderDto> = emptyList()
+    override suspend fun getPaymentStatistics(): PaymentStatistic {
+        delay(1500)
+        return generatePaymentStatistic()
+    }
+}
