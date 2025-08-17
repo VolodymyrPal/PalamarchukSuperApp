1379d26968fe3a1d242655519400fbd509a99923
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-17 13:24:48 +0200

    Change signature to use const name of database, name status, not filter.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/entities/keys/PaymentRemoteKeys.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/entities/keys/PaymentRemoteKeys.kt
@@ -2,12 +2,13 @@ package com.hfad.palamarchuksuperapp.feature.bone.data.local.entities.keys
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.DATABASE_REMOVE_KEYS_PAYMENT
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.PaymentStatus
 
-@Entity(tableName = "remote_payment_keys")
+@Entity(tableName = DATABASE_REMOVE_KEYS_PAYMENT)
 data class PaymentRemoteKeys(
     @PrimaryKey val id: Int,
-    val filter: PaymentStatus?,
+    val status: PaymentStatus?,
     val prevKey: Int?,
     val nextKey: Int?,
 )
--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/entities/keys/SaleRemoteKeys.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/data/local/entities/keys/SaleRemoteKeys.kt
@@ -2,12 +2,13 @@ package com.hfad.palamarchuksuperapp.feature.bone.data.local.entities.keys
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.hfad.palamarchuksuperapp.feature.bone.data.local.database.DATABASE_REMOVE_KEYS_SALES
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.SaleStatus
 
-@Entity(tableName = "remote_sale_keys")
+@Entity(tableName = DATABASE_REMOVE_KEYS_SALES)
 data class SaleRemoteKeys(
     @PrimaryKey val id: Int,
-    val filter: SaleStatus?,
+    val status: SaleStatus?,
     val prevKey: Int?,
     val nextKey: Int?,
 )
