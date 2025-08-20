27ec504d9c4d231e7d8ed3db49dbb0a6ee7b3c05
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-20 18:27:22 +0300

    Adding date range state to use accross page state.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/OrderPageState.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/OrderPageState.kt
@@ -3,9 +3,11 @@ package com.hfad.palamarchuksuperapp.feature.bone.ui.viewModels
 import com.hfad.palamarchuksuperapp.core.ui.genericViewModel.ScreenState
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatistics
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.OrderStatus
+import java.util.Date
 
 data class OrderPageState(
     val orderMetrics: OrderStatistics = OrderStatistics(), // TODO for testing
     val orderStatusFilter: List<OrderStatus> = emptyList(),
     val searchQuery: String = "",
+    val dateRange: Pair<Date, Date> = Pair(Date(), Date()),
 ) : ScreenState
