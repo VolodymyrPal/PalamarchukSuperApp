e61865b64aa71759c3876a754d3bd10a1a441b3d
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-25 23:30:07 +0300

    Update finance page state, fix repository interface.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/FinanceRepository.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/repository/FinanceRepository.kt
@@ -5,9 +5,8 @@ import com.hfad.palamarchuksuperapp.core.domain.AppResult
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.FinanceStatistics
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.TypedTransaction
 import kotlinx.coroutines.flow.Flow
-import java.util.Date
 
 interface FinanceRepository {
-    suspend fun operationsInRange(from: Date, to: Date): AppResult<List<TypedTransaction>, AppError>
+    suspend fun operationsInRange(from: Long, to: Long): AppResult<List<TypedTransaction>, AppError>
     val financeStatistics: Flow<AppResult<FinanceStatistics, AppError>>
 }
--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/FinancePageState.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/ui/viewModels/FinancePageState.kt
@@ -1,13 +1,19 @@
 package com.hfad.palamarchuksuperapp.feature.bone.ui.viewModels
 
+import com.hfad.palamarchuksuperapp.core.ui.genericViewModel.ScreenState
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.FinanceStatistics
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.TypedTransaction
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.generateExchangeOrderItems
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.generateOrderItems
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.generatePaymentOrderItems
 import com.hfad.palamarchuksuperapp.feature.bone.domain.models.generateSaleOrderItems
+import java.util.Calendar
 
 data class FinancePageState(
     val salesItems: List<TypedTransaction> = (generateOrderItems() + generateSaleOrderItems() + generateExchangeOrderItems() + generatePaymentOrderItems()).shuffled(),
     val financeStatistics: FinanceStatistics = FinanceStatistics(),
-)
+    val query: String = "",
+    val loading: Boolean = false,
+    val startDate: Calendar = Calendar.getInstance(),
+    val endDate: Calendar = Calendar.getInstance(),
+) : ScreenState
