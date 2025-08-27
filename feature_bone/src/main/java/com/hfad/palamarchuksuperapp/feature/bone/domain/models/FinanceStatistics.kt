c8bad0e27b256b058462f324555564419137cb38
palamarchukhome <palamarchuk.office@gmail.com>
2025-08-27 23:12:45 +0300

    Finance statistics produce payment list, add function for generating mock one.

--- feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/FinanceStatistics.kt
+++ feature_bone/src/main/java/com/hfad/palamarchuksuperapp/feature/bone/domain/models/FinanceStatistics.kt
@@ -4,4 +4,17 @@ data class FinanceStatistics(
     val totalSales: Float = 0f,
     val totalExpenses: Float = 0f,
     val totalProfit: Float = 0f,
+    val paymentsList: List<AmountCurrency> = emptyList(),
+)
+
+fun generateFinanceStatistics(): FinanceStatistics = FinanceStatistics(
+    totalSales = 40f,
+    totalExpenses = 20f,
+    totalProfit = 80000f,
+    paymentsList = listOf(
+        AmountCurrency(Currency.UAH, 725025.56f),
+        AmountCurrency(Currency.USD, 200000f),
+        AmountCurrency(Currency.EUR, 56350f),
+        AmountCurrency(Currency.BTC, 0.00005f),
+    ),
 )
